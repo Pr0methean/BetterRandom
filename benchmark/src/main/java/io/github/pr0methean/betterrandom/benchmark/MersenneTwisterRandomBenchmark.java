3b3638cd93fbb9dcfa67380af8285fcb62eccca6
Pr0methean <seahen123@gmail.com>
2017-09-05 16:58:17 -0700

    Fix Checker Framework errors

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/MersenneTwisterRandomBenchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/MersenneTwisterRandomBenchmark.java
@@ -3,11 +3,12 @@ package io.github.pr0methean.betterrandom.benchmark;
 import io.github.pr0methean.betterrandom.prng.MersenneTwisterRandom;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import java.util.Random;
+import org.checkerframework.checker.initialization.qual.UnknownInitialization;
 
 public class MersenneTwisterRandomBenchmark extends AbstractRandomBenchmark {
 
   @Override
-  protected Random createPrng() throws SeedException {
+  protected Random createPrng(@UnknownInitialization MersenneTwisterRandomBenchmark this) throws SeedException {
     return new MersenneTwisterRandom();
   }
 }
