e11b75a61536d72eac5314299b4aadac8cf12784
Pr0methean <seahen123@gmail.com>
2017-09-05 16:58:55 -0700

    Lagged changes from previous commit

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/XorShiftRandomBenchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/XorShiftRandomBenchmark.java
@@ -3,11 +3,12 @@ package io.github.pr0methean.betterrandom.benchmark;
 import io.github.pr0methean.betterrandom.prng.XorShiftRandom;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import java.util.Random;
+import org.checkerframework.checker.initialization.qual.UnknownInitialization;
 
 public class XorShiftRandomBenchmark extends AbstractRandomBenchmark {
 
   @Override
-  protected Random createPrng() throws SeedException {
+  protected Random createPrng(@UnknownInitialization XorShiftRandomBenchmark this) throws SeedException {
     return new XorShiftRandom();
   }
 }
