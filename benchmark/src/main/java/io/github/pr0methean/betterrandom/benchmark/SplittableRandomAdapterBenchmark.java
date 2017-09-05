d95dcc44c8236bca0fdeacbec8dbc35959d73721
Pr0methean <seahen123@gmail.com>
2017-09-05 16:58:17 -0700

    Fix Checker Framework errors

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/SplittableRandomAdapterBenchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/SplittableRandomAdapterBenchmark.java
@@ -4,11 +4,13 @@ import io.github.pr0methean.betterrandom.prng.adapter.SplittableRandomAdapter;
 import io.github.pr0methean.betterrandom.seed.DefaultSeedGenerator;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import java.util.Random;
+import org.checkerframework.checker.initialization.qual.UnknownInitialization;
 
 public class SplittableRandomAdapterBenchmark extends AbstractRandomBenchmark {
 
   @Override
-  protected Random createPrng() throws SeedException {
+  protected Random createPrng(@UnknownInitialization SplittableRandomAdapterBenchmark
+      this) throws SeedException {
     return new SplittableRandomAdapter(DefaultSeedGenerator.DEFAULT_SEED_GENERATOR);
   }
 }
