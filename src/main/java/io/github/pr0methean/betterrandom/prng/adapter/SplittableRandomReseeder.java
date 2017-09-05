c73a3f3523a4c19358423ad1a58528aa02ebfa0c
Pr0methean <seahen123@gmail.com>
2017-09-05 15:12:50 -0700

    Add class SplittableRandomReseeder
    Future work will test whether this improves RSRA performance.

---
+++ src/main/java/io/github/pr0methean/betterrandom/prng/adapter/SplittableRandomReseeder.java
@@ -0,0 +1,69 @@
+package io.github.pr0methean.betterrandom.prng.adapter;
+
+import io.github.pr0methean.betterrandom.util.LogPreFormatter;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.SplittableRandom;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.objenesis.Objenesis;
+import org.objenesis.ObjenesisStd;
+
+/**
+ * Utility class to re-seed a {@link java.util.SplittableRandom} instance using reflection and
+ * {@link sun.misc.Unsafe#putLongVolatile(Object, long, long)}. Since SplittableRandom doesn't
+ * support reseeding through its public API, this is the only way a {@link
+ * ReseedingSplittableRandomAdapter} can avoid a ridiculous load on the garbage collector.
+ */
+public final class SplittableRandomReseeder {
+  private static final LogPreFormatter LOG = new LogPreFormatter(SplittableRandomReseeder.class);
+  private static final Objenesis OBJENESIS = new ObjenesisStd();
+  @Nullable private static MethodHandle PUT_VOLATILE_LONG;
+  private static long GAMMA_FIELD_OFFSET;
+  private static long SEED_FIELD_OFFSET;
+  private static long GOLDEN_GAMMA;
+  private static boolean CAN_RESEED_REFLECTIVELY;
+
+  private SplittableRandomReseeder() {}
+
+  static {
+    try {
+      Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+      Object unsafe;
+      try {
+        Field unsafeInstance = unsafeClass.getDeclaredField("theUnsafe");
+        unsafeInstance.setAccessible(true);
+        unsafe = unsafeInstance.get(null);
+      } catch (NoSuchFieldException | IllegalAccessException e) {
+        unsafe = OBJENESIS.newInstance(unsafeClass);
+      }
+      Method getFieldOffset = unsafeClass.getDeclaredMethod("objectFieldOffset", Field.class);
+      GAMMA_FIELD_OFFSET = (long) (getFieldOffset.invoke(unsafe, SplittableRandom.class.getDeclaredField("gamma")));
+      SEED_FIELD_OFFSET = (long) (getFieldOffset.invoke(unsafe, SplittableRandom.class.getDeclaredField("seed")));
+      GOLDEN_GAMMA = (long) SplittableRandom.class.getDeclaredField("GOLDEN_GAMMA").get(null);
+      Method putVolatileLong = unsafeClass.getDeclaredMethod("putLongVolatile", Object.class, long.class, long.class);
+      putVolatileLong.setAccessible(true);
+      PUT_VOLATILE_LONG = MethodHandles.lookup().unreflect(putVolatileLong).bindTo(unsafe);
+      CAN_RESEED_REFLECTIVELY = true;
+    } catch (ReflectiveOperationException e) {
+      LOG.error("Can't reflectively reseed SplittableRandom instances: %s", e);
+      CAN_RESEED_REFLECTIVELY = false;
+    }
+  }
+
+  public static SplittableRandom reseed(SplittableRandom original, long seed) {
+    if (CAN_RESEED_REFLECTIVELY) {
+      try {
+        PUT_VOLATILE_LONG.invoke(original, SEED_FIELD_OFFSET, seed);
+        PUT_VOLATILE_LONG.invoke(original, GAMMA_FIELD_OFFSET, GOLDEN_GAMMA);
+        return original;
+      } catch (Throwable throwable) {
+        throw new RuntimeException(throwable);
+      }
+    } else {
+      return new SplittableRandom(seed);
+    }
+  }
+}
