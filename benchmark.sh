ddf5a8578a3c096e0ccf5a362dc342bc7c5e20af
Chris Hennick <seahen123@gmail.com>
2017-09-06 19:40:25 +0000

    Update benchmark.sh to tee results to a file.

--- benchmark.sh
+++ benchmark.sh
@@ -4,4 +4,6 @@ mvn -DskipTests clean install proguard:proguard && (
     cd ../benchmark
     mvn clean install
     cd ..
-    java -jar benchmark/target/benchmarks.jar) || cd ..
+    java -jar benchmark/target/benchmarks.jar 2>&1 |\
+        tee benchmark/target/benchmark_results.txt) ||\
+cd ..
