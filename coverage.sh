fa54c9749368452733f5b4289fcba7d5b0afa7fb
Chris Hennick <seahen123@gmail.com>
2017-09-06 02:31:11 +0000

    Shell-script debugs.

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-mvn jacoco:prepare-agent test jacoco:report -pl betterrandom &&\
+mvn jacoco:prepare-agent -pl betterrandom &&\
+mvn test -pl betterrandom &&\
+mvn jacoco:report -pl betterrandom &&\
 if [ "$TRAVIS" = "true" ]; then
   mvn coveralls:report -pl betterrandom
 fi
