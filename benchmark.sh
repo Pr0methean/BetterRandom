b53732118b86fa95cb682ab015d477c7579a1b56
Chris Hennick <seahen123@gmail.com>
2017-09-06 20:14:45 +0000

    Skip trying to unit-test the benchmarks (they don't have tests since they *are* tests). Should reduce junk output.

--- benchmark.sh
+++ benchmark.sh
@@ -2,7 +2,7 @@
 cd betterrandom
 mvn -DskipTests clean install proguard:proguard && (
     cd ../benchmark
-    mvn clean install
+    mvn -DskipTests clean install
     cd ..
     java -jar benchmark/target/benchmarks.jar 2>&1 |\
         tee benchmark/target/benchmark_results.txt) ||\
