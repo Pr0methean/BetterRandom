28f60ea37c2144cf30740950c15a2c08d604b0da
Chris Hennick <seahen123@gmail.com>
2017-09-07 10:40:12 +0000

    Bugfix; also fix false negatives in the test shell scripts

--- benchmark.sh
+++ benchmark.sh
@@ -3,7 +3,7 @@ cd betterrandom
 mvn -DskipTests clean package proguard:proguard &&\
 cd ../benchmark &&\
 mvn -DskipTests clean package &&\
-cd ..
+cd .. &&\
 java -cp benchmark/target/benchmarks.jar \
     io.github.pr0methean.betterrandom.benchmark.AbstractRandomBenchmark \
     -v EXTRA
