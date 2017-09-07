5ea4cdf0b17c28dca22a2dbeb8a8d5934bddf76d
Chris Hennick <seahen123@gmail.com>
2017-09-07 02:37:58 +0000

    Extra verbose output from benchmark (since exceptions are disappearing otherwise)

--- benchmark.sh
+++ benchmark.sh
@@ -4,5 +4,5 @@ mvn -DskipTests clean package proguard:proguard &&\
 cd ../benchmark &&\
 mvn -DskipTests clean package &&\
 cd ..
-java -jar benchmark/target/benchmarks.jar -f 1 -foe true 2>&1 |\
+java -jar benchmark/target/benchmarks.jar -f 1 -foe true -v EXTRA 2>&1 |\
     tee ./benchmark/target/benchmark_results.txt
