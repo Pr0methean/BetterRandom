7b6e7cff8ef6c1d968a784d5e8ffcecd63b1fddc
Pr0methean <seahen123@gmail.com>
2017-09-07 01:19:53 -0700

    Move benchmark configuration into Java code

--- benchmark.sh
+++ benchmark.sh
@@ -4,7 +4,4 @@ mvn -DskipTests clean package proguard:proguard &&\
 cd ../benchmark &&\
 mvn -DskipTests clean package &&\
 cd ..
-java -jar benchmark/target/benchmarks.jar -f 1 -t 1 -foe true -v EXTRA 2>&1 |\
-    tee ./benchmark/target/benchmark_results_one_thread.txt
-java -jar benchmark/target/benchmarks.jar -f 1 -t 2 -foe true -v EXTRA 2>&1 |\
-    tee ./benchmark/target/benchmark_results_two_threads.txt
+java -jar benchmark/target/benchmarks.jar -foe true -v EXTRA
