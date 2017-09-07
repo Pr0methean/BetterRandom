7cf54745836aa6531c33d1a0fcc00ace4912e786
Chris Hennick <seahen123@gmail.com>
2017-09-07 05:55:26 +0000

    CompletionService wasn't behaving as expected, so we'll try just using thread joins

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/AbstractRandomBenchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/AbstractRandomBenchmark.java
@@ -2,25 +2,17 @@ package io.github.pr0methean.betterrandom.benchmark;
 
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorCompletionService;
-import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
 import org.checkerframework.checker.initialization.qual.UnknownInitialization;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.TearDown;
 
 @State(Scope.Benchmark)
 public abstract class AbstractRandomBenchmark {
 
   private static final int COLUMNS = 2;
-  private static final int ROWS = 10_000;
+  private static final int ROWS = 50_000;
   protected final byte[][] bytes = new byte[COLUMNS][ROWS];
-  protected final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(2);
-  protected final ExecutorCompletionService<Void> completion =
-      new ExecutorCompletionService<>(executor);
   protected final Random prng;
 
   {
@@ -46,31 +38,21 @@ public abstract class AbstractRandomBenchmark {
     return innerTestBytesSequential();
   }
 
-  protected byte innerTestBytesContended() throws InterruptedException, ExecutionException {
-    for (final byte[] column : bytes) {
-      completion.submit(() -> prng.nextBytes(column), null);
+  protected byte innerTestBytesContended() throws InterruptedException {
+    Thread[] threads = new Thread[COLUMNS];
+    for (int column = 0; column < COLUMNS; column++) {
+      threads[column] = new Thread(() -> prng.nextBytes(column));
+      threads[column].start();
     }
-    // Wait for all jobs to finish
-    while (true) {
-      Future<Void> future = completion.poll();
-      if (future == null) {
-        break;
-      } else {
-        future.get();
-      }
+    // Wait for the threads to finish
+    for (Thread thread : threads) {
+      thread.join();
     }
     return bytes[prng.nextInt(COLUMNS)][prng.nextInt(ROWS)];
   }
 
   @Benchmark
-  public byte testBytesContended() throws SeedException, InterruptedException, ExecutionException {
+  public byte testBytesContended() throws SeedException, InterruptedException {
     return innerTestBytesContended();
-  }
-
-  @TearDown
-  public void tearDown() {
-    if (!(executor.shutdownNow().isEmpty())) {
-      throw new AssertionError("Thread pool had unfinished tasks at teardown");
-    }
   }
 }
