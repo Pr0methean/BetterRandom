595c484889f79af6c66af1fa958d57b0bdb6f271
Chris Hennick <seahen123@gmail.com>
2017-09-07 03:28:15 +0000

    Remove benchmark_plumbr.sh; this is the wrong type of workload for plumbr

--- benchmark_plumbr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd betterrandom
-mvn -DskipTests clean package proguard:proguard &&\
-cd ../benchmark &&\
-mvn -DskipTests clean package &&\
-cd ..
-java -javaagent:../plumbr/plumbr.jar -jar ./benchmark/target/benchmarks.jar -f 1 -foe true -v EXTRA 2>&1 |\
-    tee ./benchmark/target/benchmark_results.txt
