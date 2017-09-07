242d462b697d2f1a52997742e50d757795c44d22
Chris Hennick <seahen123@gmail.com>
2017-09-07 01:00:31 +0000

    Enhancement: RandomSeederThread.iterate() now doesn't hold one PRNG alive while reseeding the next one.

--- benchmark.sh
+++ benchmark.sh
@@ -4,6 +4,6 @@ mvn -DskipTests clean package proguard:proguard && (
     cd ../benchmark
     mvn -DskipTests clean package
     cd ..
-    java -jar benchmark/target/benchmarks.jar -gc true -f 1 -foe true 2>&1 |\
+    java -jar benchmark/target/benchmarks.jar -f 1 -foe true 2>&1 |\
         tee ./benchmark/target/benchmark_results.txt) ||\
 cd ..
