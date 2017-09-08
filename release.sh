a9b2d739e89077ed1845e18e9cae39b0ba1cd410
Chris Hennick <seahen123@gmail.com>
2017-09-08 19:52:03 +0000

    Detach betterrandom/pom.xml from root pom.xml so that we don't have to release an empty parent package anymore.

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+cd betterrandom &&\
 rm -f release.properties
-rm -f betterrandom/release.properties
 rm -rf ../.m2/repository/io/github/pr0methean/betterrandom/
 mvn release:clean release:prepare &&\
-mvn release:perform
+mvn release:perform &&\
+cd ..
