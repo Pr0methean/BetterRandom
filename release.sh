ef6dc39cf4a4315cf55277ce82ab2edc570d3402
Chris Hennick <seahen123@gmail.com>
2017-09-08 08:54:39 +0000

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+rm -f release.properties
+rm -f betterrandom/release.properties
+rm -rf ../.m2/repository/io/github/pr0methean/betterrandom/
 mvn release:clean release:prepare &&\
 mvn release:perform
