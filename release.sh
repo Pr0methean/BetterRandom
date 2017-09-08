0d5dd7c9cf6c2fd935dab8f2c47b4c9f336e807b
Chris Hennick <seahen123@gmail.com>
2017-09-08 22:36:07 +0000

    Fix minor bugs in release.sh

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd betterrandom &&\
-rm -f release.properties
-rm -rf ../.m2/repository/io/github/pr0methean/betterrandom/
+rm -f release.properties &&\
+rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
 mvn release:clean release:prepare &&\
 mvn release:perform &&\
 cd ..
