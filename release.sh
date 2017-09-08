ce49824e989b2c9fd79304a225aba93f1e0aa80f
Chris Hennick <seahen123@gmail.com>
2017-09-08 23:36:24 +0000

    Update release.sh to work around Proguard's expectation of "-SNAPSHOT" in the jar name.

--- release.sh
+++ release.sh
@@ -2,6 +2,12 @@
 cd betterrandom &&\
 rm -f release.properties &&\
 rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
-mvn release:clean release:prepare proguard:proguard &&\
+mvn release:clean release:prepare &&\
+cd target &&\
+# WTF? Why does proguard:proguard still expect -SNAPSHOT in the jar name?!
+rename 's/(.*)\.jar$/$1-SNAPSHOT\.jar/' &&\
+mvn proguard:proguard &&\
+rename 's/(.*)-SNAPSHOT\.jar$/$1\.jar/' &&\
+cd .. &&\
 mvn release:perform &&\
 cd ..
