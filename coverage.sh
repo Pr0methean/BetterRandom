09b70a26da70f09c2e4b78b3f4f267bac90de123
Chris Hennick <seahen123@gmail.com>
2017-09-09 04:30:59 +0000

    Enable full stack traces in coverage runs, to try to troubleshoot JDK9 failures on Travis.

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd betterrandom
-mvn jacoco:prepare-agent test jacoco:report
+mvn jacoco:prepare-agent test jacoco:report -e
 STATUS=$?
 if [ "$STATUS" = 0 ] && [ "$TRAVIS" = "true" ]; then
   mvn coveralls:report
