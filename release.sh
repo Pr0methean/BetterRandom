be52d410b0af93b6011185dbcb4fe9e6347cec7d
Chris Hennick <seahen123@gmail.com>
2017-09-10 17:57:14 +0000

    Revert "Temporary commit for revert"
    This reverts commit 6fa9fa74da495b11b56e93944454fb2a1e8842b8.

--- release.sh
+++ release.sh
@@ -5,9 +5,9 @@ rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
 mvn release:clean release:prepare &&\
 cd target &&\
 # WTF? Why does proguard:proguard still expect -SNAPSHOT in the jar name?!
-rename 's/(.*)\.jar$/$1-SNAPSHOT\.jar/' &&\
-mvn proguard:proguard &&\
-rename 's/(.*)-SNAPSHOT\.jar$/$1\.jar/' &&\
+# rename 's/(.*)\.jar$/$1-SNAPSHOT\.jar/' &&\
+# mvn proguard:proguard &&\
+# rename 's/(.*)-SNAPSHOT\.jar$/$1\.jar/' &&\
 cd .. &&\
 mvn release:perform &&\
 cd ..
