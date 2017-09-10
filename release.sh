5dc0266f1e2957d7b1dbd7058bd22f9281e867a2
Chris Hennick <seahen123@gmail.com>
2017-09-10 18:49:29 +0000

    Update release.sh

--- release.sh
+++ release.sh
@@ -2,12 +2,7 @@
 cd betterrandom &&\
 rm -f release.properties &&\
 rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
-mvn release:clean release:prepare &&\
-cd target &&\
-# WTF? Why does proguard:proguard still expect -SNAPSHOT in the jar name?!
-rename 's/(.*)\.jar$/$1-SNAPSHOT\.jar/' &&\
-mvn proguard:proguard &&\
-rename 's/(.*)-SNAPSHOT\.jar$/$1\.jar/' &&\
-cd .. &&\
+mvn release:clean &&\
+mvn release:prepare -DpreparationGoals="clean package proguard:proguard verify" &&\
 mvn release:perform &&\
 cd ..
