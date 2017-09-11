45be83ef85f4aecae47ee8519be1b97ec4cef198
Chris Hennick <seahen123@gmail.com>
2017-09-11 06:36:50 +0000

    Fix bug in benchmark script where errors were being swallowed

--- benchmark.sh
+++ benchmark.sh
@@ -4,8 +4,12 @@ mvn -DskipTests clean package proguard:proguard install &&\
 cd ../benchmark &&\
 mvn -DskipTests clean package &&\
 cd target &&\
-java -jar benchmarks.jar -f 1 -t 1 -foe true -v EXTRA 2>&1 |\
-    tee benchmark_results_one_thread.txt &&\
-java -jar benchmarks.jar -f 1 -t 2 -foe true -v EXTRA 2>&1 |\
-    tee benchmark_results_two_threads.txt &&\
-cd ../..
+if [ $TRAVIS = "true" ]; then
+    java -jar benchmarks.jar -f 1 -t 1 -foe true -v EXTRA &&\
+    java -jar benchmarks.jar -f 1 -t 2 -foe true -v EXTRA
+else
+    java -jar benchmarks.jar -f 1 -t 1 -foe true -v EXTRA 2>&1 |\
+        tee benchmark_results_one_thread.txt &&\
+    java -jar benchmarks.jar -f 1 -t 2 -foe true -v EXTRA 2>&1 |\
+        tee benchmark_results_two_threads.txt
+fi && cd ../..
