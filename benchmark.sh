173ca303d3ce80ba68a2a77f184a51c6a02bb697
Chris Hennick <seahen123@gmail.com>
2017-09-11 07:40:54 +0000

    Override Proguard version to 5.3.3 to fix VerifyError

--- benchmark.sh
+++ benchmark.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
+JVMARGS="-jar benchmarks.jar -f 1 -foe true -v EXTRA"
+
+#Workaround for https://sourceforge.net/p/proguard/bugs/578/
+# JVMARGS="$JVMARGS -Xverify:none"
+
 cd betterrandom
 mvn -DskipTests clean package proguard:proguard install &&\
 cd ../benchmark &&\
 mvn -DskipTests clean package &&\
 cd target &&\
 if [ "$TRAVIS" = "true" ]; then
-    java -jar benchmarks.jar -f 1 -t 1 -foe true -v EXTRA &&\
-    java -jar benchmarks.jar -f 1 -t 2 -foe true -v EXTRA
+    java "$JVMARGS" -t 1 &&\
+    java "$JVMARGS" -t 2 &&\
 else
-    java -jar benchmarks.jar -f 1 -t 1 -foe true -v EXTRA 2>&1 |\
-        tee benchmark_results_one_thread.txt &&\
-    java -jar benchmarks.jar -f 1 -t 2 -foe true -v EXTRA 2>&1 |\
-        tee benchmark_results_two_threads.txt
+    java "$JVMARGS" -t 1 2>&1 | tee benchmark_results_one_thread.txt &&\
+    java "$JVMARGS" -t 2 2>&1 | tee benchmark_results_two_threads.txt &&\
 fi && cd ../..
