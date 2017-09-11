231ea0538d7fbe01ae8ca583c335e56586c2bb13
Chris Hennick <seahen123@gmail.com>
2017-09-11 19:29:22 +0000

    Add explicit dep on maven-scm-provider-gitexe. May fix my release issue per https://stackoverflow.com/questions/15166781/mvn-releaseprepare-not-committing-changes-to-pom-xml.

--- release.sh
+++ release.sh
@@ -3,6 +3,6 @@ cd betterrandom &&\
 rm -f release.properties &&\
 rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
 mvn release:clean &&\
-mvn release:prepare -DpreparationGoals="clean package proguard:proguard verify" &&\
-mvn release:perform &&\
+mvn release:prepare -X -DpreparationGoals="clean package proguard:proguard verify" &&\
+mvn release:perform -X &&\
 cd ..
