f19189a391786562925f0ee95a3524d81d910360
Chris Hennick <seahen123@gmail.com>
2017-09-11 07:42:18 +0000

    Script bugfix

--- benchmark.sh
+++ benchmark.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
-JVMARGS="-jar benchmarks.jar -f 1 -foe true -v EXTRA"
-
-#Workaround for https://sourceforge.net/p/proguard/bugs/578/
-# JVMARGS="$JVMARGS -Xverify:none"
-
 cd betterrandom
 mvn -DskipTests clean package proguard:proguard install &&\
 cd ../benchmark &&\
 mvn -DskipTests clean package &&\
 cd target &&\
 if [ "$TRAVIS" = "true" ]; then
-    java "$JVMARGS" -t 1 &&\
-    java "$JVMARGS" -t 2 &&\
+    java -jar benchmarks.jar -f 1 -t 1 -foe true -v EXTRA &&\
+    java -jar benchmarks.jar -f 1 -t 2 -foe true -v EXTRA
 else
-    java "$JVMARGS" -t 1 2>&1 | tee benchmark_results_one_thread.txt &&\
-    java "$JVMARGS" -t 2 2>&1 | tee benchmark_results_two_threads.txt &&\
+    java -jar benchmarks.jar -f 1 -t 1 -foe true -v EXTRA 2>&1 |\
+        tee benchmark_results_one_thread.txt &&\
+    java -jar benchmarks.jar -f 1 -t 2 -foe true -v EXTRA 2>&1 |\
+        tee benchmark_results_two_threads.txt
 fi && cd ../..
