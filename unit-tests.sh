fa9129eb48b2a35d42588952cda422a1c131a4f9
Chris Hennick <seahen123@gmail.com>
2017-09-13 22:21:53 +0000

    More partial Androidization

--- unit-tests.sh
+++ unit-tests.sh
@@ -6,16 +6,16 @@ else
 fi
 cd betterrandom
 # Coverage test
-mvn $MAYBE_ANDROID_FLAG clean jacoco:prepare-agent test jacoco:report -e
+mvn "$MAYBE_ANDROID_FLAG" clean jacoco:prepare-agent test jacoco:report -e
 STATUS=$?
 if [ "$STATUS" = 0 ]; then
   if [ "$TRAVIS" = "true" ]; then
-    mvn $MAYBE_ANDROID_FLAG coveralls:report
+    mvn "$MAYBE_ANDROID_FLAG" coveralls:report
   fi
-  mvn -DskipTests $MAYBE_ANDROID_FLAG package proguard:proguard && (
+  mvn -DskipTests "$MAYBE_ANDROID_FLAG" package proguard:proguard && (
   rm .surefire-* # Workaround for https://issues.apache.org/jira/browse/SUREFIRE-1414
   # Post-Proguard test (verifies Proguard settings)
-  mvn test -e)
+  mvn "$MAYBE_ANDROID_FLAG" test -e)
   STATUS=$?
 fi
 cd ..
