4195075af4c5787d5668d6dcd1ab33c5354bece6
Pr0methean <seahen123@gmail.com>
2017-09-13 16:57:24 -0700

    Delayed changes from previous commit

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/RandomWrapperTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/RandomWrapperTest.java
@@ -15,8 +15,8 @@
 // ============================================================================
 package io.github.pr0methean.betterrandom.prng;
 
-import io.github.pr0methean.betterrandom.seed.SeedException;
 import io.github.pr0methean.betterrandom.Failing;
+import io.github.pr0methean.betterrandom.seed.SeedException;
 import org.testng.annotations.Test;
 
 /**
