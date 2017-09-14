82de9139abc1ea3b9864ce12e2fc3b5ca28b7033
Pr0methean <seahen123@gmail.com>
2017-09-14 15:49:43 -0700

    LooperThread and LooperThreadTest improvements

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/util/LooperThreadTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/util/LooperThreadTest.java
@@ -1,40 +1,85 @@
 package io.github.pr0methean.betterrandom.util;
 
-import static org.checkerframework.checker.nullness.NullnessUtil.castNonNull;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertNotSame;
 import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertSame;
 import static org.testng.Assert.assertTrue;
 
 import io.github.pr0methean.betterrandom.TestUtil;
+import java.io.InvalidObjectException;
 import java.io.Serializable;
 import java.lang.Thread.State;
 import java.lang.Thread.UncaughtExceptionHandler;
+import java.lang.reflect.Field;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import org.testng.Assert;
-import org.testng.TestException;
-import org.testng.annotations.AfterTest;
+import org.checkerframework.checker.nullness.qual.RequiresNonNull;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class LooperThreadTest {
 
+  private static final String THREAD_NAME = "LooperThread for serialization test";
+  private static final String GROUP_NAME = SerializableThreadGroup.class.getSimpleName();
+  private static final long STACK_SIZE = 1_234_567;
+  private static final Field THREAD_STACK_SIZE;
+  private static final Field THREAD_TARGET;
+
+  static {
+    try {
+      THREAD_STACK_SIZE = Thread.class.getDeclaredField("stackSize");
+      THREAD_STACK_SIZE.setAccessible(true);
+      THREAD_TARGET = Thread.class.getDeclaredField("target");
+      THREAD_TARGET.setAccessible(true);
+    } catch (NoSuchFieldException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * Intermediate used to give {@link SerializableThreadGroup} a parameterless super constructor
+   * for deserialization purposes.
+   */
+  private static class SerializableThreadGroupSurrogate extends ThreadGroup {
+    public SerializableThreadGroupSurrogate() {
+      super(GROUP_NAME);
+    }
+  }
+
+  private static class SerializableThreadGroup extends SerializableThreadGroupSurrogate
+      implements Serializable {
+
+    public SerializableThreadGroup() {
+      super();
+    }
+  }
+
   private static class SkeletonLooperThread extends LooperThread {
+
+    public SkeletonLooperThread() {
+    }
+
+    public SkeletonLooperThread(Runnable target) {
+      super(target);
+    }
+
+    public SkeletonLooperThread(ThreadGroup group, Runnable target, String name, long stackSize) {
+      super(group, target, name, stackSize);
+    }
+
+    @RequiresNonNull({"group", "target", "name"})
+    @Override
+    protected LooperThread readResolveConstructorWrapper() throws InvalidObjectException {
+      return new SkeletonLooperThread(group, target, name, stackSize);
+    }
+
     @Override
     public boolean iterate() throws InterruptedException {
-      if (shouldThrow.get()) {
-        throw new TestUtil.MockException();
-      }
-      return iterationsRun.addAndGet(1) < 100;
+      TARGET.run();
+      return iterationsRun.get() < 100;
     }
   }
 
@@ -49,10 +94,15 @@ public class LooperThreadTest {
   private static class MockClassLoader extends ClassLoader {}
   private static class SerializableClassLoader extends ClassLoader implements Serializable {}
 
-  private static final Lock TEST_LOCK = new ReentrantLock();
   private static final AtomicLong iterationsRun = new AtomicLong();
   private static final AtomicBoolean shouldThrow = new AtomicBoolean(false);
   private static final     AtomicBoolean exceptionHandlerRun = new AtomicBoolean(false);
+  private static final Runnable TARGET = (Serializable & Runnable)() -> {
+    if (shouldThrow.get()) {
+      throw new TestUtil.MockException();
+    }
+    iterationsRun.addAndGet(1);
+  };
 
   @BeforeTest
   public void setUp() {
@@ -90,13 +140,12 @@ public class LooperThreadTest {
   @SuppressWarnings("argument.type.incompatible")
   @Test
   public void testSerializable_nonSerializableState()
-      throws InterruptedException, MalformedURLException {
-    LooperThread thread = new SkeletonLooperThread();
+      throws InterruptedException, MalformedURLException, IllegalAccessException {
+    LooperThread thread = new SkeletonLooperThread(() -> {});
     thread.setContextClassLoader(new MockClassLoader());
     thread.setUncaughtExceptionHandler((thread_, throwable) -> exceptionHandlerRun.set(true));
     LooperThread copy = TestUtil.serializeAndDeserialize(thread);
     assertSame(copy.getContextClassLoader(), Thread.currentThread().getContextClassLoader());
-    assertNull(copy.getUncaughtExceptionHandler());
     shouldThrow.set(true);
     copy.start();
     copy.join();
@@ -105,13 +154,22 @@ public class LooperThreadTest {
 
   @SuppressWarnings("dereference.of.nullable")
   @Test
-  public void testSerializable_serializableState() throws InterruptedException {
-    LooperThread thread = new SkeletonLooperThread();
+  public void testSerializable_serializableState()
+      throws InterruptedException, IllegalAccessException {
+    LooperThread thread = new LooperThread(new SerializableThreadGroup(), TARGET, THREAD_NAME, STACK_SIZE);
     thread.setContextClassLoader(new SerializableClassLoader());
     thread.setUncaughtExceptionHandler(new SerializableUncaughtExceptionHandler());
+    thread.setPriority(2);
+    thread.setDaemon(true);
     LooperThread copy = TestUtil.serializeAndDeserialize(thread);
     assertTrue(copy.getContextClassLoader() instanceof SerializableClassLoader);
     assertTrue(copy.getUncaughtExceptionHandler() instanceof SerializableUncaughtExceptionHandler);
+    assertEquals(2, copy.getPriority());
+    assertTrue(copy.isDaemon());
+    assertEquals(THREAD_NAME, copy.getName());
+    assertTrue(copy.getThreadGroup() instanceof SerializableThreadGroup);
+    assertEquals(GROUP_NAME, copy.getThreadGroup().getName());
+    assertEquals(STACK_SIZE, THREAD_STACK_SIZE.get(copy));
     shouldThrow.set(true);
     copy.start();
     copy.join();
