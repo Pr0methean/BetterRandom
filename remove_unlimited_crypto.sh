55087d29252bc85861ac97eed97ec1f6aa7e5e63
Chris Hennick <seahen123@gmail.com>
2017-09-14 07:31:30 +0000

    Narrow the remove_unlimited_crypto bypass to only OS X

--- remove_unlimited_crypto.sh
+++ remove_unlimited_crypto.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ "$JAVA_HOME" != "" ]; then
+if [ "$TRAVIS_OS_NAME" != "osx" ]; then
   sudo mv -f travis-resources/local_policy.jar $JAVA_HOME/jre/lib/security/local_policy.jar
   sudo mv -f travis-resources/US_export_policy.jar $JAVA_HOME/jre/lib/security/US_export_policy.jar
 fi
