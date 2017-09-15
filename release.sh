8fa33c64de5871ef982cbf8eafca2a175061ff00
Chris Hennick <seahen123@gmail.com>
2017-09-15 03:07:06 +0000

    Bugfix: release.sh was running tests twice

--- release.sh
+++ release.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 cd betterrandom &&\
-OLDVERSION=version=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }' | sed 's/version=//'`
+OLDVERSION=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }' | sed 's/version=//'`
 rm -f release.properties &&\
 rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
 (mvn release:clean &&\
 mvn release:prepare -X &&\
-mvn release:perform -X -Dgoals="package proguard:proguard" ) ||\
+mvn release:perform -X -DskipTests -Dgoals="package proguard:proguard" ) ||\
 (
-    NEWVERSION=version=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }' | sed 's/version=//'`
+    NEWVERSION=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }' | sed 's/version=//'`
     if [ "$NEWVERSION" != "$OLDVERSION" ]; then
       git tag -d "BetterRandom-$NEWVERSION"
       git push origin ":refs/tags/BetterRandom-$NEWVERSION"
