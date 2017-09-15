b7eb5d696e8e24cb7087a63c3ecf4c4fb04f19e7
Chris Hennick <seahen123@gmail.com>
2017-09-15 02:44:43 +0000

    Bugfix for release.sh; rollback failed release

--- release.sh
+++ release.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 cd betterrandom &&\
-OLDVERSION=version=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
+OLDVERSION=version=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }' | sed 's/version=//'`
 rm -f release.properties &&\
 rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
 (mvn release:clean &&\
 mvn release:prepare -X &&\
 mvn release:perform -X -Dgoals="package proguard:proguard" ) ||\
 (
-    NEWVERSION=version=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
+    NEWVERSION=version=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }' | sed 's/version=//'`
     if [ "$NEWVERSION" != "$OLDVERSION" ]; then
-      git tag -d "BetterRandom-${NEWVERSION}"
-      git push origin ":refs/tags/BetterRandom-${NEWVERSION}"
+      git tag -d "BetterRandom-$NEWVERSION"
+      git push origin ":refs/tags/BetterRandom-$NEWVERSION"
       mvn versions:set "-DoldVersion=$NEWVERSION" "-DnewVersion=$OLDVERSION"
     fi
 )
