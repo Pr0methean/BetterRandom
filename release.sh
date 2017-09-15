2c21fc1ab194a64eeb261f55cf7452ec55a17ff5
Chris Hennick <seahen123@gmail.com>
2017-09-15 01:09:08 +0000

    Partial debug to get proguard into the lifecycle

--- release.sh
+++ release.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 cd betterrandom &&\
-OLDVERSION=version=`cd $project_loc && mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
+OLDVERSION=version=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
 rm -f release.properties &&\
 rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
 (mvn release:clean &&\
-mvn release:prepare -X -DpreparationGoals="clean package proguard:proguard" &&\
+mvn release:prepare -X &&\
 mvn release:perform -X) ||\
 (
-    NEWVERSION=version=`cd $project_loc && mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
+    NEWVERSION=version=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
     if [ "$NEWVERSION" != "$OLDVERSION" ]; then
       git tag -d "BetterRandom-${NEWVERSION}"
       git push origin ":refs/tags/BetterRandom-${NEWVERSION}"
