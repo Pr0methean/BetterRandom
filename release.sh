c708436a1288343b1863559bbe9ee17b222fd149
Chris Hennick <seahen123@gmail.com>
2017-09-15 06:01:03 +0000

    Uncorrupt pom.xml

--- release.sh
+++ release.sh
@@ -3,10 +3,11 @@ cd betterrandom &&\
 OLDVERSION=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }' | sed 's/version=//'`
 rm -f release.properties &&\
 rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
-(mvn release:clean &&\
-mvn release:prepare -X &&\
-mvn release:perform -X -Dmaven.test.skip=true ||\
 (
+  mvn release:clean &&\
+  mvn release:prepare -X &&\
+  mvn release:perform -X -Dmaven.test.skip=true
+) || (
     NEWVERSION=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }' | sed 's/version=//'`
     if [ "$NEWVERSION" != "$OLDVERSION" ]; then
       git tag -d "BetterRandom-$NEWVERSION"
