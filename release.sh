2359b8f301caca339423cea7a4334a8d109e96af
Chris Hennick <seahen123@gmail.com>
2017-09-15 05:17:23 +0000

    release.sh bugfix

--- release.sh
+++ release.sh
@@ -5,8 +5,8 @@ rm -f release.properties &&\
 rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
 (mvn release:clean &&\
 mvn release:prepare -X &&\
-mvn release:perform -X -Dmaven.test.skip=true -Dgoals="package proguard:proguard" ) &&\
-mvn deploy -DskipTests -Dmaven.main.skip=true ||\
+mvn package &&\
+mvn release:perform -X -Dmaven.main.skip=true -Dmaven.test.skip=true -Dgoals="proguard:proguard deploy" ) ||\
 (
     NEWVERSION=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }' | sed 's/version=//'`
     if [ "$NEWVERSION" != "$OLDVERSION" ]; then
