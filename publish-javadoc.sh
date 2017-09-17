b8fdd87c47a7ac57a1a7d029dba311f077a29c7f
Chris Hennick <seahen123@gmail.com>
2017-09-17 22:42:41 +0000

    Bugfix

--- publish-javadoc.sh
+++ publish-javadoc.sh
@@ -5,7 +5,7 @@ echo '</body></html>' >> betterrandom/src/main/javadoc/overview.html
 cd betterrandom
 mvn javadoc:javadoc
 cd ..
-mv -f betterrandom/target/site/apidocs docs
+mv -f betterrandom/target/site/apidocs/* docs
 sed -i 's/<head>/<head><script async src="\/\/pagead2.googlesyndication.com\/pagead\/js\/adsbygoogle.js"><\/script><script>(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "ca-pub-9922551172827508", enable_page_level_ads: true});<\/script>/' docs/index.html
 cd docs
 git add .
