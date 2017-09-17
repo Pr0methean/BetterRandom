2821f7188115f62589d342ec494120b417059aaf
Chris Hennick <seahen123@gmail.com>
2017-09-17 05:03:46 +0000

    First step of enabling AdSense

--- publish-javadoc.sh
+++ publish-javadoc.sh
@@ -7,6 +7,13 @@ mvn javadoc:javadoc
 cd ..
 rm -rf docs
 mv betterrandom/target/site/apidocs docs
+sed -i 's/<head>/<head><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-3178057947968979",
+    enable_page_level_ads: true
+  });
+</script>/' docs/index.html
 git add docs
 git commit -m "🤖 Update Javadocs for GitHub Pages"
 git push
