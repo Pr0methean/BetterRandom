9f73df6cc32d5827880475809726a986938088d7
Chris Hennick <seahen123@gmail.com>
2017-09-18 01:18:52 +0000

    Switch mv to cp, since mv lacks the recursive merge I need

--- publish-javadoc.sh
+++ publish-javadoc.sh
@@ -5,7 +5,7 @@ echo '</body></html>' >> betterrandom/src/main/javadoc/overview.html
 cd betterrandom
 mvn javadoc:javadoc
 cd ..
-mv -f betterrandom/target/site/apidocs/* docs
+cp -r betterrandom/target/site/apidocs/* docs
 sed -i 's/<head>/<head><script async src="\/\/pagead2.googlesyndication.com\/pagead\/js\/adsbygoogle.js"><\/script><script>(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "ca-pub-9922551172827508", enable_page_level_ads: true});<\/script>/' docs/index.html
 cd docs
 find . -iname "*.html" | xargs sed -i 's/<head>/<head><script async src="https://www.googletagmanager.com/gtag/js?id=UA-106545233-1"><\/script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config', 'UA-106545233-1');<\/script>/' {}
