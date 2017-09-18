9bb484b6cae5a44190ef27caacf869f32b06aac0
Chris Hennick <seahen123@gmail.com>
2017-09-18 01:25:49 +0000

    Google Analytics bugfixes

--- publish-javadoc.sh
+++ publish-javadoc.sh
@@ -8,7 +8,7 @@ cd ..
 cp -r betterrandom/target/site/apidocs/* docs
 sed -i 's/<head>/<head><script async src="\/\/pagead2.googlesyndication.com\/pagead\/js\/adsbygoogle.js"><\/script><script>(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "ca-pub-9922551172827508", enable_page_level_ads: true});<\/script>/' docs/index.html
 cd docs
-find . -iname "*.html" | xargs sed -i 's/<head>/<head><script async src="https://www.googletagmanager.com/gtag/js?id=UA-106545233-1"><\/script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config', 'UA-106545233-1');<\/script>/' {}
+find . -iname "*.html" -exec sed -i 's/<head>/<head><script async src="https:\/\/www.googletagmanager.com\/gtag\/js?id=UA-106545233-1"><\/script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config', 'UA-106545233-1');<\/script>/' {} \;
 git add .
 git commit -m "🤖 Update Javadocs for GitHub Pages"
 git push
