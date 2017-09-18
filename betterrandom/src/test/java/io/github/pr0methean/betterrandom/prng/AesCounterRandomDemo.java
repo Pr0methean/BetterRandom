e7b56e686e61683955390c0ccce76b8fdb25dd4d
Chris Hennick <seahen123@gmail.com>
2017-09-18 22:10:16 +0000

    Switch AesCounterRandomDemo to SecureRandomSeedGenerator, since it was overloading random.org

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/AesCounterRandomDemo.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/AesCounterRandomDemo.java
@@ -1,6 +1,6 @@
 package io.github.pr0methean.betterrandom.prng;
 
-import static io.github.pr0methean.betterrandom.seed.RandomDotOrgSeedGenerator.RANDOM_DOT_ORG_SEED_GENERATOR;
+import static io.github.pr0methean.betterrandom.seed.SecureRandomSeedGenerator.SECURE_RANDOM_SEED_GENERATOR;
 
 import io.github.pr0methean.betterrandom.seed.RandomSeederThread;
 import io.github.pr0methean.betterrandom.seed.SeedException;
@@ -8,8 +8,8 @@ import io.github.pr0methean.betterrandom.util.BinaryUtils;
 
 public class AesCounterRandomDemo {
   public static void main(String[] args) throws SeedException {
-    AesCounterRandom random = new AesCounterRandom(RANDOM_DOT_ORG_SEED_GENERATOR);
-    RandomSeederThread.getInstance(RANDOM_DOT_ORG_SEED_GENERATOR).add(random);
+    AesCounterRandom random = new AesCounterRandom(SECURE_RANDOM_SEED_GENERATOR);
+    RandomSeederThread.getInstance(SECURE_RANDOM_SEED_GENERATOR).add(random);
     byte[] randomBytes = new byte[32];
     for (int i=0; i<20; i++) {
       random.nextBytes(randomBytes);
