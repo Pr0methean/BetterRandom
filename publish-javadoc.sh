3d001d470b8c2730ca811617fe64da4404acd4b3
Chris Hennick <seahen123@gmail.com>
2017-09-18 01:50:33 +0000

    Bugfix: index page needs a <body> tag

--- publish-javadoc.sh
+++ publish-javadoc.sh
@@ -6,18 +6,17 @@ cd betterrandom
 mvn javadoc:javadoc
 cd ..
 cp -r betterrandom/target/site/apidocs/* docs
-
-# AdSense
-sed -i 's/<head>/<head><script async src="\/\/pagead2.googlesyndication.com\/pagead\/js\/adsbygoogle.js"><\/script><script>(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "ca-pub-9922551172827508", enable_page_level_ads: true});<\/script>/' docs/index.html
-
 cd docs
 
+# AdSense
+sed -i 's/<head>/<head><script async src="\/\/pagead2.googlesyndication.com\/pagead\/js\/adsbygoogle.js"><\/script><script>(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "ca-pub-9922551172827508", enable_page_level_ads: true});<\/script>/' index.html
+
 # Google Analytics
-find . -iname "*.html" -exec sed -i 's/<head>/<head><!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https:\/\/www.googletagmanager.com\/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K9NNCTT');<\/script><!-- End Google Tag Manager -->/' {} \;
-find . -iname "*.html" -exec sed -i 's/<body>/<body><!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https:\/\/www.googletagmanager.com\/ns.html?id=GTM-K9NNCTT"
-height="0" width="0" style="display:none;visibility:hidden"><\/iframe><\/noscript>
-<!-- End Google Tag Manager (noscript) -->/' {} \;
+sed -i 's/<\/head>/<\/head><body>' index.html
+sed -i 's/<\/html>/<\/body><\/html>' index.html
+
+find . -iname "*.html" -exec sed -i 's/<head>/<head><!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start": new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src="https:\/\/www.googletagmanager.com\/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);})(window,document,"script","dataLayer","GTM-K9NNCTT");<\/script><!-- End Google Tag Manager -->/' {} \;
+find . -iname "*.html" -exec sed -i 's/<body>/<body><!-- Google Tag Manager (noscript) --><noscript><iframe src="https:\/\/www.googletagmanager.com\/ns.html?id=GTM-K9NNCTT" height="0" width="0" style="display:none;visibility:hidden"><\/iframe><\/noscript><!-- End Google Tag Manager (noscript) -->/' {} \;
 
 git add .
 git commit -m "🤖 Update Javadocs for GitHub Pages"
