7b2a20bfe7f972631700414ea41b0f88a934c5ab
Chris Hennick <seahen123@gmail.com>
2017-09-18 02:54:18 +0000

    Add privacy policy link

--- publish-javadoc.sh
+++ publish-javadoc.sh
@@ -8,13 +8,16 @@ cd ..
 cp -r betterrandom/target/site/apidocs/* docs
 cd docs
 
+# Disable frames
+mv overview-summary.html index.html
+
 # AdSense
 sed -i 's/<head>/<head><script async src="\/\/pagead2.googlesyndication.com\/pagead\/js\/adsbygoogle.js"><\/script><script>(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "ca-pub-9922551172827508", enable_page_level_ads: true});<\/script>/' index.html
+find . -iname "*.html" -exec sed -i 's/<li><a href="[^\"]*" target="_top">Frames<\/a><\/li>//' {} \;
+find . -iname "*.html" -exec sed -i 's/<li><a href="[^\"]*" target="_top">No&nbsp;Frames<\/a><\/li>//' {} \;
+
 
 # Google Analytics
-sed -i 's/<\/head>/<\/head><body>' index.html
-sed -i 's/<\/html>/<\/body><\/html>' index.html
-
 find . -iname "*.html" -exec sed -i 's/<head>/<head><!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start": new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src="https:\/\/www.googletagmanager.com\/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);})(window,document,"script","dataLayer","GTM-K9NNCTT");<\/script><!-- End Google Tag Manager -->/' {} \;
 find . -iname "*.html" -exec sed -i 's/<body>/<body><!-- Google Tag Manager (noscript) --><noscript><iframe src="https:\/\/www.googletagmanager.com\/ns.html?id=GTM-K9NNCTT" height="0" width="0" style="display:none;visibility:hidden"><\/iframe><\/noscript><!-- End Google Tag Manager (noscript) -->/' {} \;
 
