eb0e83b5d2d847c0c9e63bf83915e7514b0d18fa
Chris Hennick <seahen123@gmail.com>
2017-09-23 19:25:43 +0000

    Skip coverage runs on Appveyor; test partly re-enabling RandomDotOrgSeedGeneratorTest there

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/seed/RandomDotOrgSeedGeneratorTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/seed/RandomDotOrgSeedGeneratorTest.java
@@ -16,7 +16,6 @@
 package io.github.pr0methean.betterrandom.seed;
 
 import static io.github.pr0methean.betterrandom.TestUtils.canRunRandomDotOrgLargeTest;
-import static io.github.pr0methean.betterrandom.TestUtils.isNotAppveyor;
 import static org.testng.Assert.assertEquals;
 
 import io.github.pr0methean.betterrandom.TestUtils;
@@ -41,9 +40,7 @@ public class RandomDotOrgSeedGeneratorTest {
 
   @Test(timeOut = 120000)
   public void testGenerator() throws SeedException {
-    if (isNotAppveyor()) {
-      SeedTestUtils.testGenerator(RandomDotOrgSeedGenerator.RANDOM_DOT_ORG_SEED_GENERATOR);
-    }
+    SeedTestUtils.testGenerator(RandomDotOrgSeedGenerator.RANDOM_DOT_ORG_SEED_GENERATOR);
   }
 
   /**
