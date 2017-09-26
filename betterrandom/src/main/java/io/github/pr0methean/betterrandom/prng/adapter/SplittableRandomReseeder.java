149c539dd9a223f2193752f3e7bced1ca43a1a1e
Pr0methean <seahen123@gmail.com>
2017-09-26 16:58:01 -0700

    Debug: getLongVolatile/putLongVolatile don't work properly on Travis
    Even though they work on c9.

--- betterrandom/src/main/java/io/github/pr0methean/betterrandom/prng/adapter/SplittableRandomReseeder.java
+++ betterrandom/src/main/java/io/github/pr0methean/betterrandom/prng/adapter/SplittableRandomReseeder.java
@@ -31,50 +31,21 @@ public final class SplittableRandomReseeder {
   /** Visible only for test debugging. */
   protected static final Class<?> SPLITTABLE_RANDOM_CLASS;
   private static final LogPreFormatter LOG = new LogPreFormatter(SplittableRandomReseeder.class);
-  private static final Objenesis OBJENESIS = new ObjenesisStd();
-  private static @MonotonicNonNull MethodHandle GET_LONG_VOLATILE;
-  private static @MonotonicNonNull MethodHandle PUT_LONG_VOLATILE;
   private static @MonotonicNonNull Field SEED_FIELD;
   private static @MonotonicNonNull Field GAMMA_FIELD;
-  private static long GAMMA_FIELD_OFFSET;
-  private static long SEED_FIELD_OFFSET;
   private static final long GOLDEN_GAMMA = 0x9e3779b97f4a7c15L;
   private static boolean CAN_RESEED_REFLECTIVELY;
 
   static {
     SPLITTABLE_RANDOM_CLASS = SplittableRandom.class;
+    CAN_RESEED_REFLECTIVELY = false;
     try {
-      CAN_RESEED_REFLECTIVELY = false;
-      final Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
-      Object unsafe;
-      try {
-        final Field unsafeInstance = unsafeClass.getDeclaredField("theUnsafe");
-        unsafeInstance.setAccessible(true);
-        unsafe = unsafeInstance.get(null);
-      } catch (NoSuchFieldException | IllegalAccessException e) {
-        unsafe = OBJENESIS.newInstance(unsafeClass);
-      }
-      final Method getFieldOffset = unsafeClass.getDeclaredMethod("objectFieldOffset", Field.class);
       GAMMA_FIELD = SPLITTABLE_RANDOM_CLASS.getDeclaredField("gamma");
       GAMMA_FIELD.setAccessible(true);
       SEED_FIELD = SPLITTABLE_RANDOM_CLASS.getDeclaredField("seed");
       SEED_FIELD.setAccessible(true);
       CAN_RESEED_REFLECTIVELY = true;
-      GAMMA_FIELD_OFFSET = (long) (getFieldOffset
-          .invoke(unsafe, GAMMA_FIELD));
-      SEED_FIELD_OFFSET = (long) (getFieldOffset
-          .invoke(unsafe, SEED_FIELD));
-      final MethodHandles.Lookup lookup = MethodHandles.lookup();
-      final Method putLongVolatile = unsafeClass
-          .getDeclaredMethod("putLongVolatile", Object.class, long.class, long.class);
-      putLongVolatile.setAccessible(true);
-      PUT_LONG_VOLATILE = lookup.unreflect(putLongVolatile).bindTo(unsafe);
-      final Method getLongVolatile = unsafeClass.getDeclaredMethod("getLongVolatile",
-          Object.class, long.class);
-      getLongVolatile.setAccessible(true);
-      GET_LONG_VOLATILE = lookup.unreflect(getLongVolatile).bindTo(unsafe);
     } catch (final Exception e) {
-      // May include at least one exception type that's new in Java 9
       LOG.error("Can't reflectively reseed SplittableRandom instances: %s", e);
     }
   }
@@ -95,13 +66,8 @@ public final class SplittableRandomReseeder {
       final long seed) {
     if (CAN_RESEED_REFLECTIVELY && original != null) {
       try {
-        if (PUT_LONG_VOLATILE == null) {
-          castNonNull(SEED_FIELD).setLong(original, seed);
-          castNonNull(GAMMA_FIELD).setLong(original, GOLDEN_GAMMA);
-        } else {
-          PUT_LONG_VOLATILE.invokeExact((Object) original, SEED_FIELD_OFFSET, seed);
-          PUT_LONG_VOLATILE.invokeExact((Object) original, GAMMA_FIELD_OFFSET, GOLDEN_GAMMA);
-        }
+        castNonNull(SEED_FIELD).setLong(original, seed);
+        castNonNull(GAMMA_FIELD).setLong(original, GOLDEN_GAMMA);
         return original;
       } catch (final Throwable t) {
         throw new RuntimeException(t);
@@ -120,11 +86,7 @@ public final class SplittableRandomReseeder {
   public static byte[] getSeed(final SplittableRandom splittableRandom) {
     if (canGetSeed()) {
       try {
-        return BinaryUtils.convertLongToBytes(
-            GET_LONG_VOLATILE == null
-                ? castNonNull(SEED_FIELD).getLong(splittableRandom)
-                : (long) (GET_LONG_VOLATILE
-                    .invokeExact((Object) splittableRandom, (long) SEED_FIELD_OFFSET)));
+        return BinaryUtils.convertLongToBytes(castNonNull(SEED_FIELD).getLong(splittableRandom));
       } catch (final Throwable t) {
         throw new RuntimeException(t);
       }
