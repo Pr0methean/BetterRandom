cb38d39eca405cccc3867431920ab3f45932fdeb
Pr0methean <seahen123@gmail.com>
2017-09-26 16:18:43 -0700

    Isolate affected class and its test

--- betterrandom/src/main/java/io/github/pr0methean/betterrandom/prng/package-info.java
+++
@@ -1,2 +0,0 @@
-/** Implementations of {@link java.util.Random}. */
-package io.github.pr0methean.betterrandom.prng;
--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/MockException.java
+++
@@ -1,5 +0,0 @@
-package io.github.pr0methean.betterrandom;
-
-public class MockException extends RuntimeException {
-
-}
