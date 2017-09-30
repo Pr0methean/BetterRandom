c1d27315993d8b65d2338a933b24f011dd70947f
Chris Hennick <seahen123@gmail.com>
2017-09-30 16:08:35 +0000

    Remove debug code from BaseRandom

--- betterrandom/src/main/java/io/github/pr0methean/betterrandom/prng/BaseRandom.java
+++ betterrandom/src/main/java/io/github/pr0methean/betterrandom/prng/BaseRandom.java
@@ -510,11 +510,9 @@ public abstract class BaseRandom extends Random implements ByteArrayReseedableRa
    * @param bits The number of bits of entropy spent.
    */
   protected void recordEntropySpent(final long bits) {
-    System.out.format("Entropy spent: %d%n", bits);
-    for (StackTraceElement e : Thread.currentThread().getStackTrace()) {
-      System.out.format("  %s%n", e);
+    if (entropyBits.addAndGet(-bits) <= 0) {
+      asyncReseedIfPossible();
     }
-    System.out.format("New entropy: %d%n", entropyBits.get());
   }
 
   private void asyncReseedIfPossible() {
