5e3eef518711fd91aa44ea9a295f6028931a7380
Pr0methean <seahen123@gmail.com>
2017-09-29 22:49:37 -0700

    Bugfix

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/AesCounterTestNgBug1563WorkaroundTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/AesCounterTestNgBug1563WorkaroundTest.java
@@ -1,10 +1,15 @@
 package io.github.pr0methean.betterrandom.prng;
 
+import static io.github.pr0methean.betterrandom.prng.RandomTestUtils.checkStream;
+
+import org.testng.annotations.Test;
+
 /**
  * Temporary workaround for https://github.com/cbeust/testng/issues/1563
  */
 public class AesCounterTestNgBug1563WorkaroundTest extends AesCounterRandom128Test {
 
+  @Test
   public void testLongs3_() throws Exception {
     super.testLongs3();
   }
