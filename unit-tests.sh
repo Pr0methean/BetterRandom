26eb827ce0a2d4f34d8b68ec67811ea570346049
Chris Hennick <seahen123@gmail.com>
2017-10-01 08:51:13 +0000

    Attempt to merge Jacoco reports

--- unit-tests.sh
+++ unit-tests.sh
@@ -12,7 +12,22 @@ fi
 STATUS=$?
 if [ "$STATUS" = 0 ]; then
   if [ "$TRAVIS" = "true" ]; then
-    mvn $MAYBE_ANDROID_FLAG coveralls:report
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI on behalf of Chris Hennick"
+    git clone https://github.com/Pr0methean/betterrandom-coverage.git
+    mkdir -p "betterrandom-coverage/$TRAVIS_BUILD_NUMBER"
+    cp target/jacoco.exec "betterrandom-coverage/$TRAVIS_BUILD_NUMBER/$TRAVIS_JOB_NUMBER.exec"
+    cp travis-resources/jacoco_merge.xml betterrandom-coverage/$TRAVIS_BUILD_NUMBER/pom.xml
+    cd betterrandom-coverage/$TRAVIS_BUILD_NUMBER
+    git add "$TRAVIS_BUILD_NUMBER"
+    git commit -m "Coverage report from job $TRAVIS_JOB_NUMBER"
+    while [ ! git push ]; do
+      git pull --commit  # Merge
+    done
+    mvn jacoco:merge
+    mv target/jacoco.exec ../target
+    cd ..
+    mvn coveralls:report
   fi
   mvn -DskipTests $MAYBE_ANDROID_FLAG package && (
   # Post-Proguard test (verifies Proguard settings)
