dac2167e3acadf78c9d65726ce0a3f96184eb53d
Pr0methean <seahen123@gmail.com>
2017-10-02 17:14:53 -0700

    Refactor *SupplierSpliterator by extracting an abstract superclass

---
+++ betterrandom/src/main/java/io/github/pr0methean/betterrandom/util/spliterator/AbstractSupplierSpliterator.java
@@ -0,0 +1,85 @@
+package io.github.pr0methean.betterrandom.util.spliterator;
+
+import static java.util.Spliterator.CONCURRENT;
+import static java.util.Spliterator.IMMUTABLE;
+import static java.util.Spliterator.NONNULL;
+import static java.util.Spliterator.SIZED;
+
+import java.util.Spliterator;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Consumer;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+/**
+ * An unordered, concurrent spliterator (possibly unboxed) that invokes a supplier (possibly
+ * unboxed) to get its values and has a preset size.
+ *
+ * @param <TSupplier> The supplier type.
+ * @param <TConsumer> The consumer type that {@link #tryAdvance(Object)} receives.
+ * @param <TSplitInto> The return type of {@link #trySplit()}.
+ */
+public abstract class AbstractSupplierSpliterator<TSupplier, TConsumer, TSplitInto> {
+
+  private final AtomicLong remaining;
+  private final AtomicLong splitsRemaining;
+  protected final TSupplier supplier;
+
+  /**
+   * Create an instance.
+   *
+   * @param size The maximum number of values to output.
+   * @param supplier The supplier to wrap.
+   */
+  public AbstractSupplierSpliterator(final long size, final TSupplier supplier) {
+    this(new AtomicLong(size), new AtomicLong(Long.SIZE - Long.numberOfLeadingZeros(size)),
+        supplier);
+  }
+
+  /** Used to share the AtomicLongs between partitions. */
+  protected AbstractSupplierSpliterator(final AtomicLong remaining,
+      final AtomicLong splitsRemaining,
+      final TSupplier supplier) {
+    this.remaining = remaining;
+    this.splitsRemaining = splitsRemaining;
+    this.supplier = supplier;
+  }
+
+  @SuppressWarnings("override.return.invalid") // actually is nullable in the interface
+  /** @see Spliterator#trySplit() */
+  public @Nullable TSplitInto trySplit() {
+    //LOG.logStackTrace(Level.INFO, Thread.currentThread().getStackTrace());
+    return ((splitsRemaining.getAndDecrement() <= 0) || (remaining.get() <= 0))
+        ? null
+        : internalSplit(remaining, splitsRemaining);
+  }
+
+  /**
+   * Should wrap a constructor that calls {@link #AbstractSupplierSpliterator(AtomicLong,
+   * AtomicLong, Object)}.
+   */
+  protected abstract TSplitInto internalSplit(AtomicLong remaining, AtomicLong splitsRemaining);
+
+  /** @see Spliterator#estimateSize() */
+  public long estimateSize() {
+    return remaining.get();
+  }
+
+  /** @see Spliterator#characteristics() */
+  public int characteristics() {
+    return SIZED | CONCURRENT | IMMUTABLE | NONNULL;
+  }
+
+  /** @see Spliterator#tryAdvance(Consumer) */
+  public boolean tryAdvance(final TConsumer action) {
+    //LOG.logStackTrace(Level.INFO, Thread.currentThread().getStackTrace());
+    if (remaining.decrementAndGet() >= 0) {
+      internalSupplyAndAccept(action);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /** Should be {@code action.accept(supplier.get())} or an equivalent. */
+  protected abstract void internalSupplyAndAccept(TConsumer action);
+}
--- betterrandom/src/main/java/io/github/pr0methean/betterrandom/util/spliterator/DoubleSupplierSpliterator.java
+++ betterrandom/src/main/java/io/github/pr0methean/betterrandom/util/spliterator/DoubleSupplierSpliterator.java
@@ -1,22 +1,17 @@
 package io.github.pr0methean.betterrandom.util.spliterator;
 
-import io.github.pr0methean.betterrandom.util.LogPreFormatter;
 import java.util.Spliterator.OfDouble;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.DoubleConsumer;
 import java.util.function.DoubleSupplier;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
 /**
  * An unordered, concurrent {@link OfDouble} that invokes a {@link DoubleSupplier} to get its values
  * and has a preset size.
  */
-public class DoubleSupplierSpliterator implements OfDouble {
-
-  private static final LogPreFormatter LOG = new LogPreFormatter(DoubleSupplierSpliterator.class);
-  private final AtomicLong remaining;
-  private final AtomicLong splitsRemaining;
-  private final DoubleSupplier supplier;
+public class DoubleSupplierSpliterator extends
+    AbstractSupplierSpliterator<DoubleSupplier, DoubleConsumer, DoubleSupplierSpliterator> implements
+    OfDouble {
 
   /**
    * Create an instance.
@@ -25,45 +20,22 @@ public class DoubleSupplierSpliterator implements OfDouble {
    * @param supplier The supplier to wrap.
    */
   public DoubleSupplierSpliterator(final long size, final DoubleSupplier supplier) {
-    this(new AtomicLong(size), new AtomicLong(Long.SIZE - Long.numberOfLeadingZeros(size)),
-        supplier);
+    super(size, supplier);
   }
 
-  /** Used to share the AtomicLongs between partitions. */
-  private DoubleSupplierSpliterator(final AtomicLong remaining, final AtomicLong splitsRemaining,
-      final DoubleSupplier supplier) {
-    this.remaining = remaining;
-    this.splitsRemaining = splitsRemaining;
-    this.supplier = supplier;
-  }
-
-  @SuppressWarnings("override.return.invalid") // actually is nullable in the interface
-  @Override
-  public @Nullable OfDouble trySplit() {
-    //LOG.logStackTrace(Level.INFO, Thread.currentThread().getStackTrace());
-    return ((splitsRemaining.getAndDecrement() <= 0) || (remaining.get() <= 0))
-        ? null
-        : new DoubleSupplierSpliterator(remaining, splitsRemaining, supplier);
+  private DoubleSupplierSpliterator(AtomicLong remaining, AtomicLong splitsRemaining,
+      DoubleSupplier doubleSupplier) {
+    super(remaining, splitsRemaining, doubleSupplier);
   }
 
   @Override
-  public long estimateSize() {
-    return remaining.get();
+  protected DoubleSupplierSpliterator internalSplit(AtomicLong remaining,
+      AtomicLong splitsRemaining) {
+    return new DoubleSupplierSpliterator(remaining, splitsRemaining, supplier);
   }
 
   @Override
-  public int characteristics() {
-    return SIZED | CONCURRENT | IMMUTABLE | NONNULL;
-  }
-
-  @Override
-  public boolean tryAdvance(final DoubleConsumer action) {
-    //LOG.logStackTrace(Level.INFO, Thread.currentThread().getStackTrace());
-    if (remaining.decrementAndGet() >= 0) {
-      action.accept(supplier.getAsDouble());
-      return true;
-    } else {
-      return false;
-    }
+  protected void internalSupplyAndAccept(DoubleConsumer action) {
+    action.accept(supplier.getAsDouble());
   }
 }
