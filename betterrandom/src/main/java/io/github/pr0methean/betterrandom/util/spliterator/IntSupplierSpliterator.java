c2e87795bccc924f9c782ec5ffc6ff3648b5302d
Pr0methean <seahen123@gmail.com>
2017-10-02 17:14:53 -0700

    Refactor *SupplierSpliterator by extracting an abstract superclass

--- betterrandom/src/main/java/io/github/pr0methean/betterrandom/util/spliterator/IntSupplierSpliterator.java
+++ betterrandom/src/main/java/io/github/pr0methean/betterrandom/util/spliterator/IntSupplierSpliterator.java
@@ -4,17 +4,12 @@ import java.util.Spliterator.OfInt;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.IntConsumer;
 import java.util.function.IntSupplier;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
 /**
  * An unordered, concurrent {@link OfInt} that invokes an {@link IntSupplier} to get its values and
  * has a preset size.
  */
-public class IntSupplierSpliterator implements OfInt {
-
-  private final AtomicLong remaining;
-  private final AtomicLong splitsRemaining;
-  private final IntSupplier supplier;
+public class IntSupplierSpliterator extends AbstractSupplierSpliterator<IntSupplier, IntConsumer, IntSupplierSpliterator> implements OfInt {
 
   /**
    * Create an instance.
@@ -23,43 +18,22 @@ public class IntSupplierSpliterator implements OfInt {
    * @param supplier The supplier to wrap.
    */
   public IntSupplierSpliterator(final long size, final IntSupplier supplier) {
-    this(new AtomicLong(size), new AtomicLong(Long.SIZE - Long.numberOfLeadingZeros(size)),
-        supplier);
+    super(size, supplier);
   }
 
   /** Used to share the AtomicLong between partitions. */
   private IntSupplierSpliterator(final AtomicLong remaining, final AtomicLong splitsRemaining,
       final IntSupplier supplier) {
-    this.remaining = remaining;
-    this.splitsRemaining = splitsRemaining;
-    this.supplier = supplier;
-  }
-
-  @SuppressWarnings("override.return.invalid") // actually is nullable in the interface
-  @Override
-  public @Nullable OfInt trySplit() {
-    return ((splitsRemaining.getAndDecrement() <= 0) || (remaining.get() <= 0))
-        ? null
-        : new IntSupplierSpliterator(remaining, splitsRemaining, supplier);
+    super(remaining, splitsRemaining, supplier);
   }
 
   @Override
-  public long estimateSize() {
-    return remaining.get();
+  protected IntSupplierSpliterator internalSplit(AtomicLong remaining, AtomicLong splitsRemaining) {
+    return new IntSupplierSpliterator(remaining, splitsRemaining, supplier);
   }
 
   @Override
-  public int characteristics() {
-    return SIZED | CONCURRENT | IMMUTABLE | NONNULL;
-  }
-
-  @Override
-  public boolean tryAdvance(final IntConsumer action) {
-    if (remaining.decrementAndGet() >= 0) {
-      action.accept(supplier.getAsInt());
-      return true;
-    } else {
-      return false;
-    }
+  protected void internalSupplyAndAccept(IntConsumer action) {
+    action.accept(supplier.getAsInt());
   }
 }
