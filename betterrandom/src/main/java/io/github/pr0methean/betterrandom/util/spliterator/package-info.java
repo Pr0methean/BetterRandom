4a13fc287bf3a151ad53b5bcbd1fddf34aee822e
Pr0methean <seahen123@gmail.com>
2017-10-10 07:13:28 -0700

    Refactor the stream functions in BaseRandom
    Now use TStream.generate(TSupplier)

--- betterrandom/src/main/java/io/github/pr0methean/betterrandom/util/spliterator/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Implementations of {@link java.util.Spliterator} and unboxed-primitive equivalents that wrap a
- * {@link java.util.function.Supplier} or the unboxed-primitive equivalent.
- */
-package io.github.pr0methean.betterrandom.util.spliterator;
