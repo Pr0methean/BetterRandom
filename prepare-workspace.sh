505a98caee1aba48430e62b6e740ffcb9b64eb31
Chris Hennick <seahen123@gmail.com>
2017-10-23 22:08:24 +0000

    Branch prepare-workspace.sh

--- prepare-workspace.sh
+++ prepare-workspace.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 sudo apt-get -y remove maven2
-sudo apt-add-repository ppa:webupd8team/java
 sudo apt-get -y update
-sudo apt-get -y install maven markdown oracle-java8-installer
+sudo apt-get -y install maven markdown
 sudo apt-get -y autoremove
 gem install github-markup
 gem install commonmarker
