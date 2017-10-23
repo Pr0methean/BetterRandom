d56ba821934620c8d48b2c86fb841c990f3cf582
Chris Hennick <seahen123@gmail.com>
2017-10-23 22:10:43 +0000

    Revert "Branch prepare-workspace.sh"
    This reverts commit 505a98caee1aba48430e62b6e740ffcb9b64eb31.

--- prepare-workspace.sh
+++ prepare-workspace.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 sudo apt-get -y remove maven2
+sudo apt-add-repository ppa:webupd8team/java
 sudo apt-get -y update
-sudo apt-get -y install maven markdown
+sudo apt-get -y install maven markdown oracle-java8-installer
 sudo apt-get -y autoremove
 gem install github-markup
 gem install commonmarker
