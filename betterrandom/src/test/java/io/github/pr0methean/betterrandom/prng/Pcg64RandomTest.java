30f9b364f2da5f7caadae29c4f24068b829f12f4
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2017-10-26 14:36:08 -0700

    New algo: Pcg64Random

---
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/Pcg64RandomTest.java
@@ -0,0 +1,42 @@
+package io.github.pr0methean.betterrandom.prng;
+
+import org.testng.annotations.Test;
+
+public class Pcg64RandomTest extends BaseRandomTest {
+
+  private static final int ITERATIONS = 8;
+
+  @Test
+  public void testAdvanceForward() {
+    Pcg64Random copy1 = createRng();
+    Pcg64Random copy2 = createRng(copy1.getSeed());
+    for (int i=0; i < ITERATIONS; i++) {
+      copy1.nextInt();
+    }
+    copy2.advance(ITERATIONS);
+    RandomTestUtils.testEquivalence(copy1, copy2, 20);
+  }
+
+  @Test
+  public void testAdvanceBackward() {
+    Pcg64Random copy1 = createRng();
+    Pcg64Random copy2 = createRng(copy1.getSeed());
+    for (int i=0; i < ITERATIONS; i++) {
+      copy1.nextInt();
+    }
+    copy1.advance(-ITERATIONS);
+    RandomTestUtils.testEquivalence(copy1, copy2, 20);
+  }
+
+  @Override protected Class<? extends BaseRandom> getClassUnderTest() {
+    return Pcg64Random.class;
+  }
+
+  @Override protected Pcg64Random createRng() {
+    return new Pcg64Random();
+  }
+
+  @Override protected Pcg64Random createRng(byte[] seed) {
+    return new Pcg64Random(seed);
+  }
+}
