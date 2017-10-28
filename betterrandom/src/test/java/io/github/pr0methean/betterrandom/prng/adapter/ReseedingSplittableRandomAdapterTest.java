28169965ca92a3e926bc20dc1b566c6c0cb46647
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2017-10-28 18:53:46 +0000

    Revert "Temporarily isolate the failing tests"
    This reverts commit 99a63d45ea531c1cd4896256176c214497b5d387.

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/adapter/ReseedingSplittableRandomAdapterTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/adapter/ReseedingSplittableRandomAdapterTest.java
@@ -1,5 +1,6 @@
 package io.github.pr0methean.betterrandom.prng.adapter;
 
+import static io.github.pr0methean.betterrandom.seed.DefaultSeedGenerator.DEFAULT_SEED_GENERATOR;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotEquals;
 
@@ -8,6 +9,8 @@ import io.github.pr0methean.betterrandom.seed.DefaultSeedGenerator;
 import io.github.pr0methean.betterrandom.seed.FakeSeedGenerator;
 import io.github.pr0methean.betterrandom.seed.RandomSeederThread;
 import io.github.pr0methean.betterrandom.seed.SeedException;
+import io.github.pr0methean.betterrandom.util.BinaryUtils;
+import io.github.pr0methean.betterrandom.util.CloneViaSerialization;
 import org.testng.annotations.Test;
 
 public class ReseedingSplittableRandomAdapterTest extends SingleThreadSplittableRandomAdapterTest {
@@ -16,17 +19,58 @@ public class ReseedingSplittableRandomAdapterTest extends SingleThreadSplittable
     return ReseedingSplittableRandomAdapter.getDefaultInstance();
   }
 
+  // FIXME: Why does this need more time than other PRNGs?!
+  @Test(timeOut = 120_000) @Override public void testDistribution() throws SeedException {
+    super.testDistribution();
+  }
+
+  // FIXME: Why does this need more time than other PRNGs?!
+  @Test(timeOut = 120_000) @Override public void testStandardDeviation() throws SeedException {
+    super.testStandardDeviation();
+  }
+
+  @Override @Test public void testSerializable() throws SeedException {
+    final BaseSplittableRandomAdapter adapter = createRng();
+    assertEquals(adapter, CloneViaSerialization.clone(adapter));
+  }
+
   @Override protected Class<? extends BaseRandom> getClassUnderTest() {
     return ReseedingSplittableRandomAdapter.class;
+  }
+
+  @Override @Test(enabled = false) public void testRepeatability() {
+    // No-op.
   }
 
   @Override @Test(enabled = false) public void testReseeding() {
     // No-op.
   }
 
+  /** Test for crashes only, since setSeed is a no-op. */
+  @Override @Test public void testSetSeed() throws SeedException {
+    final BaseRandom prng = createRng();
+    prng.nextLong();
+    prng.setSeed(DEFAULT_SEED_GENERATOR.generateSeed(8));
+    prng.setSeed(BinaryUtils.convertBytesToLong(DEFAULT_SEED_GENERATOR.generateSeed(8)));
+    prng.nextLong();
+  }
+
   /** This class manages its own interaction with a RandomSeederThread, so setSeederThread makes no sense. */
   @Override @Test(expectedExceptions = UnsupportedOperationException.class) public void testRandomSeederThreadIntegration() throws Exception {
     createRng().setSeederThread(RandomSeederThread.getInstance(DefaultSeedGenerator.DEFAULT_SEED_GENERATOR));
+  }
+
+  @Override @Test(enabled = false) public void testSeedTooShort() {
+    // No-op.
+  }
+
+  @Override @Test(enabled = false) public void testSeedTooLong() {
+    // No-op.
+  }
+
+  @Override @Test public void testDump() throws SeedException {
+    assertNotEquals(ReseedingSplittableRandomAdapter.getInstance(DEFAULT_SEED_GENERATOR).dump(),
+        ReseedingSplittableRandomAdapter.getInstance(new FakeSeedGenerator()).dump());
   }
 
   @Test public void testFinalize() throws SeedException {
