16cd3e944a5f5770577dc230bad14390ac376b54
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2017-10-28 18:53:46 +0000

    Revert "Temporarily isolate the failing tests"
    This reverts commit 7d63383715842c072a17760f5fe4d023352b45c1.

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/RandomWrapperAesCounterRandomTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/RandomWrapperAesCounterRandomTest.java
@@ -1,6 +1,7 @@
 package io.github.pr0methean.betterrandom.prng;
 
 import io.github.pr0methean.betterrandom.seed.SeedException;
+import java.lang.reflect.InvocationTargetException;
 import org.testng.annotations.Test;
 
 public class RandomWrapperAesCounterRandomTest extends AesCounterRandom128Test {
@@ -16,6 +17,12 @@ public class RandomWrapperAesCounterRandomTest extends AesCounterRandom128Test {
 
   @Override protected Class<? extends BaseRandom> getClassUnderTest() {
     return RandomWrapper.class;
+  }
+
+  @Override @Test(enabled = false) public void testAllPublicConstructors()
+      throws SeedException, IllegalAccessException, InstantiationException,
+      InvocationTargetException {
+    // No-op: redundant to super insofar as it works.
   }
 
   @Override protected RandomWrapper createRng() throws SeedException {
