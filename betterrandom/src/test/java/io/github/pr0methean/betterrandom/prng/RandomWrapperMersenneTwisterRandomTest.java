1535cecac6add28277c5468fa00bb67fc2740497
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2017-10-28 11:42:47 -0700

    Temporarily isolate the failing tests

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/RandomWrapperMersenneTwisterRandomTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/RandomWrapperMersenneTwisterRandomTest.java
@@ -1,19 +1,11 @@
 package io.github.pr0methean.betterrandom.prng;
 
 import io.github.pr0methean.betterrandom.seed.SeedException;
-import java.lang.reflect.InvocationTargetException;
-import org.testng.annotations.Test;
 
 public class RandomWrapperMersenneTwisterRandomTest extends BaseRandomTest {
 
   @Override protected Class<? extends BaseRandom> getClassUnderTest() {
     return RandomWrapper.class;
-  }
-
-  @Override @Test(enabled = false) public void testAllPublicConstructors()
-      throws SeedException, IllegalAccessException, InstantiationException,
-      InvocationTargetException {
-    // No-op: redundant to super insofar as it works.
   }
 
   @Override protected RandomWrapper createRng() throws SeedException {
--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/RandomWrapperRandomTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/RandomWrapperRandomTest.java
@@ -15,14 +15,7 @@
 // ============================================================================
 package io.github.pr0methean.betterrandom.prng;
 
-import com.google.common.collect.ImmutableMap;
-import io.github.pr0methean.betterrandom.TestUtils;
-import io.github.pr0methean.betterrandom.seed.DefaultSeedGenerator;
 import io.github.pr0methean.betterrandom.seed.SeedException;
-import io.github.pr0methean.betterrandom.seed.SeedGenerator;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Random;
-import org.testng.annotations.Test;
 
 /**
  * Unit test for the JDK RNG.
@@ -32,30 +25,6 @@ public class RandomWrapperRandomTest extends BaseRandomTest {
 
   @Override protected Class<? extends BaseRandom> getClassUnderTest() {
     return RandomWrapper.class;
-  }
-
-  @Override public void testAllPublicConstructors()
-      throws SeedException, IllegalAccessException, InstantiationException,
-      InvocationTargetException {
-    final BaseRandom basePrng = createRng();
-    final int seedLength = getNewSeedLength(basePrng);
-    TestUtils.testAllPublicConstructors(getClassUnderTest(), ImmutableMap
-        .of(int.class, seedLength, long.class, TEST_SEED, byte[].class,
-            DefaultSeedGenerator.DEFAULT_SEED_GENERATOR.generateSeed(seedLength),
-            SeedGenerator.class, DefaultSeedGenerator.DEFAULT_SEED_GENERATOR, Random.class,
-            new Random()), BaseRandom::nextInt);
-  }
-
-  /**
-   * Test to ensure that two distinct RNGs with the same seed return the same sequence of numbers.
-   */
-  @Override @Test(timeOut = 15000) public void testRepeatability() throws SeedException {
-    // Create an RNG using the default seeding strategy.
-    final RandomWrapper rng = new RandomWrapper();
-    // Create second RNG using same seed.
-    final RandomWrapper duplicateRNG = new RandomWrapper(rng.getSeed());
-    assert RandomTestUtils.testEquivalence(rng, duplicateRNG, 1000)
-        : "Generated sequences do not match.";
   }
 
   @Override protected BaseRandom createRng() throws SeedException {
