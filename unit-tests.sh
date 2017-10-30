e2616b32bcf58620b0799ca4daff4b089d27ce7c
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2017-10-30 13:45:29 -0700

    Tweaks to speed up Travis execution

--- unit-tests.sh
+++ unit-tests.sh
@@ -28,6 +28,9 @@ if [ "$STATUS" = 0 ]; then
       # Send coverage to Codacy
       wget 'https://github.com/codacy/codacy-coverage-reporter/releases/download/2.0.0/codacy-coverage-reporter-2.0.0-assembly.jar'
       java -jar codacy-coverage-reporter-2.0.0-assembly.jar -l Java -r target/site/jacoco/jacoco.xml
+
+      # Send coverage to Codecov
+      bash <(curl -s https://codecov.io/bash)
     fi
     COMMIT="$TRAVIS_COMMIT"
     JOB_ID="travis_$TRAVIS_JOB_NUMBER"
