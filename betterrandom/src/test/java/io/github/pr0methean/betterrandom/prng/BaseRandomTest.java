7ce653601c0f840b91adee839636bc406a5510d8
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2017-10-30 16:08:23 -0700

    Enable DeadlockWatchdogThread in testRandomSeederThreadIntegration

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/BaseRandomTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/BaseRandomTest.java
@@ -18,6 +18,7 @@ import io.github.pr0methean.betterrandom.seed.DefaultSeedGenerator;
 import io.github.pr0methean.betterrandom.seed.RandomSeederThread;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import io.github.pr0methean.betterrandom.seed.SeedGenerator;
+import io.github.pr0methean.betterrandom.util.DeadlockWatchdogThread;
 import io.github.pr0methean.betterrandom.util.EntryPoint;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -205,6 +206,7 @@ public abstract class BaseRandomTest {
     final RandomSeederThread seederThread = RandomSeederThread.getInstance(DefaultSeedGenerator.DEFAULT_SEED_GENERATOR);
     final BaseRandom rng = createRng();
     rng.setSeederThread(seederThread);
+    DeadlockWatchdogThread.ensureStarted();
     try {
       final byte[] oldSeed = rng.getSeed();
       while (rng.getEntropyBits() >= Long.SIZE) {
@@ -221,6 +223,7 @@ public abstract class BaseRandomTest {
       assertGreaterOrEqual(newSeed.length * 8L, rng.getEntropyBits());
     } finally {
       rng.setSeederThread(null);
+      DeadlockWatchdogThread.stopInstance();
     }
   }
 
