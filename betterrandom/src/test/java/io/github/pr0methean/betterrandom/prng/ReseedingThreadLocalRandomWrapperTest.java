6cd5f2b461f92ff97305a9b0d5b745e79c4af0fc
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2017-11-12 20:03:03 -0800

    Initial implementation
    Also refactors testAllPublicConstructors to reduce redundant code and
    improve readability

---
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/ReseedingThreadLocalRandomWrapperTest.java
@@ -0,0 +1,24 @@
+package io.github.pr0methean.betterrandom.prng;
+
+import io.github.pr0methean.betterrandom.seed.DefaultSeedGenerator;
+import io.github.pr0methean.betterrandom.seed.SeedException;
+import java.io.Serializable;
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class ReseedingThreadLocalRandomWrapperTest extends ThreadLocalRandomWrapperTest {
+
+  @Override public void testWrapLegacy() throws SeedException {
+    ReseedingThreadLocalRandomWrapper
+        .wrapLegacy(Random::new, DefaultSeedGenerator.DEFAULT_SEED_GENERATOR).nextInt();
+  }
+
+  @Override protected Class<? extends BaseRandom> getClassUnderTest() {
+    return super.getClassUnderTest();
+  }
+
+  @Override protected BaseRandom createRng() throws SeedException {
+    return new ReseedingThreadLocalRandomWrapper(DefaultSeedGenerator.DEFAULT_SEED_GENERATOR,
+        (Serializable & Supplier<BaseRandom>) MersenneTwisterRandom::new);
+  }
+}
