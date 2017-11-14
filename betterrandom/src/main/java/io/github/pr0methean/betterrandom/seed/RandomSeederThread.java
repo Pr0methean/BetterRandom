d3621d37c7781ee3b2331b6ff54750b8bb900ff8
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2017-11-13 18:09:26 -0800

    Bug fix

--- betterrandom/src/main/java/io/github/pr0methean/betterrandom/seed/RandomSeederThread.java
+++ betterrandom/src/main/java/io/github/pr0methean/betterrandom/seed/RandomSeederThread.java
@@ -233,6 +233,8 @@ public final class RandomSeederThread extends LooperThread {
         LOG.error("%s", t);
         LOG.logStackTrace(Level.SEVERE, t.getStackTrace());
         interrupt();
+        // Must lock again before returning, so we can notify conditions
+        lock.lock();
         return false;
       }
     }
