53c15c0cc60c891b0cb6262c7c1ad0b7a8d7719e
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2017-11-20 23:41:56 +0000

    Use submodule for mutation report output

--- mutation.sh
+++ mutation.sh
@@ -7,9 +7,10 @@ unset RANDOM_DOT_ORG_KEY # Would probably exhaust even our 5Mbit/day limit
 mvn compile test-compile org.pitest:pitest-maven:mutationCoverage
 cd ..
 git checkout https://github.com/Pr0methean/pr0methean.github.io docs
-rm -rf docs/betterrandom-pit-reports
-mv betterrandom/target/pit-reports docs/betterrandom-pit-reports
 cd docs
+git pull
+rm -rf docs/betterrandom-pit-reports
+mv ../betterrandom/target/pit-reports betterrandom-pit-reports
 git add betterrandom-pit-reports
 git commit -m "Update PIT mutation reports"
 git remote add originauth "https://${GH_TOKEN}@github.com/Pr0methean/betterrandom-coverage.git"
