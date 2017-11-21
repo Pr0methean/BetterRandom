e710ad47b8c768f258079ee46d8b0c1ce3ce4212
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2017-11-21 19:44:44 +0000

    Bug fix in mutation-test reporting

--- mutation.sh
+++ mutation.sh
@@ -5,9 +5,8 @@ if ([ "$TRAVIS_JDK_VERSION" = "oraclejdk9" ] || [ "$TRAVIS_JDK_VERSION" = "openj
 fi
 unset RANDOM_DOT_ORG_KEY # Would probably exhaust even our 5Mbit/day limit
 mvn compile test-compile org.pitest:pitest-maven:mutationCoverage
-cd ..
-git checkout https://github.com/Pr0methean/pr0methean.github.io docs
-cd docs
+cd ../docs
+git pull
 rm -rf docs/betterrandom-pit-reports
 mv ../betterrandom/target/pit-reports betterrandom-pit-reports
 git add betterrandom-pit-reports
@@ -15,6 +14,6 @@ git commit -m "Update PIT mutation reports"
 git remote add originauth "https://${GH_TOKEN}@github.com/Pr0methean/betterrandom-coverage.git"
 git push --set-upstream originauth master
 while [ ! $? ]; do
-  git pull --rebase  # Merge
+  git pull --rebase # Merge
   git push
 done
