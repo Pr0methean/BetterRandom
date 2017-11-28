145e3e3ff3cbd072fb14be8dec01f90842080384
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2017-11-28 13:22:45 -0800

    Bugfix for mutation reports

--- mutation.sh
+++ mutation.sh
@@ -6,14 +6,14 @@ fi
 unset RANDOM_DOT_ORG_KEY # Would probably exhaust even our 5Mbit/day limit
 mvn compile test-compile org.pitest:pitest-maven:mutationCoverage
 cd ../docs
-git remote add originauth "https://${GH_TOKEN}@github.com/Pr0methean/betterrandom-coverage.git"
-git branch --set-upstream-to originauth/master
-git pull --rebase
+git remote add originauth "https://${GH_TOKEN}@github.com/Pr0methean/pr0methean.github.io.git"
+git pull --rebase originauth master
+git checkout originauth/master
 rm -rf docs/betterrandom-pit-reports
 mv ../betterrandom/target/pit-reports betterrandom-pit-reports
 git add betterrandom-pit-reports
 git commit -m "Update PIT mutation reports"
-git push
+git push originauth HEAD:master
 while [ ! $? ]; do
   git pull --rebase # Merge
   git push
