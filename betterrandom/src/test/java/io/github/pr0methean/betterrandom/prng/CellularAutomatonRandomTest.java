d54a9b0d260524daa6f9556ac1f13172208de4a3
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2017-11-29 22:39:39 -0800

    Strengthen testSetSeedLong where appropriate

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/CellularAutomatonRandomTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/CellularAutomatonRandomTest.java
@@ -15,13 +15,25 @@
 // ============================================================================
 package io.github.pr0methean.betterrandom.prng;
 
+import io.github.pr0methean.betterrandom.seed.DefaultSeedGenerator;
 import io.github.pr0methean.betterrandom.seed.SeedException;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for the cellular automaton RNG.
  * @author Daniel Dyer
  */
 public class CellularAutomatonRandomTest extends BaseRandomTest {
+
+  @Override public void testSetSeedLong() throws SeedException {
+    final BaseRandom rng = createRng();
+    final BaseRandom rng2 = createRng();
+    rng.nextLong(); // ensure they won't both be in initial state before reseeding
+    rng.setSeed(0x0123456789ABCDEFL);
+    rng2.setSeed(0x0123456789ABCDEFL);
+    assert RandomTestUtils.testEquivalence(rng, rng2, 20)
+        : "Output mismatch after reseeding with same seed";
+  }
 
   @Override protected Class<? extends BaseRandom> getClassUnderTest() {
     return CellularAutomatonRandom.class;
--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/Pcg64RandomTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/Pcg64RandomTest.java
@@ -1,5 +1,6 @@
 package io.github.pr0methean.betterrandom.prng;
 
+import io.github.pr0methean.betterrandom.seed.SeedException;
 import org.testng.annotations.Test;
 
 public class Pcg64RandomTest extends BaseRandomTest {
@@ -31,6 +32,16 @@ public class Pcg64RandomTest extends BaseRandomTest {
     Pcg64Random copy2 = createRng(copy1.getSeed());
     copy2.advance(0);
     RandomTestUtils.testEquivalence(copy1, copy2, 20);
+  }
+
+  @Override public void testSetSeedLong() throws SeedException {
+    final BaseRandom rng = createRng();
+    final BaseRandom rng2 = createRng();
+    rng.nextLong(); // ensure they won't both be in initial state before reseeding
+    rng.setSeed(0x0123456789ABCDEFL);
+    rng2.setSeed(0x0123456789ABCDEFL);
+    assert RandomTestUtils.testEquivalence(rng, rng2, 20)
+        : "Output mismatch after reseeding with same seed";
   }
 
   @Override protected Class<? extends BaseRandom> getClassUnderTest() {
