e00813ab0c1bdcff3e5a404092f9a65b890066dd
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2017-11-30 06:06:18 -0800

    Test of allowing mutation tests to call random.org

--- mutation.sh
+++ mutation.sh
@@ -3,7 +3,6 @@ cd betterrandom
 if ([ "$TRAVIS_JDK_VERSION" = "oraclejdk9" ] || [ "$TRAVIS_JDK_VERSION" = "openjdk9" ]); then
   mv pom9.xml pom.xml
 fi
-unset RANDOM_DOT_ORG_KEY # Would probably exhaust even our 5Mbit/day limit
 mvn compile test-compile org.pitest:pitest-maven:mutationCoverage
 cd ../docs
 git remote add originauth "https://${GH_TOKEN}@github.com/Pr0methean/pr0methean.github.io.git"
