59dae4be9dde27dc0d9f178949dff2025629cbb1
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2017-12-04 01:49:16 +0000

    Move Dieharder on Travis to Java 9 Should improve performance

--- dieharder.sh
+++ dieharder.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 cd betterrandom
+if ([ "$TRAVIS_JDK_VERSION" = "oraclejdk9" ] || [ "$TRAVIS_JDK_VERSION" = "openjdk9" ]); then
+  mv pom9.xml pom.xml
+fi
 mvn -DskipTests -Darguments=-DskipTests -Dmaven.test.skip=true clean package install
 cd ../FifoFiller
 mvn package
