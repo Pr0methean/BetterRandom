6800c8c71a8a76706214a7716e83aeba48b660c2
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-05-03 22:03:05 -0700

    Revert "Test an alternative solution"
    This reverts commit 16c000199d325b03573bc9a60a8a10902954eb1b.

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 if [ "$ANDROID" = 1 ]; then
   MAYBE_ANDROID_FLAG="-Pandroid"
 else
@@ -11,9 +11,11 @@ cd betterrandom
 if [ "$TRAVIS_JDK_VERSION" = "oraclejdk9" ]; then
   mv pom9.xml pom.xml
 fi
-mvn -DskipTests -Darguments=-DskipTests -Dmaven.test.skip=true ${MAYBE_ANDROID_FLAG} clean package install &&\
+# Remove git from path (causes conflicts), based on https://stackoverflow.com/a/370192
+NO_GIT_PATH=`echo "${PATH}" | awk -v RS=':' -v ORS=':' '/git/ {next} {print}'`
+PATH="${NO_GIT_PATH}" mvn -DskipTests -Darguments=-DskipTests -Dmaven.test.skip=true ${MAYBE_ANDROID_FLAG} clean package install &&\
 cd ../benchmark &&\
-mvn -DskipTests ${MAYBE_ANDROID_FLAG} package &&\
+PATH="${NO_GIT_PATH}" mvn -DskipTests ${MAYBE_ANDROID_FLAG} package &&\
 cd target &&\
 if [ "$TRAVIS" = "true" ]; then
     java -jar benchmarks.jar -f 1 -t 1 -foe true &&\
