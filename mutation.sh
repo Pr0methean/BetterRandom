d6e5856ab443326558d91d3ab3e7df5818e4eafa
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-05-21 00:30:14 -0700

    Skip updating PIT reports from cron builds

--- mutation.sh
+++ mutation.sh
@@ -7,6 +7,10 @@ mvn compile test-compile org.pitest:pitest-maven:mutationCoverage
 if [ ! $? ]; then
   exit $?
 fi
+if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
+  # Do not update reports from cron builds
+  exit
+fi
 cd ../docs
 git remote add originauth "https://${GH_TOKEN}@github.com/Pr0methean/pr0methean.github.io.git"
 git pull --rebase originauth master
