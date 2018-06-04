ce52e765aee8ae06f8aba686e37b84b579a4916f
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-06-03 22:35:11 -0700

    Revert "Skip updating PIT reports from cron builds"
    This reverts commit 3abbf992d92b95cfd387187821f767a88f1fec08.

--- mutation.sh
+++ mutation.sh
@@ -7,10 +7,6 @@ mvn clean compile test-compile org.pitest:pitest-maven:mutationCoverage
 if [ ! $? ]; then
   exit 1
 fi
-if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
-  # Do not update reports from cron builds
-  exit
-fi
 cd ../docs
 git remote add originauth "https://${GH_TOKEN}@github.com/Pr0methean/pr0methean.github.io.git"
 git pull --rebase originauth master
