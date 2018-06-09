b53e5ff0ea92ef1ff389cd1ee0a7c099b4b3b423
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-06-09 11:55:56 -0700

    Bug fixes
    oraclejdk11-ea doesn't exist after all. Surefire and remove_unlimited_crypto needed updating.

--- remove_unlimited_crypto.sh
+++ remove_unlimited_crypto.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-if ([ "$TRAVIS_OS_NAME" != "osx" ] && [ "$TRAVIS_JDK_VERSION" != "oraclejdk9" ]); then
-  sudo mv -f travis-resources/local_policy.jar ${JAVA_HOME}/jre/lib/security/local_policy.jar
-  sudo mv -f travis-resources/US_export_policy.jar ${JAVA_HOME}/jre/lib/security/US_export_policy.jar
+if [ "$TRAVIS_OS_NAME" != "osx" ]; then
+  # || true ignores errors, because ${JAVA_HOME}/jre/lib/security isn't in all JDK versions
+  sudo mv -f travis-resources/local_policy.jar ${JAVA_HOME}/jre/lib/security/local_policy.jar || true
+  sudo mv -f travis-resources/US_export_policy.jar ${JAVA_HOME}/jre/lib/security/US_export_policy.jar || true
 fi
