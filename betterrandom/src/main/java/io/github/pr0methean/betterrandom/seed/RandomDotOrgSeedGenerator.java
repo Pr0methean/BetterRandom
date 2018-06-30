f0a36f85936d3c759dee9a84adf0834c5645a709
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-06-30 14:50:06 -0700

    Merge two static blocks

--- betterrandom/src/main/java/io/github/pr0methean/betterrandom/seed/RandomDotOrgSeedGenerator.java
+++ betterrandom/src/main/java/io/github/pr0methean/betterrandom/seed/RandomDotOrgSeedGenerator.java
@@ -121,9 +121,6 @@ public enum RandomDotOrgSeedGenerator implements SeedGenerator {
 
   static {
     earliestNextAttempt.add(YEAR, -1);
-  }
-
-  static {
     try {
       JSON_REQUEST_URL = new URL("https://api.random.org/json-rpc/1/invoke");
     } catch (MalformedURLException e) {
