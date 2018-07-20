68bd5d3642c8d95150e216f35f656c30b7c9837d
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-07-20 16:59:21 -0700

    Bug fix

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/seed/SemiFakeSeedGenerator.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/seed/SemiFakeSeedGenerator.java
@@ -1,5 +1,6 @@
 package io.github.pr0methean.betterrandom.seed;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class SemiFakeSeedGenerator implements SeedGenerator {
@@ -14,5 +15,17 @@ public class SemiFakeSeedGenerator implements SeedGenerator {
   @Override
   public void generateSeed(byte[] output) throws SeedException {
     random.nextBytes(output);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    return this == o
+        || (o instanceof SemiFakeSeedGenerator
+            && random.equals(o.random));
+  }
+
+  @Override
+  public int hashCode() {
+    return random.hashCode();
   }
 }
