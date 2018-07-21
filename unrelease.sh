171333850e03eb8e31e9ef2b96fc0738044fbbbd
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-07-21 05:57:15 +0000

    Bug fix in unrelease.sh git add fails *entirely* if *any* file is missing, so split it

--- unrelease.sh
+++ unrelease.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mv betterrandom/pom.xml.releaseBackup betterrandom/pom.xml
 rm betterrandom/release.properties
-git add betterrandom/pom.xml betterrandom/release.properties
+git add betterrandom/pom.xml
+git add betterrandom/release.properties
 git commit -m "🤖 Roll back version increment from failed release"
