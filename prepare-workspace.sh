e8eeb4e6a6a7229edbe1d739c89e8d9b4f153bdd
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-07-21 07:39:14 +0000

    Install dieharder in prepare-workspace.sh

--- prepare-workspace.sh
+++ prepare-workspace.sh
@@ -2,7 +2,7 @@
 sudo apt-get -y remove maven2
 sudo apt-add-repository ppa:webupd8team/java
 sudo apt-get -y update
-sudo apt-get -y install maven markdown oracle-java8-installer
+sudo apt-get -y install maven markdown oracle-java8-installer dieharder
 sudo apt-get -y autoremove
 gem install github-markup
 gem install commonmarker
