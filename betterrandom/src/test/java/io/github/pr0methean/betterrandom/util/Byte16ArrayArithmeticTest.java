ef9375891245ec61d43499ac91eccbe23836182b
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-07-22 13:43:45 -0700

    Rewrite tests
    No longer depends on within-limb endianness

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/util/Byte16ArrayArithmeticTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/util/Byte16ArrayArithmeticTest.java
@@ -1,7 +1,5 @@
 package io.github.pr0methean.betterrandom.util;
 
-import static io.github.pr0methean.betterrandom.util.BinaryUtils.convertBytesToHexString;
-import static io.github.pr0methean.betterrandom.util.BinaryUtils.convertHexStringToBytes;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotSame;
 import static org.testng.Assert.assertTrue;
@@ -13,68 +11,84 @@ import org.testng.annotations.Test;
 @SuppressWarnings("SpellCheckingInspection")
 public class Byte16ArrayArithmeticTest {
 
-  private static final byte[] OPERAND1 =
-      convertHexStringToBytes("79EC964A738B2EBA38FD07E7D607B6ED");
+  private static ByteBuffer longsToByteBuffer(long most, long least) {
+    ByteBuffer out = ByteBuffer.wrap(new byte[16]);
+    out.putLong(0, most);
+    out.putLong(1, least);
+    return out;
+  }
 
-  private static final byte[] OPERAND2 =
-      convertHexStringToBytes("C26B1E45A661872B631BC1882D24D6E9");
+  private static ByteBuffer cloneByteBuffer(ByteBuffer original) {
+    return ByteBuffer.wrap(original.array().clone());
+  }
 
-  private static void assertArrayEqualsHex(byte[] array, String hex) {
-    assertEquals(convertBytesToHexString(array), hex);
+  private static final ByteBuffer OPERAND1 =
+      longsToByteBuffer(0x79EC_964A_738B_2EBAL, 0x38FD_07E7_D607_B6EDL);
+
+  private static final ByteBuffer OPERAND2 =
+      longsToByteBuffer(0xC26B_1E45_A661_872BL, 0x631B_C188_2D24_D6E9L);
+
+  private static void assertByteBufferEqualsLongs(
+      ByteBuffer actual, long expectedMost, long expectedLeast) {
+    String message = String.format("Expected %16x%16x, got %s",
+        expectedMost, expectedLeast, BinaryUtils.convertBytesToHexString(actual.array()));
+    assertEquals(actual.getLong(0), expectedMost, message);
+    assertEquals(actual.getLong(1), expectedLeast, message);
   }
 
   @Test public void testAddInto() {
-    byte[] result = OPERAND1.clone();
-    Byte16ArrayArithmetic.addInto(ByteBuffer.wrap(result), ByteBuffer.wrap(OPERAND2));
-    assertArrayEqualsHex(result, "3C57B49019ECB5E59C18C970032C8DD6");
+    ByteBuffer result = cloneByteBuffer(OPERAND1);
+    Byte16ArrayArithmetic.addInto(result, OPERAND2);
+    assertByteBufferEqualsLongs(result, 0x3C57_B490_19EC_B5E5L, 0x9C18_C970_032C_8DD6L);
   }
 
   @Test public void testAddInto1() {
-    byte[] result = OPERAND1.clone();
-    Byte16ArrayArithmetic.addInto(ByteBuffer.wrap(result), 0x631bc1882d24d6e9L);
-    assertArrayEqualsHex(result, "79EC964A738B2EBA9C18C970032C8DD6");
+    ByteBuffer result = cloneByteBuffer(OPERAND1);
+    Byte16ArrayArithmetic.addInto(result, 0x631bc1882d24d6e9L);
+    assertByteBufferEqualsLongs(result, 0x79EC_964A_738B_2EBAL, 0x9C18_C970_032C_8DD6L);
   }
 
   @Test public void testMultiplyInto() {
-    byte[] result = OPERAND1.clone();
-    Byte16ArrayArithmetic.multiplyInto(ByteBuffer.wrap(result), ByteBuffer.wrap(OPERAND2));
-    assertArrayEqualsHex(result, "347795005A1BEFFCB7224E11A2439BB5");
+    ByteBuffer result = cloneByteBuffer(OPERAND1);
+    Byte16ArrayArithmetic.multiplyInto(result, OPERAND2);
+    assertByteBufferEqualsLongs(result, 0x3477_9500_5A1B_EFFCL, 0xB722_4E11_A243_9BB5L);
   }
 
   @Test public void testUnsignedShiftRight() {
-    byte[] result = OPERAND1.clone();
-    Byte16ArrayArithmetic.unsignedShiftRight(ByteBuffer.wrap(result), 12);
-    assertArrayEqualsHex(result, "00079EC964A738B2EBA38FD07E7D607B");
+    ByteBuffer result = cloneByteBuffer(OPERAND1);
+    Byte16ArrayArithmetic.unsignedShiftRight(result, 12);
+    assertByteBufferEqualsLongs(result, 0x0007_9EC9_64A7_38B2L, 0xEBA3_8FD0_7E7D_607BL);
   }
 
   @Test public void testUnsignedShiftRightOffEdge() {
-    byte[] result = OPERAND1.clone();
-    Byte16ArrayArithmetic.unsignedShiftRight(ByteBuffer.wrap(result), 128);
-    assertTrue(Arrays.equals(result, Byte16ArrayArithmetic.ZERO));
+    ByteBuffer result = cloneByteBuffer(OPERAND1);
+    Byte16ArrayArithmetic.unsignedShiftRight(result, 128);
+    assertByteBufferEqualsLongs(result, 0, 0);
   }
 
   @Test public void testUnsignedShiftLeft() {
-    byte[] result = OPERAND1.clone();
-    Byte16ArrayArithmetic.unsignedShiftLeft(ByteBuffer.wrap(result), 12);
-    assertArrayEqualsHex(result, "C964A738B2EBA38FD07E7D607B6ED000");
+    ByteBuffer result = cloneByteBuffer(OPERAND1);
+    Byte16ArrayArithmetic.unsignedShiftLeft(result, 12);
+    assertByteBufferEqualsLongs(result, 0xC964_A738_B2EB_A38FL, 0xD07E_7D60_7B6E_D000L);
   }
 
   @Test public void testUnsignedShiftLeftOffEdge() {
-    byte[] result = OPERAND1.clone();
-    Byte16ArrayArithmetic.unsignedShiftLeft(ByteBuffer.wrap(result), 128);
-    assertTrue(Arrays.equals(result, Byte16ArrayArithmetic.ZERO));
+    ByteBuffer result = cloneByteBuffer(OPERAND1);
+    Byte16ArrayArithmetic.unsignedShiftLeft(result, 128);
+    assertByteBufferEqualsLongs(result, 0, 0);
   }
 
   @Test public void testXorInto() {
-    byte[] result = OPERAND1.clone();
-    Byte16ArrayArithmetic.xorInto(ByteBuffer.wrap(result), ByteBuffer.wrap(OPERAND2));
-    assertArrayEqualsHex(result, "BB87880FD5EAA9915BE6C66FFB236004");
+    ByteBuffer result = cloneByteBuffer(OPERAND1);
+    Byte16ArrayArithmetic.xorInto(result, OPERAND2);
+    assertByteBufferEqualsLongs(result, 0xBB87_880F_D5EA_A991L, 0x5BE6_C66F_FB23_6004L);
   }
 
   @Test public void testCopyInto() {
     ThreadLocal<ByteBuffer> threadLocal = Byte16ArrayArithmetic.makeByteArrayThreadLocal();
-    byte[] copyOfOperand1 = Byte16ArrayArithmetic.copyInto(threadLocal, OPERAND1).array();
-    assertTrue(Arrays.equals(copyOfOperand1, OPERAND1));
-    assertNotSame(copyOfOperand1, OPERAND1);
+    final byte[] operand1Array = OPERAND1.array();
+    byte[] copyOfOperand1 = Byte16ArrayArithmetic.copyInto(threadLocal, operand1Array).array();
+    assertTrue(Arrays.equals(copyOfOperand1, operand1Array));
+    assertNotSame(copyOfOperand1, operand1Array);
   }
 }
