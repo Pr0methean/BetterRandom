55825756fb8b7e151a86b7d0552309ad12cceddd
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-07-21 17:12:56 -0700

    Revert "Bug fix?"
    This reverts commit 7316811429d81749fb501713c90022968ebc0d83.

--- unit-tests.sh
+++ unit-tests.sh
@@ -85,7 +85,7 @@ if [ "${STATUS}" = 0 ] && [ "${NO_JACOCO}" != "true" ]; then
 fi
 if [ "${JAVA8}" = "true" ]; then
   echo "[unit-tests.sh] Running Proguard and testing against Proguarded jar."
-  PATH="${NO_GIT_PATH}" mvn ${MAYBE_ANDROID_FLAG} integration-test
+  PATH="${NO_GIT_PATH}" mvn ${MAYBE_ANDROID_FLAG} pre-integration-test integration-test -e
   STATUS=$?
 fi
 cd ..
