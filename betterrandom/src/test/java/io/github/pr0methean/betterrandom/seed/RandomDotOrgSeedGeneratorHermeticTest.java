1df13661ad4bebc2e3c16757d940ab8314932890
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-07-26 05:22:35 -0700

    Revert "Fix AppVeyor timeout"
    This reverts commit aa6b092c9a182717384203ec835dbd18419d1c1a.

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/seed/RandomDotOrgSeedGeneratorHermeticTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/seed/RandomDotOrgSeedGeneratorHermeticTest.java
@@ -25,11 +25,8 @@ import org.powermock.core.classloader.annotations.MockPolicy;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.testng.PowerMockTestCase;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -111,11 +108,6 @@ public class RandomDotOrgSeedGeneratorHermeticTest extends PowerMockTestCase {
     return exception;
   }
 
-  @BeforeClass public void setUpClass() {
-    // Ensuring the logger exists before testing prevents a timeout on AppVeyor.
-    LoggerFactory.getLogger(RandomDotOrgSeedGenerator.class);
-  }
-  
   @BeforeMethod public void setUpMethod() {
     spy(RandomDotOrgSeedGenerator.class);
     usingSmallRequests = maybeSetMaxRequestSize();
