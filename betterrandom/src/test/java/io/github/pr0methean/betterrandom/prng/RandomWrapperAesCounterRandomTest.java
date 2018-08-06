426d734a59f60d3e2234cf1ea0f4def168166a57
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-08-05 17:10:55 -0700

    Fix another DEFAULT_SEED_GENERATOR usage

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/RandomWrapperAesCounterRandomTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/RandomWrapperAesCounterRandomTest.java
@@ -8,8 +8,8 @@ import org.testng.annotations.Test;
 @Test(testName = "RandomWrapper:AesCounterRandom")
 public class RandomWrapperAesCounterRandomTest extends AesCounterRandomTest {
 
-  private static final NamedFunction<Random, Double> SET_WRAPPED = new NamedFunction<>(random -> {
-    ((RandomWrapper) random).setWrapped(new AesCounterRandom());
+  private final NamedFunction<Random, Double> setWrapped = new NamedFunction<>(random -> {
+    ((RandomWrapper) random).setWrapped(new AesCounterRandom(getTestSeedGenerator()));
     return 0.0;
   }, "setWrapped");
 
@@ -19,7 +19,7 @@ public class RandomWrapperAesCounterRandomTest extends AesCounterRandomTest {
 
   @Override @Test public void testThreadSafetySetSeed() {
     testThreadSafetyVsCrashesOnly(30,
-        ImmutableList.of(NEXT_LONG, NEXT_INT, NEXT_DOUBLE, NEXT_GAUSSIAN, setSeed, SET_WRAPPED));
+        ImmutableList.of(NEXT_LONG, NEXT_INT, NEXT_DOUBLE, NEXT_GAUSSIAN, setSeed, setWrapped));
   }
 
   @Override @Test(enabled = false) public void testAdvanceForward() {
