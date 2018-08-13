10710f2ac8ea642db955c485d3c17b1eab91f57a
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-08-13 11:28:34 -0700

    Use SecureRandomSeedGenerator for all benchmark init

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/RandomWrapperBenchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/RandomWrapperBenchmark.java
@@ -1,12 +1,13 @@
 package io.github.pr0methean.betterrandom.benchmark;
 
 import io.github.pr0methean.betterrandom.prng.RandomWrapper;
+import io.github.pr0methean.betterrandom.seed.SecureRandomSeedGenerator;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import java.util.Random;
 
 public class RandomWrapperBenchmark extends AbstractRandomBenchmarkWithReseeding {
 
   @Override protected Random createPrng() throws SeedException {
-    return new RandomWrapper();
+    return new RandomWrapper(SecureRandomSeedGenerator.SECURE_RANDOM_SEED_GENERATOR);
   }
 }
--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/ReseedingSplittableRandomAdapterBenchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/ReseedingSplittableRandomAdapterBenchmark.java
@@ -1,5 +1,7 @@
 package io.github.pr0methean.betterrandom.benchmark;
 
+import static io.github.pr0methean.betterrandom.seed.SecureRandomSeedGenerator.SECURE_RANDOM_SEED_GENERATOR;
+
 import io.github.pr0methean.betterrandom.prng.adapter.ReseedingSplittableRandomAdapter;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import java.util.Random;
@@ -7,6 +9,6 @@ import java.util.Random;
 public class ReseedingSplittableRandomAdapterBenchmark extends AbstractRandomBenchmark {
 
   @Override protected Random createPrng() throws SeedException {
-    return ReseedingSplittableRandomAdapter.getDefaultInstance();
+    return ReseedingSplittableRandomAdapter.getInstance(SECURE_RANDOM_SEED_GENERATOR);
   }
 }
