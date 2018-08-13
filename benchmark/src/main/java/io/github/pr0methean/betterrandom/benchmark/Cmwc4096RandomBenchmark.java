1bdb088ad8a6d4f030984909aa4f58b72f82b795
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-08-13 18:20:43 +0000

    Remove DEFAULT_SEED_GENERATOR from Cmwc4096 benchmark

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/Cmwc4096RandomBenchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/Cmwc4096RandomBenchmark.java
@@ -1,12 +1,13 @@
 package io.github.pr0methean.betterrandom.benchmark;
 
 import io.github.pr0methean.betterrandom.prng.Cmwc4096Random;
+import io.github.pr0methean.betterrandom.seed.SecureRandomSeedGenerator;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import java.util.Random;
 
 public class Cmwc4096RandomBenchmark extends AbstractRandomBenchmarkWithReseeding {
 
   @Override protected Random createPrng() throws SeedException {
-    return new Cmwc4096Random();
+    return new Cmwc4096Random(SecureRandomSeedGenerator.SECURE_RANDOM_SEED_GENERATOR);
   }
 }
