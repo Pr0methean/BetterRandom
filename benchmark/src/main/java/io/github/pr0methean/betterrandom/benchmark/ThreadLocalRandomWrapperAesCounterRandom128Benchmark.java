3c59860d5e290fd80366d5a51ec96a1e62c60db5
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-08-13 11:28:34 -0700

    Use SecureRandomSeedGenerator for all benchmark init

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/ThreadLocalRandomWrapperAesCounterRandom128Benchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/ThreadLocalRandomWrapperAesCounterRandom128Benchmark.java
@@ -1,15 +1,16 @@
 package io.github.pr0methean.betterrandom.benchmark;
 
+import static io.github.pr0methean.betterrandom.seed.SecureRandomSeedGenerator.SECURE_RANDOM_SEED_GENERATOR;
+
 import io.github.pr0methean.betterrandom.prng.AesCounterRandom;
 import io.github.pr0methean.betterrandom.prng.ThreadLocalRandomWrapper;
-import io.github.pr0methean.betterrandom.seed.DefaultSeedGenerator;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import java.util.Random;
 
 public class ThreadLocalRandomWrapperAesCounterRandom128Benchmark extends AbstractRandomBenchmark {
 
   @Override protected Random createPrng() throws SeedException {
-    return new ThreadLocalRandomWrapper(16, DefaultSeedGenerator.DEFAULT_SEED_GENERATOR,
+    return new ThreadLocalRandomWrapper(16, SECURE_RANDOM_SEED_GENERATOR,
         AesCounterRandom::new);
   }
 }
