32ff724f44e1c05f8e362bb5126c01d19773980e
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-08-14 05:33:54 +0000

    Bug fix

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 cd betterrandom
 if [ "$#" -ge 1 ]; then
   MAYBE_P="-P"
-  MAYBE_RELEASE="release"
+  MAYBE_RELEASE="release-sign-artifacts"
   OLDVERSION=$(mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }' | sed 's/version=//')
 else
   MAYBE_P=""
@@ -23,12 +23,20 @@ rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
   fi
   mvn -DskipTests -Darguments=-DskipTests -Dmaven.test.skip=true -P release-sign-artifacts \
       clean compile pre-integration-test deploy ${MAYBE_P} ${MAYBE_RELEASE}
-  if [ ! $? ]; then
+  if [ $? ]; then
+    NEWVERSION=$(mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }' | sed 's/version=//')
+    sed -i "s/$1/${NEWVERSION}/" ../benchmark/pom.xml
+    sed -i "s/$1/${NEWVERSION}/" ../FifoFiller/pom.xml
+    git add pom.xml
+    git add ../benchmark/pom.xml
+    git add ../FifoFiller/pom.xml
+    git commit -m "🤖 Update version numbers"
+  else
     if [ "$#" -ge 1 ]; then
       git tag -d "BetterRandom-$1"
       git push --delete origin "BetterRandom-$1"
       git revert --no-edit ${VERSION_COMMIT}
     fi
   fi
-)
+) && git push
 cd ..
