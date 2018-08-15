ee342f9ac8714d3a7feaa11d6173efc7a86a8161
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-08-15 01:27:08 -0700

    Bug fixes

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/RandomWrapperSecureRandomBenchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/RandomWrapperSecureRandomBenchmark.java
@@ -1,13 +1,12 @@
 package io.github.pr0methean.betterrandom.benchmark;
 
 import io.github.pr0methean.betterrandom.prng.RandomWrapper;
-import io.github.pr0methean.betterrandom.seed.SeedException;
 import java.security.SecureRandom;
 import java.util.Random;
 
 public class RandomWrapperSecureRandomBenchmark extends AbstractRandomBenchmark {
 
-  @Override protected Random createPrng() throws SeedException {
+  @Override protected Random createPrng() throws Exception {
     return new RandomWrapper(SecureRandom.getInstance("SHA1PRNG"));
   }
 }
