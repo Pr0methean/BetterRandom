4c981d03d8030478792be283a7064e40690c2c6a
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-08-18 16:26:54 -0700

    Bug fixes

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/ReseedingThreadLocalRandomWrapperFailingSeedGeneratorTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/ReseedingThreadLocalRandomWrapperFailingSeedGeneratorTest.java
@@ -5,6 +5,7 @@ import io.github.pr0methean.betterrandom.seed.FailingSeedGenerator;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import io.github.pr0methean.betterrandom.seed.SeedGenerator;
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
 import java.util.function.Supplier;
 import org.testng.annotations.Test;
 
@@ -15,6 +16,36 @@ public class ReseedingThreadLocalRandomWrapperFailingSeedGeneratorTest
   public ReseedingThreadLocalRandomWrapperFailingSeedGeneratorTest() {
     pcgSupplier = (Serializable & Supplier<BaseRandom>)
         () -> new Pcg64Random(semiFakeSeedGenerator);
+  }
+
+  @Override @Test(enabled = false) public void testWrapLegacy() throws SeedException {
+    // No-op.
+  }
+
+  @Override @Test(enabled = false) public void testSetSeedAfterNextLong() throws SeedException {
+    // No-op.
+  }
+
+  @Override @Test(enabled = false) public void testSetSeedAfterNextInt() throws SeedException {
+    // No-op.
+  }
+
+  @Override @Test(enabled = false) public void testSerializable() throws SeedException {
+    // No-op.
+  }
+
+  @Override @Test(enabled = false) public void testSeedTooLong() throws SeedException {
+    // No-op.
+  }
+
+  @Override @Test(enabled = false) public void testThreadSafety() {
+    // No-op.
+  }
+
+  @Override @Test(enabled = false) public void testAllPublicConstructors()
+      throws SeedException, IllegalAccessException, InstantiationException,
+      InvocationTargetException {
+    // No-op.
   }
 
   @Override protected EntropyCheckMode getEntropyCheckMode() {
