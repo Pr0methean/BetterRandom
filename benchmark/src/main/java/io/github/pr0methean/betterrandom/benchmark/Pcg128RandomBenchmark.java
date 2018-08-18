865d01a6328f101e0dac86be2add44adf47da108
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-08-18 10:44:49 -0700

    Fix an import

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/Pcg128RandomBenchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/Pcg128RandomBenchmark.java
@@ -1,6 +1,6 @@
 package io.github.pr0methean.betterrandom.benchmark;
 
-import io.github.pr0methean.betterrandom.prng.Pcg64Random;
+import io.github.pr0methean.betterrandom.prng.Pcg128Random;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import java.util.Random;
 
