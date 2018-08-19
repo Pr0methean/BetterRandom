d0875b4c58f8943e042946c4deb467c6b331249e
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-08-18 22:00:03 -0700

    Eliminate some arraycopy calls

--- betterrandom/src/main/java/io/github/pr0methean/betterrandom/prng/adapter/DirectSplittableRandomAdapter.java
+++ betterrandom/src/main/java/io/github/pr0methean/betterrandom/prng/adapter/DirectSplittableRandomAdapter.java
@@ -43,6 +43,10 @@ public abstract class DirectSplittableRandomAdapter extends BaseSplittableRandom
     setSeed(seed);
   }
 
+  @Override protected boolean usesByteBuffer() {
+    return true;
+  }
+
   @Override protected ToStringHelper addSubclassFields(final ToStringHelper original) {
     return original.add("underlying", underlying);
   }
@@ -60,7 +64,7 @@ public abstract class DirectSplittableRandomAdapter extends BaseSplittableRandom
       throw new IllegalArgumentException("DirectSplittableRandomAdapter requires an 8-byte seed");
     }
     super.setSeedInternal(seed);
-    underlying = new SplittableRandom(BinaryUtils.convertBytesToLong(seed));
+    underlying = new SplittableRandom(seedBuffer.getLong(0));
   }
 
   /**
