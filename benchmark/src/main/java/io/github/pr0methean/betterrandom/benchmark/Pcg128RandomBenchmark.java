9a39eb14678010c7b0575824e51315577e46caf7
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-08-30 22:01:25 -0700

    @Fork(0)

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/Pcg128RandomBenchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/Pcg128RandomBenchmark.java
@@ -3,7 +3,9 @@ package io.github.pr0methean.betterrandom.benchmark;
 import io.github.pr0methean.betterrandom.prng.Pcg128Random;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import java.util.Random;
+import org.openjdk.jmh.annotations.Fork;
 
+@Fork(0)
 public class Pcg128RandomBenchmark extends AbstractRandomBenchmarkWithReseeding {
 
   @Override protected Random createPrng() throws SeedException {
