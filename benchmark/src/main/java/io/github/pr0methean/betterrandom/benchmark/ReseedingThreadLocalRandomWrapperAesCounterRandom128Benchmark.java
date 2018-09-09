d481b1dde1a242496c1d7f5e5cdcb61c3a6e75c4
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-09-09 16:14:01 -0700

    ReseedingThreadLocalRandomWrapper benchmark

---
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/ReseedingThreadLocalRandomWrapperAesCounterRandom128Benchmark.java
@@ -0,0 +1,16 @@
+package io.github.pr0methean.betterrandom.benchmark;
+
+import static io.github.pr0methean.betterrandom.seed.SecureRandomSeedGenerator.SECURE_RANDOM_SEED_GENERATOR;
+
+import io.github.pr0methean.betterrandom.prng.AesCounterRandom;
+import io.github.pr0methean.betterrandom.prng.ReseedingThreadLocalRandomWrapper;
+import java.util.Random;
+
+public class ReseedingThreadLocalRandomWrapperAesCounterRandom128Benchmark
+    extends AbstractRandomBenchmark {
+
+  @Override protected Random createPrng() {
+    return new ReseedingThreadLocalRandomWrapper(SECURE_RANDOM_SEED_GENERATOR,
+        () -> new AesCounterRandom(SECURE_RANDOM_SEED_GENERATOR.generateSeed(16)));
+  }
+}
