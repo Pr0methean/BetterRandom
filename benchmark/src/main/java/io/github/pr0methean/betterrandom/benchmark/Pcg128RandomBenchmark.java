875dd48c69f5a6187fad74d424016737508b5cd9
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-09-09 13:39:27 -0700

    Fix benchmark perf issue

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/Pcg128RandomBenchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/Pcg128RandomBenchmark.java
@@ -1,12 +1,13 @@
 package io.github.pr0methean.betterrandom.benchmark;
 
 import io.github.pr0methean.betterrandom.prng.Pcg128Random;
+import io.github.pr0methean.betterrandom.seed.SecureRandomSeedGenerator;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import java.util.Random;
 
 public class Pcg128RandomBenchmark extends AbstractRandomBenchmarkWithReseeding {
 
   @Override protected Random createPrng() throws SeedException {
-    return new Pcg128Random();
+    return new Pcg128Random(SecureRandomSeedGenerator.SECURE_RANDOM_SEED_GENERATOR);
   }
 }
