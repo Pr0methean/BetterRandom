65ac346295d84dec1607d5bb342205a25c773f2e
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-09-10 11:24:09 -0700

    Fix Java-8-ism

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/ReseedingThreadLocalRandomWrapperAesCounterRandom128Benchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/ReseedingThreadLocalRandomWrapperAesCounterRandom128Benchmark.java
@@ -5,12 +5,17 @@ import static io.github.pr0methean.betterrandom.seed.SecureRandomSeedGenerator.S
 import io.github.pr0methean.betterrandom.prng.AesCounterRandom;
 import io.github.pr0methean.betterrandom.prng.ReseedingThreadLocalRandomWrapper;
 import java.util.Random;
+import java8.util.function.Supplier;
 
 public class ReseedingThreadLocalRandomWrapperAesCounterRandom128Benchmark
     extends AbstractRandomBenchmark {
 
   @Override protected Random createPrng() {
     return new ReseedingThreadLocalRandomWrapper(SECURE_RANDOM_SEED_GENERATOR,
-        () -> new AesCounterRandom(SECURE_RANDOM_SEED_GENERATOR.generateSeed(16)));
+        new Supplier<AesCounterRandom>() {
+          @Override public AesCounterRandom get() {
+            return new AesCounterRandom(SECURE_RANDOM_SEED_GENERATOR.generateSeed(16));
+          }
+        });
   }
 }
