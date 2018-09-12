16486e1c63fa405a210e60904df84d2cea56bdc2
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-09-11 19:19:01 -0700

    Run Proguard on all Java versions

--- unit-tests.sh
+++ unit-tests.sh
@@ -4,17 +4,8 @@ if [ "${ANDROID}" = 1 ]; then
 else
   MAYBE_ANDROID_FLAG=""
 fi
-if [ "${JAVA8}" = "true" ]; then
-  echo "[unit-tests.sh] Using Java 8 mode. JaCoCo will run."
-  MAYBE_JACOCO_PREPARE="compile jacoco:instrument jacoco:prepare-agent"
-  MAYBE_JACOCO_REPORT="jacoco:restore-instrumented-classes jacoco:report"
-else
-  echo "[unit-tests.sh] Using Java 9+ mode."
-  # https://github.com/jacoco/jacoco/issues/663
-  NO_JACOCO="true"
-  MAYBE_JACOCO_PREPARE=""
-  MAYBE_JACOCO_REPORT=""
-fi
+MAYBE_JACOCO_PREPARE="compile jacoco:instrument jacoco:prepare-agent"
+MAYBE_JACOCO_REPORT="jacoco:restore-instrumented-classes jacoco:report"
 NO_GIT_PATH="${PATH}"
 if [ "${APPVEYOR}" != "" ]; then
   export RANDOM_DOT_ORG_KEY=$(powershell 'Write-Host ($env:random_dot_org_key) -NoNewLine')
@@ -28,7 +19,7 @@ cd betterrandom
 PATH="${NO_GIT_PATH}" mvn ${MAYBE_ANDROID_FLAG} clean ${MAYBE_JACOCO_PREPARE} \
     test ${MAYBE_JACOCO_REPORT} -e
 STATUS=$?
-if [ "${STATUS}" = 0 ] && [ "${NO_JACOCO}" != "true" ]; then
+if [ "${STATUS}" = 0 ]; then
   if [ "${TRAVIS}" = "true" ]; then
     COMMIT="$TRAVIS_COMMIT"
     JOB_ID="travis_$TRAVIS_JOB_NUMBER"
