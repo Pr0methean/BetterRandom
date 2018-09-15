4e4a761bf5c031041eba2495bddc4e2248d95cdb
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-09-15 13:37:13 -0700

    Add test of getWrapped

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/adapter/RandomWrapperRandomTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/adapter/RandomWrapperRandomTest.java
@@ -15,6 +15,8 @@
 // ============================================================================
 package io.github.pr0methean.betterrandom.prng.adapter;
 
+import static org.testng.Assert.assertTrue;
+
 import com.google.common.collect.ImmutableList;
 import io.github.pr0methean.betterrandom.prng.BaseRandom;
 import io.github.pr0methean.betterrandom.prng.BaseRandomTest;
@@ -82,11 +84,15 @@ public class RandomWrapperRandomTest extends BaseRandomTest {
         "Generated sequences do not match.");
   }
 
-  @Override protected BaseRandom createRng() throws SeedException {
+  @Override protected RandomWrapper createRng() throws SeedException {
     return new RandomWrapper(getTestSeedGenerator());
   }
 
-  @Override protected BaseRandom createRng(final byte[] seed) throws SeedException {
+  @Override protected RandomWrapper createRng(final byte[] seed) throws SeedException {
     return new RandomWrapper(seed);
+  }
+
+  @Test public void testGetWrapped() {
+    assertTrue(getClassUnderTest().isInstance(createRng().getWrapped()));
   }
 }
