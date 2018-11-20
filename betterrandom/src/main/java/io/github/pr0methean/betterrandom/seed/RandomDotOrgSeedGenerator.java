e09bdb123200a4288959eab037dd81faca16de21
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-11-20 14:54:17 -0800

    Fix bad merge

--- betterrandom/src/main/java/io/github/pr0methean/betterrandom/seed/RandomDotOrgSeedGenerator.java
+++ betterrandom/src/main/java/io/github/pr0methean/betterrandom/seed/RandomDotOrgSeedGenerator.java
@@ -223,14 +223,6 @@ public enum RandomDotOrgSeedGenerator implements SeedGenerator {
               "Too few bytes returned: expected %d bytes, got '%s'", length, base64seed));
         }
         System.arraycopy(decodedSeed, 0, seed, offset, length);
-        final String base64seed =
-            ((data instanceof JSONArray) ? ((JSONArray) data).get(0) : data).toString();
-        final byte[] decodedSeed = BASE64.decode(base64seed);
-        if (decodedSeed.length < length) {
-          throw new SeedException(String.format(
-              "Too few bytes returned: expected %d bytes, got '%s'", length, base64seed));
-        }
-        System.arraycopy(decodedSeed, 0, seed, offset, length);
         final Object advisoryDelayMs = result.get("advisoryDelay");
         if (advisoryDelayMs instanceof Number) {
           // Wait RETRY_DELAY or the advisory delay, whichever is shorter
