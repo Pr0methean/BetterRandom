1f98fefdf97b8db669d1a72e44378dcbbd33f423
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-11-19 22:30:16 -0800

    BufferedSeedGeneratorTest

---
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/seed/BufferedSeedGeneratorTest.java
@@ -0,0 +1,48 @@
+package io.github.pr0methean.betterrandom.seed;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Test(singleThreaded = true)
+public class BufferedSeedGeneratorTest extends AbstractSeedGeneratorTest {
+
+  private static final FakeSeedGenerator FAKE_SEED_GENERATOR = new FakeSeedGenerator();
+  private static final int BUFFER_SIZE = 256;
+
+  private void generateAndCheckSeed(int length) {
+    byte[] seed = seedGenerator.generateSeed(length);
+    assertEquals(seed.length, length);
+    for (int i = 0; i < length; i++) {
+      assertEquals(seed[i], 1);
+    }
+  }
+
+  public BufferedSeedGeneratorTest() {
+    super(new BufferedSeedGenerator(FAKE_SEED_GENERATOR, BUFFER_SIZE));
+  }
+
+  @BeforeMethod
+  public void setUp() {
+    FAKE_SEED_GENERATOR.resetCalls();
+  }
+
+  @Test
+  public void testLargeRequestDoneAsOne() {
+    generateAndCheckSeed(2 * BUFFER_SIZE);
+    assertEquals(FAKE_SEED_GENERATOR.countCalls(), 1);
+    generateAndCheckSeed(BUFFER_SIZE / 2);
+    assertEquals(FAKE_SEED_GENERATOR.countCalls(), 2);
+  }
+
+  @Test
+  public void testSmallRequests() {
+    generateAndCheckSeed(BUFFER_SIZE / 2);
+    assertEquals(FAKE_SEED_GENERATOR.countCalls(), 1);
+    generateAndCheckSeed(BUFFER_SIZE / 2);
+    assertEquals(FAKE_SEED_GENERATOR.countCalls(), 1);
+    generateAndCheckSeed(BUFFER_SIZE / 2);
+    assertEquals(FAKE_SEED_GENERATOR.countCalls(), 2);
+  }
+}
--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/seed/FakeSeedGenerator.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/seed/FakeSeedGenerator.java
@@ -1,11 +1,14 @@
 package io.github.pr0methean.betterrandom.seed;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class FakeSeedGenerator implements SeedGenerator {
 
   private static final long serialVersionUID = 2310664903337315190L;
   private final String name;
+
+  private final AtomicLong calls = new AtomicLong(0);
 
   public FakeSeedGenerator() {
     this("FakeSeedGenerator");
@@ -20,6 +23,7 @@ public class FakeSeedGenerator implements SeedGenerator {
   }
 
   @Override public void generateSeed(final byte[] output) throws SeedException {
+    calls.incrementAndGet();
     Arrays.fill(output, (byte) 1);
   }
 
@@ -38,5 +42,13 @@ public class FakeSeedGenerator implements SeedGenerator {
   @Override
   public int hashCode() {
     return name.hashCode();
+  }
+
+  public long countCalls() {
+    return calls.get();
+  }
+
+  public void resetCalls() {
+    calls.set(0);
   }
 }
