9febf206617e421bef21f6a05a52f8fa6dbbab6f
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-11-20 17:53:01 -0800

    Formatting fix

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/util/LooperThreadTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/util/LooperThreadTest.java
@@ -38,7 +38,7 @@ public class LooperThreadTest {
       try {
         assertTrue(thread.awaitIteration(1, TimeUnit.SECONDS));
       } catch (final InterruptedException e) {
-        throw new RuntimeException(e);
+        throw new AssertionError(e);
       }
     });
   }
