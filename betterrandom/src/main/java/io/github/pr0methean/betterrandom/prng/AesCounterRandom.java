5e6b5f1857da21393a64d370900bbb3b3fcc34ac
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-12-20 14:07:40 -0800

    Extract out CipherCounterRandom

--- betterrandom/src/main/java/io/github/pr0methean/betterrandom/prng/AesCounterRandom.java
+++ betterrandom/src/main/java/io/github/pr0methean/betterrandom/prng/AesCounterRandom.java
@@ -15,18 +15,15 @@
 // ============================================================================
 package io.github.pr0methean.betterrandom.prng;
 
-import com.google.common.base.MoreObjects.ToStringHelper;
 import io.github.pr0methean.betterrandom.SeekableRandom;
 import io.github.pr0methean.betterrandom.seed.DefaultSeedGenerator;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import io.github.pr0methean.betterrandom.seed.SeedGenerator;
-import io.github.pr0methean.betterrandom.util.BinaryUtils;
 import io.github.pr0methean.betterrandom.util.Byte16ArrayArithmetic;
 import java.security.GeneralSecurityException;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 import java.util.Random;
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
@@ -45,10 +42,11 @@ import org.slf4j.LoggerFactory;
  * @author Daniel Dyer
  * @author Chris Hennick
  */
-public class AesCounterRandom extends BaseRandom implements SeekableRandom {
+public class AesCounterRandom extends CipherCounterRandom implements SeekableRandom {
 
-  private static final long serialVersionUID = 5949778642428995210L;
+  private static final long serialVersionUID = -7872636191973295031L;
   private static final int DEFAULT_SEED_SIZE_BYTES = 32;
+
   /**
    * Theoretically, the Rijndael algorithm supports key sizes and block sizes of 16, 20, 24, 28 & 32
    * bytes. Thus, if Java contained a full implementation of Rijndael, specifying it would let us
@@ -58,6 +56,27 @@ public class AesCounterRandom extends BaseRandom implements SeekableRandom {
   private static final String ALGORITHM = "AES";
   @SuppressWarnings("HardcodedFileSeparator") private static final String ALGORITHM_MODE =
       ALGORITHM + "/ECB/NoPadding";
+
+  @Override
+  public int getCounterSizeBytes() {
+    return COUNTER_SIZE_BYTES;
+  }
+
+  @Override
+  public int getBlocksAtOnce() {
+    return BLOCKS_AT_ONCE;
+  }
+
+  @Override
+  public int getBytesAtOnce() {
+    return BYTES_AT_ONCE;
+  }
+
+  @Override
+  public int getMaxTotalSeedLengthBytes() {
+    return MAX_TOTAL_SEED_LENGTH_BYTES;
+  }
+
   /**
    * 128-bit counter. Package-visible for testing. Note to forkers: when running a cipher in ECB
    * mode, this counter's length should equal the cipher's block size.
@@ -87,17 +106,6 @@ public class AesCounterRandom extends BaseRandom implements SeekableRandom {
     MAX_KEY_LENGTH_BYTES = Math.min(MAX_KEY_LENGTH_BYTES, 32);
     MAX_TOTAL_SEED_LENGTH_BYTES = MAX_KEY_LENGTH_BYTES + COUNTER_SIZE_BYTES;
   }
-
-  private final byte[] currentBlock;
-  // WARNING: Don't initialize any instance fields at declaration; they may be initialized too late!
-  @SuppressWarnings("InstanceVariableMayNotBeInitializedByReadObject") private transient Cipher
-      cipher;
-  private volatile byte[] counter;
-  private volatile byte[] counterInput;
-  private volatile boolean seeded;
-  private volatile int index;
-  private transient MessageDigest hash;
-  private transient byte[] addendDigits;
 
   /**
    * Creates a new RNG and seeds it using 256 bits from the {@link DefaultSeedGenerator}.
@@ -134,174 +142,35 @@ public class AesCounterRandom extends BaseRandom implements SeekableRandom {
    */
   public AesCounterRandom(final byte[] seed) {
     super(seed);
-    currentBlock = new byte[BYTES_AT_ONCE];
     index = BYTES_AT_ONCE; // force generation of first block on demand
   }
 
-  /**
-   * Returns the maximum length in bytes of an AES key, which is {@code
-   * Math.min(Cipher.getMaxAllowedKeyLength("AES/ECB/NoPadding") / 8, 32)}. If the seed is longer
-   * than this, part of it becomes the counter's initial value. Otherwise, the full seed becomes the
-   * AES key and the counter is initially zero.
-   * @return the maximum length in bytes of an AES key.
-   */
-  public static int getMaxKeyLengthBytes() {
-    return MAX_KEY_LENGTH_BYTES;
-  }
-
-  private static int getKeyLength(final byte[] input) {
-    return (input.length > MAX_KEY_LENGTH_BYTES) ? MAX_KEY_LENGTH_BYTES
-        : ((input.length >= 24) ? 24 : 16);
-  }
-
-  @Override public ToStringHelper addSubclassFields(final ToStringHelper original) {
-    return original.add("counter", BinaryUtils.convertBytesToHexString(counter))
-        .add("cipher", cipher)
-        .add("index", index);
-  }
-
-  @Override protected void initTransientFields() {
-    super.initTransientFields();
-    addendDigits = new byte[COUNTER_SIZE_BYTES];
-    if (counter == null) {
-      counter = new byte[COUNTER_SIZE_BYTES];
-    }
-    if (counterInput == null) {
-      counterInput = new byte[BYTES_AT_ONCE];
-    }
+  @Override
+  protected MessageDigest createHash() {
     try {
-      cipher = Cipher.getInstance(ALGORITHM_MODE);
-      hash = MessageDigest.getInstance(HASH_ALGORITHM);
-    } catch (final NoSuchAlgorithmException | NoSuchPaddingException e) {
-      throw new InternalError("JVM is missing a required cipher or hash algorithm", e);
+      return MessageDigest.getInstance(HASH_ALGORITHM);
+    } catch (NoSuchAlgorithmException e) {
+      throw new InternalError("Required hash algorithm missing");
     }
   }
 
-  /**
-   * Generates BLOCKS_AT_ONCE 128-bit (16-byte) blocks. Copies them to currentBlock.
-   * @throws IllegalStateException If there is a problem with the cipher that generates the
-   *     random data.
-   */
-  private void nextBlock() {
-    for (int i = 0; i < BLOCKS_AT_ONCE; i++) {
-      Byte16ArrayArithmetic.addInto(counter, Byte16ArrayArithmetic.ONE);
-      System.arraycopy(counter, 0, counterInput, i * COUNTER_SIZE_BYTES, COUNTER_SIZE_BYTES);
-    }
+  @Override
+  protected Cipher createCipher() {
     try {
-      cipher.doFinal(counterInput, 0, BYTES_AT_ONCE, currentBlock);
-    } catch (final GeneralSecurityException ex) {
-      // Should never happen.  If initialisation succeeds without exceptions
-      // we should be able to proceed indefinitely without exceptions.
-      throw new IllegalStateException("Failed creating next random block.", ex);
+      return Cipher.getInstance(ALGORITHM_MODE);
+    } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+      throw new InternalError("Required cipher missing");
     }
   }
 
-  @Override protected final int next(final int bits) {
-    lock.lock();
-    int result;
-    try {
-      if ((BYTES_AT_ONCE - index) < 4) {
-        nextBlock();
-        index = 0;
-      }
-      result = BinaryUtils.convertBytesToInt(currentBlock, index);
-      index += 4;
-    } finally {
-      lock.unlock();
-    }
-    return result >>> (32 - bits);
-  }
-
-  /**
-   * {@inheritDoc} If the seed is not of the maximum length, it is combined with the existing seed
-   * using SHA-256.
-   */
-  @Override public void setSeed(final byte[] seed) {
-    checkNotTooLong(seed);
-    final byte[] key;
-    if (seed.length == MAX_KEY_LENGTH_BYTES) {
-      key = seed.clone();
-    } else {
-      lock.lock();
-      boolean weAreSeeded;
-      try {
-        weAreSeeded = seeded;
-      } finally {
-        lock.unlock();
-      }
-      if (weAreSeeded) {
-        // Extend the key
-        final byte[] newSeed = new byte[this.seed.length + seed.length];
-        System.arraycopy(this.seed, 0, newSeed, 0, this.seed.length);
-        System.arraycopy(seed, 0, newSeed, this.seed.length, seed.length);
-        final int keyLength = getKeyLength(newSeed);
-        if (newSeed.length > keyLength) {
-          final byte[] digest = hash.digest(newSeed);
-          key = digest.length > keyLength ? Arrays.copyOf(digest, keyLength) : digest;
-        } else {
-          key = newSeed;
-        }
-      } else {
-        key = seed.clone();
-      }
-    }
-    lock.lock();
-    try {
-      setSeedInternal(key);
-      entropyBits.addAndGet(8L * (seed.length - key.length));
-    } finally {
-      lock.unlock();
-    }
-  }
-
-  private static void checkNotTooLong(final byte[] seed) {
-    if (seed.length > MAX_TOTAL_SEED_LENGTH_BYTES) {
-      throw new IllegalArgumentException(String.format(
-          "Seed length is %d bytes; maximum is %d bytes", seed.length, MAX_TOTAL_SEED_LENGTH_BYTES));
-    }
-  }
-
-  /**
-   * Combines the given seed with the existing seed using SHA-256.
-   */
-  public void setSeed(final long seed) {
-    if (superConstructorFinished) {
-      final byte[] seedBytes = BinaryUtils.convertLongToBytes(seed);
-      setSeed(seedBytes);
-    }
-  }
-
-  @Override protected void setSeedInternal(final byte[] seed) {
-    checkNotTooLong(seed);
-    if (seed.length < 16) {
-      throw new IllegalArgumentException(String.format(
-          "Seed length is %d bytes; need at least 16 bytes", seed.length));
-    }
-    super.setSeedInternal(seed);
-    // determine how much of seed can go to key
-    final int keyLength = getKeyLength(seed);
-    final byte[] key = (seed.length == keyLength) ? seed : Arrays.copyOfRange(seed, 0, keyLength);
-    // rest goes to counter
-    final int bytesToCopyToCounter = seed.length - keyLength;
-    if (bytesToCopyToCounter > 0) {
-      System.arraycopy(seed, keyLength, counter, 0, bytesToCopyToCounter);
-    }
-    System.arraycopy(Byte16ArrayArithmetic.ZERO, 0, counter, bytesToCopyToCounter,
-        COUNTER_SIZE_BYTES - bytesToCopyToCounter);
-    try {
-      cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key, ALGORITHM));
-    } catch (final InvalidKeyException e) {
-      throw new InternalError("Invalid key: " + Arrays.toString(key), e);
-    }
-    if (currentBlock != null) {
-      index = BYTES_AT_ONCE;
-    } // else it'll be initialized in ctor
-    seeded = true;
+  @Override
+  protected void setKey(byte[] key) throws InvalidKeyException {
+    cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key, ALGORITHM));
   }
 
   /** Returns the longest supported seed length. */
   @Override public int getNewSeedLength() {
-    return MAX_TOTAL_SEED_LENGTH_BYTES;
+    return getMaxTotalSeedLengthBytes();
   }
 
   @Override public void advance(final long delta) {
@@ -332,5 +201,16 @@ public class AesCounterRandom extends BaseRandom implements SeekableRandom {
 
   @Override protected boolean supportsMultipleSeedLengths() {
     return true;
+  }
+
+  @Override
+  protected int getKeyLength(final byte[] input) {
+    return (input.length > AesCounterRandom.MAX_KEY_LENGTH_BYTES) ? AesCounterRandom.MAX_KEY_LENGTH_BYTES
+        : ((input.length >= 24) ? 24 : 16);
+  }
+
+  @Override
+  public int getMaxKeyLengthBytes() {
+    return AesCounterRandom.MAX_KEY_LENGTH_BYTES;
   }
 }
