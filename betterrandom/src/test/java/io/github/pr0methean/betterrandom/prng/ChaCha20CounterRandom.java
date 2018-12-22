01467dda89098fd240835a0475fa7b193478d4be
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-12-21 17:09:56 -0800

    ChaCha20CounterRandom for testing

---
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/ChaCha20CounterRandom.java
@@ -0,0 +1,66 @@
+package io.github.pr0methean.betterrandom.prng;
+
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import org.bouncycastle.jcajce.provider.digest.SHA3;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+public class ChaCha20CounterRandom extends CipherCounterRandom {
+  private static final Provider BOUNCY_CASTLE = new BouncyCastleProvider();
+  private static final int LARGE_KEY_LENGTH = 32;
+  private static final int SMALL_KEY_LENGTH = 16;
+
+
+  public ChaCha20CounterRandom(byte[] seed) {
+    super(seed);
+  }
+
+  @Override
+  public int getMaxKeyLengthBytes() {
+    return LARGE_KEY_LENGTH;
+  }
+
+  @Override
+  protected int getKeyLength(int inputLength) {
+    return inputLength >= LARGE_KEY_LENGTH ? LARGE_KEY_LENGTH : SMALL_KEY_LENGTH;
+  }
+
+  @Override
+  public int getCounterSizeBytes() {
+    return 64;
+  }
+
+  @Override
+  public int getBlocksAtOnce() {
+    return 1;
+  }
+
+  @Override
+  protected MessageDigest createHash() {
+    return new SHA3.Digest256();
+  }
+
+  @Override
+  protected Cipher createCipher() {
+    try {
+      return Cipher.getInstance("ChaCha20/ECB/NoPadding", BOUNCY_CASTLE);
+    } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+      throw new AssertionError(e);
+    }
+  }
+
+  @Override
+  protected void setKey(byte[] key) throws InvalidKeyException {
+    cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key, "ChaCha20"));
+  }
+
+  @Override
+  public int getNewSeedLength() {
+    return 0;
+  }
+}
