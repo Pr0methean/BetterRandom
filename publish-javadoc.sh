2da7b3196810feecb6d2efd40a168073602a7cd6
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-02-09 04:03:34 +0000

    Bug fixes for Google Tag Manager

--- publish-javadoc.sh
+++ publish-javadoc.sh
@@ -20,11 +20,18 @@ mv overview-summary.html index.html
 # Create sitemap
 find . -iname "*.html" | sed 's/^\./https:\/\/pr0methean.github.io/' > sitemap.txt
 
-# AdSense
-sed -i 's/<head>/<head><script async src="\/\/pagead2.googlesyndication.com\/pagead\/js\/adsbygoogle.js"><\/script><script>(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "ca-pub-9922551172827508", enable_page_level_ads: true});<\/script>/' index.html
+# AdSense, Analytics, Tag Manager
+replace '<head>' "<head><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\
+})(window,document,'script','dataLayer','GTM-K9NNCTT');</script>\
+<script async src='//pagead2.googlesyndication.com\/pagead\/js\/adsbygoogle.js'></script>\
+<script>(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: 'ca-pub-9922551172827508', \
+enable_page_level_ads: true});</script>"
 
-# Disable frames, step 2, & Google Analytics & Tag Manager
-find . -iname "*.html" -exec sed -i 's/<li><a href="[^\"]*" target="_top">Frames<\/a><\/li>//; s/<li><a href="[^\"]*" target="_top">No&nbsp;Frames<\/a><\/li>//; s/<head>/<head><!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start": new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src="https:\/\/www.googletagmanager.com\/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);})(window,document,"script","dataLayer","GTM-K9NNCTT");<\/script><!-- End Google Tag Manager -->/; s/<body>/<body><noscript><iframe src="https:\/\/www.googletagmanager.com\/ns.html?id=GTM-K9NNCTT" height="0" width="0" style="display:none;visibility:hidden"><\/iframe><\/noscript>/; s/overview-summary.html/index.html/g' {} \;
+# Disable frames, step 2
+find . -iname "*.html" -exec sed -i 's/<li><a href="[^\"]*" target="_top">Frames<\/a><\/li>//; s/<li><a href="[^\"]*" target="_top">No&nbsp;Frames<\/a><\/li>//; s/overview-summary.html/index.html/g' {} \;
 git add .
 git commit -m "🤖 Update Javadocs for GitHub Pages"
 git branch
