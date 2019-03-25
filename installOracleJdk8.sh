eda938fa587b10e2ace1b3e312edfd48102c52d8
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-03-25 14:53:03 -0700

    Work around lack of su

--- installOracleJdk8.sh
+++ installOracleJdk8.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-su -
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
