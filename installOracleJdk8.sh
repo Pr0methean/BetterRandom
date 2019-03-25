91d6108e32ca8dda7c816b0fbeea764e4f26e2fc
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-03-25 15:01:33 -0700

    Bug fix

--- installOracleJdk8.sh
+++ installOracleJdk8.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
