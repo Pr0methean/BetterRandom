d8b48c77e7ffec81cb2430b9ab9468ad4f13b4f8
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-03-24 20:28:21 -0700

    Bug fix? Inner class may be causing a problem, so make NamedFunction an outer class

---
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/NamedFunction.java
@@ -0,0 +1,23 @@
+package io.github.pr0methean.betterrandom;
+
+import java.io.Serializable;
+import java.util.function.Function;
+
+public final class NamedFunction<T, R> implements Function<T, R>, Serializable {
+
+  private final Function<T, R> function;
+  private final String name;
+
+  public NamedFunction(final Function<T, R> function, final String name) {
+    this.function = function;
+    this.name = name;
+  }
+
+  @Override public R apply(final T t) {
+    return function.apply(t);
+  }
+
+  @Override public String toString() {
+    return name;
+  }
+}
--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/concurrent/RandomWrapperRandomTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/concurrent/RandomWrapperRandomTest.java
@@ -16,9 +16,9 @@
 package io.github.pr0methean.betterrandom.prng.concurrent;
 
 import com.google.common.collect.ImmutableList;
+import io.github.pr0methean.betterrandom.NamedFunction;
 import io.github.pr0methean.betterrandom.prng.AbstractLargeSeedRandomTest;
 import io.github.pr0methean.betterrandom.prng.BaseRandom;
-import io.github.pr0methean.betterrandom.prng.BaseRandomTest;
 import io.github.pr0methean.betterrandom.prng.RandomTestUtils;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import java.util.Collections;
