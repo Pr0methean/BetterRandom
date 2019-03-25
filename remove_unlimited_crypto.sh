3b4f1c0e84e3da0d07f95810e372cd3caa47c7cb
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-03-25 15:04:05 -0700

    Bug fix

--- remove_unlimited_crypto.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-if [ "${LIMITED_CRYPTO}" = "true" ]; then
-  sudo mv -f travis-resources/local_policy.jar ${JAVA_HOME}/jre/lib/security/local_policy.jar
-  sudo mv -f travis-resources/US_export_policy.jar ${JAVA_HOME}/jre/lib/security/US_export_policy.jar
-fi
