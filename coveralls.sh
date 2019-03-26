f724adf79b37e55f32562b52dcc28a20da77339f
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-03-25 21:41:05 -0700

    Diagnose: is COVERALLS_TOKEN not set?

--- coveralls.sh
+++ coveralls.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+if [ -z "${COVERALLS_TOKEN}" ]; then echo "COVERALLS_TOKEN is unset or blank!"; fi
 cd betterrandom
-mvn -DrepoToken=${COVERALLS_TOKEN} coveralls:report 
+mvn -DrepoToken=${COVERALLS_TOKEN} coveralls:report
