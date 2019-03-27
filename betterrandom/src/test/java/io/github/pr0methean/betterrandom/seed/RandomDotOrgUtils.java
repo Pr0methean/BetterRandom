99ff177d0e8616624f6f6985b548401fc7e9d741
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-03-27 14:01:40 -0700

    Provide random.org key to all tests, remove platform-specific exclusions

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/seed/RandomDotOrgUtils.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/seed/RandomDotOrgUtils.java
@@ -1,7 +1,5 @@
 package io.github.pr0methean.betterrandom.seed;
 
-import io.github.pr0methean.betterrandom.TestUtils;
-import io.github.pr0methean.betterrandom.TestingDeficiency;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.Proxy.Type;
@@ -32,17 +30,6 @@ public enum RandomDotOrgUtils {
 
   public static Proxy createTorProxy() {
     return new Proxy(Type.SOCKS, new InetSocketAddress("localhost", TOR_PORT));
-  }
-
-  /**
-   * Appveyor and the OSX environment on Travis-CI don't currently use enough IP addresses to get
-   * heavy random.org usage allowed, so tests that are sufficiently demanding of random.org won't
-   * run on those environments.
-   * @return true if we're not running on Appveyor or a Travis-CI OSX instance, false if we are.
-   */
-  @SuppressWarnings("CallToSystemGetenv") @TestingDeficiency
-  public static boolean canRunRandomDotOrgLargeTest() {
-    return !TestUtils.isAppveyor() && !("osx".equals(System.getenv("TRAVIS_OS_NAME")));
   }
 
   public static SSLSocketFactory createSocketFactory() {
