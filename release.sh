a14a9f80bd5c255eb6457ec134ed889a62cd4822
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-04-17 18:40:19 -0700

    Read version from an environment variable (easier on Azure than a parameter)

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd betterrandom
-if [ "$#" -ge 1 ]; then
+if [ "${VERSION}" -ne "" ]; then
   MAYBE_P="-P"
   MAYBE_RELEASE="release-sign-artifacts"
   OLDVERSION=$(mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }' | sed 's/version=//')
@@ -12,9 +12,9 @@ rm -f release.properties &&\
 rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
 (
   if [ "$#" -ge 1 ]; then
-    mvn versions:set -DnewVersion=$1
-    sed -i "s/${OLDVERSION}<!--updateme-->/$1<!--updateme-->/" ../benchmark/pom.xml
-    sed -i "s/${OLDVERSION}<!--updateme-->/$1<!--updateme-->/" ../FifoFiller/pom.xml
+    mvn versions:set -DnewVersion=${VERSION}
+    sed -i "s/${OLDVERSION}<!--updateme-->/${VERSION}<!--updateme-->/" ../benchmark/pom.xml
+    sed -i "s/${OLDVERSION}<!--updateme-->/${VERSION}<!--updateme-->/" ../FifoFiller/pom.xml
     git add pom.xml
     git add ../benchmark/pom.xml
     git add ../FifoFiller/pom.xml
@@ -24,11 +24,11 @@ rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
   mvn -DskipTests -Darguments=-DskipTests -Dmaven.test.skip=true -P release-sign-artifacts \
       clean compile pre-integration-test deploy ${MAYBE_P} ${MAYBE_RELEASE}
   if [ $? ]; then
-    if [ "$#" -ge 1 ]; then
+    if [ "${VERSION}" -ne "" ]; then
       cd ..
       ./publish-javadoc.sh
-      git tag "BetterRandom-$1"
-      git push origin "BetterRandom-$1"
+      git tag "BetterRandom-${VERSION}"
+      git push origin "BetterRandom-${VERSION}"
       cd betterrandom
       # https://unix.stackexchange.com/a/23244/79452
       n=${1##*[!0-9]}; p=${1%%$n}
@@ -36,10 +36,10 @@ rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
       mvn versions:set -DnewVersion=${NEWVERSION}
       rm pom.xml.versionsBackup
       # For some reason we end up with -SNAPSHOT-SNAPSHOT without next 2 lines:
-      sed -i "s/$1-SNAPSHOT<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../benchmark/pom.xml
-      sed -i "s/$1-SNAPSHOT<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../FifoFiller/pom.xml
-      sed -i "s/$1<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../benchmark/pom.xml
-      sed -i "s/$1<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../FifoFiller/pom.xml
+      sed -i "s/${VERSION}-SNAPSHOT<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../benchmark/pom.xml
+      sed -i "s/${VERSION}-SNAPSHOT<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../FifoFiller/pom.xml
+      sed -i "s/${VERSION}<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../benchmark/pom.xml
+      sed -i "s/${VERSION}<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../FifoFiller/pom.xml
       git add pom.xml
       git add ../benchmark/pom.xml
       git add ../FifoFiller/pom.xml
@@ -47,8 +47,8 @@ rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
     fi
   else
     if [ "$#" -ge 1 ]; then
-      git tag -d "BetterRandom-$1"
-      git push --delete origin "BetterRandom-$1"
+      git tag -d "BetterRandom-${VERSION}"
+      git push --delete origin "BetterRandom-${VERSION}"
       git revert --no-edit ${VERSION_COMMIT}
       mv pom.xml.versionsBackup pom.xml
       git commit --amend --no-edit
