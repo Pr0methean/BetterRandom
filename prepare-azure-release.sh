8e17eac6e9f78ff1f526588fe5afb2aab03b3cd1
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-04-22 13:48:03 -0700

    Script cleanup

--- prepare-azure-release.sh
+++ prepare-azure-release.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
+echo '[prepare-azure-release.sh] Installing Apt packages...'
 sudo apt-get -y install markdown dieharder
-gem install github-markup
-gem install commonmarker
+echo '[prepare-azure-release.sh] Installing Gems...'
+gem install github-markup commonmarker
+echo '[prepare-azure-release.sh] Configuring Git...'
 git config --global user.email "4961925+Pr0methean@users.noreply.github.com"
-mkdir ~/.m2
-
 # Set up passwords
 # FIXME: Why does this hang if run without timeout?
+echo '[prepare-azure-release.sh] Configuring PGP...'
 timeout 60s gpg --import ${PGPKEY_PATH} </dev/null
 rm ${PGPKEY_PATH}
-
+echo '[prepare-azure-release.sh] Configuring Maven...'
+mkdir ~/.m2
 echo -n '<settingsSecurity><master>' > ~/.m2/settings-security.xml
 echo -n $(mvn -emp "${MVN_MASTER_PASS}") >> ~/.m2/settings-security.xml
 echo '</master></settingsSecurity>' >> ~/.m2/settings-security.xml
 password_crypt=$(mvn -ep "${SONATYPE_PASS}")
-read -s -p "Sonatype PGP password: " pgp_pass
-echo ""
 rm ~/.m2/settings.xml
 cat settings.xml.part1 > ~/.m2/settings_temp.xml
 echo -n "${password_crypt}" >> ~/.m2/settings_temp.xml
--- release.sh
+++ release.sh
@@ -9,51 +9,48 @@ else
   MAYBE_RELEASE=""
 fi &&
 rm -f release.properties &&\
-rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom/ &&\
-(
-  if [ "$#" -ge 1 ]; then
-    mvn versions:set -DnewVersion=${VERSION}
-    sed -i "s/${OLDVERSION}<!--updateme-->/${VERSION}<!--updateme-->/" ../benchmark/pom.xml
-    sed -i "s/${OLDVERSION}<!--updateme-->/${VERSION}<!--updateme-->/" ../FifoFiller/pom.xml
+rm -rf ../../.m2/repository/io/github/pr0methean/betterrandom
+if [ "$#" -ge 1 ]; then
+  mvn versions:set -DnewVersion=${VERSION}
+  sed -i "s/${OLDVERSION}<!--updateme-->/${VERSION}<!--updateme-->/" ../benchmark/pom.xml
+  sed -i "s/${OLDVERSION}<!--updateme-->/${VERSION}<!--updateme-->/" ../FifoFiller/pom.xml
+  git add pom.xml
+  git add ../benchmark/pom.xml
+  git add ../FifoFiller/pom.xml
+  git commit -m "🤖 Update version numbers"
+  VERSION_COMMIT=$(git rev-parse HEAD)
+fi
+mvn -DskipTests -Darguments=-DskipTests -Dmaven.test.skip=true -P!jdk9 -P release-sign-artifacts \
+    clean compile pre-integration-test deploy ${MAYBE_P} ${MAYBE_RELEASE}
+if [ $? ]; then
+  if [ "${VERSION}" -ne "" ]; then
+    cd ..
+    ./publish-javadoc.sh
+    git tag "BetterRandom-${VERSION}"
+    git push origin "BetterRandom-${VERSION}"
+    cd betterrandom
+    # https://unix.stackexchange.com/a/23244/79452
+    n=${1##*[!0-9]}; p=${1%%$n}
+    NEWVERSION="$p$((n+1))-SNAPSHOT"
+    mvn versions:set -DnewVersion=${NEWVERSION}
+    rm pom.xml.versionsBackup
+    # For some reason we end up with -SNAPSHOT-SNAPSHOT without next 2 lines:
+    sed -i "s/${VERSION}-SNAPSHOT<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../benchmark/pom.xml
+    sed -i "s/${VERSION}-SNAPSHOT<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../FifoFiller/pom.xml
+    sed -i "s/${VERSION}<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../benchmark/pom.xml
+    sed -i "s/${VERSION}<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../FifoFiller/pom.xml
     git add pom.xml
     git add ../benchmark/pom.xml
     git add ../FifoFiller/pom.xml
     git commit -m "🤖 Update version numbers"
-    VERSION_COMMIT=$(git rev-parse HEAD)
   fi
-  mvn -DskipTests -Darguments=-DskipTests -Dmaven.test.skip=true -P!jdk9 -P release-sign-artifacts \
-      clean compile pre-integration-test deploy ${MAYBE_P} ${MAYBE_RELEASE}
-  if [ $? ]; then
-    if [ "${VERSION}" -ne "" ]; then
-      cd ..
-      ./publish-javadoc.sh
-      git tag "BetterRandom-${VERSION}"
-      git push origin "BetterRandom-${VERSION}"
-      cd betterrandom
-      # https://unix.stackexchange.com/a/23244/79452
-      n=${1##*[!0-9]}; p=${1%%$n}
-      NEWVERSION="$p$((n+1))-SNAPSHOT"
-      mvn versions:set -DnewVersion=${NEWVERSION}
-      rm pom.xml.versionsBackup
-      # For some reason we end up with -SNAPSHOT-SNAPSHOT without next 2 lines:
-      sed -i "s/${VERSION}-SNAPSHOT<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../benchmark/pom.xml
-      sed -i "s/${VERSION}-SNAPSHOT<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../FifoFiller/pom.xml
-      sed -i "s/${VERSION}<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../benchmark/pom.xml
-      sed -i "s/${VERSION}<!--updateme-->/${NEWVERSION}<!--updateme-->/" ../FifoFiller/pom.xml
-      git add pom.xml
-      git add ../benchmark/pom.xml
-      git add ../FifoFiller/pom.xml
-      git commit -m "🤖 Update version numbers"
-    fi
-  else
-    if [ "$#" -ge 1 ]; then
-      git tag -d "BetterRandom-${VERSION}"
-      git push --delete origin "BetterRandom-${VERSION}"
-      git revert --no-edit ${VERSION_COMMIT}
-      mv pom.xml.versionsBackup pom.xml
-      git commit --amend --no-edit
-    fi
+else
+  if [ "${VERSION}" -ne "" ]; then
+    git tag -d "BetterRandom-${VERSION}"
+    git push --delete origin "BetterRandom-${VERSION}"
+    git revert --no-edit ${VERSION_COMMIT}
+    mv pom.xml.versionsBackup pom.xml
+    git commit --amend --no-edit
   fi
-  git push
-)
-cd ..
+fi
+git push
