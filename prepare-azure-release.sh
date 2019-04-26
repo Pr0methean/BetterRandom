2d842011cfdb2b9dad83dfc947527911a4a1b1ee
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-04-26 13:42:18 -0700

    Switching to secure files

--- prepare-azure-release.sh
+++ prepare-azure-release.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
-echo "PGPKEY_PATH=${PGPKEY_PATH}"
-echo "MVN_MASTER_PASS length ${#MVN_MASTER_PASS}"
-echo "SONATYPE_PASS length ${#SONATYPE_PASS}"
-echo "PGP_PASS length ${#PGP_PASS}"
 echo '[prepare-azure-release.sh] Configuring Apt...'
 # Microsoft FIXME: Some of the Apt caches in this image lead to 404s!
 sudo rm -r /var/lib/apt/lists
@@ -21,28 +17,3 @@ git config --global user.name "Chris Hennick"
 echo '[prepare-azure-release.sh] Configuring GnuPG...'
 gpg --import ${PGPKEY_PATH} </dev/null
 rm ${PGPKEY_PATH}
-echo '[prepare-azure-release.sh] Configuring Maven...'
-mkdir ~/.m2
-MASTER_CRYPT=$(mvn -emp "${MVN_MASTER_PASS}")
-echo "<settingsSecurity><master>${MASTER_CRYPT}</master></settingsSecurity>" > ~/.m2/settings-security.xml
-SONATYPE_CRYPT=$(mvn -ep "${SONATYPE_PASS}")
-PGP_CRYPT=$(mvn -ep "${PGP_PASS}")
-echo "<settings>
-  <servers>
-    <server>
-      <id>sonatype-nexus-staging</id>
-      <username>Pr0methean</username>
-      <password>${SONATYPE_CRYPT}</password>
-    </server>
-    <server>
-      <id>sonatype-nexus-snapshots</id>
-      <username>Pr0methean</username>
-      <password>${SONATYPE_CRYPT}</password>
-    </server>
-    <server>
-      <id>gpg.passphrase</id>
-      <passphrase>${PGP_CRYPT}</passphrase>
-    </server>
-  </servers>
-</settings>" > ~/.m2/settings.xml
-
