cf3ba9c6a2a113a0f8859a56a53a715cb6521aa8
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-04-26 12:59:17 -0700

    Create files offline, for switching to a secure-file pipeline

--- prepare-azure-release.sh
+++ prepare-azure-release.sh
@@ -4,25 +4,6 @@ echo "PGPKEY_PATH=${PGPKEY_PATH}"
 echo "MVN_MASTER_PASS length ${#MVN_MASTER_PASS}"
 echo "SONATYPE_PASS length ${#SONATYPE_PASS}"
 echo "PGP_PASS length ${#PGP_PASS}"
-echo '[prepare-azure-release.sh] Configuring Apt...'
-# Microsoft FIXME: Some of the Apt caches in this image lead to 404s!
-sudo rm -r /var/lib/apt/lists
-sudo mkdir /var/lib/apt/lists
-sudo rm /var/cache/apt/*.bin
-sudo apt-get update
-echo '[prepare-azure-release.sh] Installing Apt packages...'
-sudo apt-get -y install ruby2.3-dev markdown dieharder recode
-echo '[prepare-azure-release.sh] Installing Gems...'
-sudo gem install github-markup commonmarker
-echo '[prepare-azure-release.sh] Configuring Git...'
-git config --global user.email "4961925+Pr0methean@users.noreply.github.com"
-git config --global user.name "Chris Hennick"
-# Set up passwords
-echo '[prepare-azure-release.sh] Configuring GnuPG...'
-gpg --import ${PGPKEY_PATH} </dev/null
-rm ${PGPKEY_PATH}
-echo '[prepare-azure-release.sh] Configuring Maven...'
-mkdir ~/.m2
 MASTER_CRYPT=$(mvn -emp "${MVN_MASTER_PASS}")
 echo "<settingsSecurity><master>${MASTER_CRYPT}</master></settingsSecurity>" > ~/.m2/settings-security.xml
 SONATYPE_CRYPT=$(mvn -ep "${SONATYPE_PASS}")
