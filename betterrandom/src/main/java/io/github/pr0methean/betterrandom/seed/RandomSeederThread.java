32cd524274c3eff17e8ef7984809971ececef6c0
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-06-03 16:13:30 -0700

    Remove some unused methods

--- betterrandom/src/main/java/io/github/pr0methean/betterrandom/seed/RandomSeederThread.java
+++ betterrandom/src/main/java/io/github/pr0methean/betterrandom/seed/RandomSeederThread.java
@@ -184,25 +184,6 @@ public final class RandomSeederThread extends LooperThread {
     }
   }
 
-  /**
-   * Notifies the thread for the given {@link SeedGenerator} that PRNGs are waiting to be reseeded.
-   * This method does not block, because if it cannot immediately take the lock to signal the
-   * condition, then that means the thread is already running or being woken up.
-   * <p>
-   * Warning: This may return true during a brief window while the thread is shutting down.
-   * @param seedGenerator the {@link SeedGenerator} that should reseed {@code random}
-   * @return Whether or not the thread exists and is now awake.
-   */
-  public static boolean wakeUp(final SeedGenerator seedGenerator) {
-    Objects.requireNonNull(seedGenerator, "randomSeeder must not be null");
-    final RandomSeederThread thread = INSTANCES.get(seedGenerator);
-    if (thread == null) {
-      return false;
-    }
-    thread.wakeUp();
-    return true;
-  }
-
   public static boolean isEmpty(final SeedGenerator seedGenerator) {
     final RandomSeederThread thread = INSTANCES.get(seedGenerator);
     return thread == null || thread.isEmpty();
