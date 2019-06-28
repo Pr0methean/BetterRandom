e14b78491cd5998efe64013045c4afe88b544802
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-06-27 20:54:45 -0700

    Bug fix

--- benchmark.sh
+++ benchmark.sh
@@ -13,8 +13,8 @@ mvn -B -DskipTests -Darguments=-DskipTests\
 cd ../benchmark &&\
 mvn -DskipTests ${MAYBE_ANDROID_FLAG} package &&\
 cd target &&\
-java ${JAVA_OPTS} -jar benchmarks.jar $@ -jvm $(JAVA_HOME)/bin/java  -f 1 -t 1 -foe true -v EXTRA 2>&1 |\
+java ${JAVA_OPTS} -jar benchmarks.jar $@ -jvm "${JAVA_HOME}/bin/java"  -f 1 -t 1 -foe true -v EXTRA 2>&1 |\
     tee benchmark_results_one_thread.txt &&\
-java ${JAVA_OPTS} -jar benchmarks.jar $@ -jvm $(JAVA_HOME)/bin/java -f 1 -t 2 -foe true -v EXTRA 2>&1 |\
+java ${JAVA_OPTS} -jar benchmarks.jar $@ -jvm "${JAVA_HOME}/bin/java" -f 1 -t 2 -foe true -v EXTRA 2>&1 |\
     tee benchmark_results_two_threads.txt &&\
 cd ../..
