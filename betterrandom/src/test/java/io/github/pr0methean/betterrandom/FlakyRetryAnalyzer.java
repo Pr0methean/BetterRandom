fa3bd2060298fb89c999bf02e3b063cca0b86770
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-07-02 20:39:49 -0700

    Allow a third try for flaky tests

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/FlakyRetryAnalyzer.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/FlakyRetryAnalyzer.java
@@ -6,7 +6,7 @@ import org.testng.ITestResult;
 /** From https://www.toolsqa.com/selenium-webdriver/retry-failed-tests-testng/ */
 public class FlakyRetryAnalyzer implements IRetryAnalyzer {
   int counter = 0;
-  int retryLimit = 1;
+  int retryLimit = 2;
 
   @Override
   public boolean retry(ITestResult result) {
