02388e8b068931c91fe9667f504667122ddc88db
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-09-11 10:17:27 -0700

    Reduce log spam in benchmark & Dieharder

--- benchmark.sh
+++ benchmark.sh
@@ -12,7 +12,7 @@ mvn -B -DskipTests -Darguments=-DskipTests\
     -Dmaven.test.skip=true ${MAYBE_ANDROID_FLAG}\
     clean install &&\
 cd ../benchmark &&\
-mvn -DskipTests ${MAYBE_ANDROID_FLAG} package &&\
+mvn -B -DskipTests ${MAYBE_ANDROID_FLAG} package &&\
 cd target &&\
 "${JAVA_BIN}" ${JAVA_OPTS} -jar benchmarks.jar $@ -jvm "${JAVA_BIN}"  -f 1 -t 1 -foe true -v EXTRA 2>&1 |\
     tee benchmark_results_one_thread.txt &&\
