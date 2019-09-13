d0e475f6c8b094afe4e44e0393bb6e80ed388201
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-09-13 11:19:23 -0700

    Bug fix: still need a subshell because using same fifo for multiple readers

--- dieharder.sh
+++ dieharder.sh
@@ -19,7 +19,7 @@ mkfifo prng_out 2>&1
 # Checked Dieharder invocation
 
 chkdh() {
-    dieharder -S 1 -g 200 $@ < prng_out | tee current_test.txt
+    dieharder -S 1 -g 200 $@ | tee current_test.txt
     cat current_test.txt >> dieharder.txt
     if [ "$(grep -q -m 1 'FAILED' current_test.txt)" ]; then
       kill -9 "${JAVA_PROCESS}"
@@ -28,81 +28,83 @@ chkdh() {
 }
 "${JAVA_BIN}" ${JAVA_OPTS} -jar "${JAR}" io.github.pr0methean.betterrandom.prng.${CLASS} prng_out 2>&1 &
 JAVA_PROCESS=$!
-chkdh -Y 1 -k 2 -d 0
-chkdh -Y 1 -k 2 -d 1
-chkdh -Y 1 -k 2 -d 2
-chkdh -Y 1 -k 2 -d 3
-chkdh -Y 1 -k 2 -d 4
-chkdh -Y 1 -k 2 -d 5
-chkdh -Y 1 -k 2 -d 6
-chkdh -Y 1 -k 2 -d 7
-chkdh -Y 1 -k 2 -d 8
-chkdh -Y 1 -k 2 -d 9
-chkdh -Y 1 -k 2 -d 10
-chkdh -Y 1 -k 2 -d 11
-chkdh -Y 1 -k 2 -d 12
-chkdh -Y 1 -k 2 -d 13
-# Marked "Do Not Use": chkdh -Y 1 -k 2 -d 14
-chkdh -Y 1 -k 2 -d 15
-chkdh -Y 1 -k 2 -d 16
-chkdh -Y 1 -k 2 -d 17
-chkdh -Y 1 -k 2 -d 100
-chkdh -Y 1 -k 2 -d 101
-chkdh -d 102
-chkdh -Y 1 -k 2 -d 200 -n 1
-chkdh -Y 1 -k 2 -d 200 -n 2
-chkdh -Y 1 -k 2 -d 200 -n 3
-chkdh -Y 1 -k 2 -d 200 -n 4
-chkdh -Y 1 -k 2 -d 200 -n 5
-chkdh -Y 1 -k 2 -d 200 -n 6
-chkdh -Y 1 -k 2 -d 200 -n 7
-chkdh -Y 1 -k 2 -d 200 -n 8
-chkdh -Y 1 -k 2 -d 200 -n 9
-chkdh -Y 1 -k 2 -d 200 -n 10
-chkdh -Y 1 -k 2 -d 200 -n 11
-chkdh -Y 1 -k 2 -d 200 -n 12
-chkdh -Y 1 -k 2 -d 201 -n 2
-chkdh -Y 1 -k 2 -d 201 -n 3
-chkdh -Y 1 -k 2 -d 201 -n 4
-chkdh -Y 1 -k 2 -d 201 -n 5
-chkdh -Y 1 -k 2 -d 202
-chkdh -d 203 -p 30 -n 0
-chkdh -d 203 -p 30 -n 1
-chkdh -d 203 -p 30 -n 2
-chkdh -d 203 -p 30 -n 3
-chkdh -d 203 -p 30 -n 4
-chkdh -d 203 -p 30 -n 5
-chkdh -d 203 -p 30 -n 6
-chkdh -d 203 -p 30 -n 7
-chkdh -d 203 -p 30 -n 8
-chkdh -d 203 -p 30 -n 9
-chkdh -d 203 -p 30 -n 10
-chkdh -d 203 -p 30 -n 11
-chkdh -d 203 -p 30 -n 12
-chkdh -d 203 -p 30 -n 13
-chkdh -d 203 -p 30 -n 14
-chkdh -d 203 -p 30 -n 15
-chkdh -d 203 -p 30 -n 16
-chkdh -d 203 -p 30 -n 17
-chkdh -d 203 -p 30 -n 18
-chkdh -d 203 -p 30 -n 19
-chkdh -d 203 -p 30 -n 20
-chkdh -d 203 -p 30 -n 21
-chkdh -d 203 -p 30 -n 22
-chkdh -d 203 -p 30 -n 23
-chkdh -d 203 -p 30 -n 24
-chkdh -d 203 -p 30 -n 25
-chkdh -d 203 -p 30 -n 26
-chkdh -d 203 -p 30 -n 27
-chkdh -d 203 -p 30 -n 28
-chkdh -d 203 -p 30 -n 29
-chkdh -d 203 -p 30 -n 30
-chkdh -d 203 -p 30 -n 31
-chkdh -d 203 -p 30 -n 32
-chkdh -Y 1 -k 2 -d 204
-chkdh -d 205
-chkdh -d 206
-chkdh -d 207
-chkdh -d 208
-chkdh -d 209
-
+(
+  chkdh -Y 1 -k 2 -d 0
+  chkdh -Y 1 -k 2 -d 1
+  chkdh -Y 1 -k 2 -d 2
+  chkdh -Y 1 -k 2 -d 3
+  chkdh -Y 1 -k 2 -d 4
+  chkdh -Y 1 -k 2 -d 5
+  chkdh -Y 1 -k 2 -d 6
+  chkdh -Y 1 -k 2 -d 7
+  chkdh -Y 1 -k 2 -d 8
+  chkdh -Y 1 -k 2 -d 9
+  chkdh -Y 1 -k 2 -d 10
+  chkdh -Y 1 -k 2 -d 11
+  chkdh -Y 1 -k 2 -d 12
+  chkdh -Y 1 -k 2 -d 13
+  # Marked "Do Not Use": chkdh -Y 1 -k 2 -d 14
+  chkdh -Y 1 -k 2 -d 15
+  chkdh -Y 1 -k 2 -d 16
+  chkdh -Y 1 -k 2 -d 17
+  chkdh -Y 1 -k 2 -d 100
+  chkdh -Y 1 -k 2 -d 101
+  chkdh -d 102
+  chkdh -Y 1 -k 2 -d 200 -n 1
+  chkdh -Y 1 -k 2 -d 200 -n 2
+  chkdh -Y 1 -k 2 -d 200 -n 3
+  chkdh -Y 1 -k 2 -d 200 -n 4
+  chkdh -Y 1 -k 2 -d 200 -n 5
+  chkdh -Y 1 -k 2 -d 200 -n 6
+  chkdh -Y 1 -k 2 -d 200 -n 7
+  chkdh -Y 1 -k 2 -d 200 -n 8
+  chkdh -Y 1 -k 2 -d 200 -n 9
+  chkdh -Y 1 -k 2 -d 200 -n 10
+  chkdh -Y 1 -k 2 -d 200 -n 11
+  chkdh -Y 1 -k 2 -d 200 -n 12
+  chkdh -Y 1 -k 2 -d 201 -n 2
+  chkdh -Y 1 -k 2 -d 201 -n 3
+  chkdh -Y 1 -k 2 -d 201 -n 4
+  chkdh -Y 1 -k 2 -d 201 -n 5
+  chkdh -Y 1 -k 2 -d 202
+  chkdh -d 203 -p 30 -n 0
+  chkdh -d 203 -p 30 -n 1
+  chkdh -d 203 -p 30 -n 2
+  chkdh -d 203 -p 30 -n 3
+  chkdh -d 203 -p 30 -n 4
+  chkdh -d 203 -p 30 -n 5
+  chkdh -d 203 -p 30 -n 6
+  chkdh -d 203 -p 30 -n 7
+  chkdh -d 203 -p 30 -n 8
+  chkdh -d 203 -p 30 -n 9
+  chkdh -d 203 -p 30 -n 10
+  chkdh -d 203 -p 30 -n 11
+  chkdh -d 203 -p 30 -n 12
+  chkdh -d 203 -p 30 -n 13
+  chkdh -d 203 -p 30 -n 14
+  chkdh -d 203 -p 30 -n 15
+  chkdh -d 203 -p 30 -n 16
+  chkdh -d 203 -p 30 -n 17
+  chkdh -d 203 -p 30 -n 18
+  chkdh -d 203 -p 30 -n 19
+  chkdh -d 203 -p 30 -n 20
+  chkdh -d 203 -p 30 -n 21
+  chkdh -d 203 -p 30 -n 22
+  chkdh -d 203 -p 30 -n 23
+  chkdh -d 203 -p 30 -n 24
+  chkdh -d 203 -p 30 -n 25
+  chkdh -d 203 -p 30 -n 26
+  chkdh -d 203 -p 30 -n 27
+  chkdh -d 203 -p 30 -n 28
+  chkdh -d 203 -p 30 -n 29
+  chkdh -d 203 -p 30 -n 30
+  chkdh -d 203 -p 30 -n 31
+  chkdh -d 203 -p 30 -n 32
+  chkdh -Y 1 -k 2 -d 204
+  chkdh -d 205
+  chkdh -d 206
+  chkdh -d 207
+  chkdh -d 208
+  chkdh -d 209
+) < prng_out
+exit $?
