06fddae794f8bc53de70b544910ff4a81949fbd6
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-09-25 20:46:32 -0700

    Stub out EntropyBlockingRandomWrapperAesCounterRandomTest

---
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/concurrent/EntropyBlockingRandomWrapperAesCounterRandomTest.java
@@ -0,0 +1,8 @@
+package io.github.pr0methean.betterrandom.prng.concurrent;
+
+import org.testng.annotations.Test;
+
+@Test(testName = "EntropyBlockingRandomWrapper:AesCounterRandom")
+public class EntropyBlockingRandomWrapperAesCounterRandomTest extends RandomWrapperAesCounterRandomTest {
+  // TODO
+}
