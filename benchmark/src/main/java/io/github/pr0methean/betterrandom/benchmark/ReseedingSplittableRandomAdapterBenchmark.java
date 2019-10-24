cd24f28380c937263af1252d45b1eada2cccd3c3
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-10-24 16:27:19 -0700

    Fix bad merge

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/ReseedingSplittableRandomAdapterBenchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/ReseedingSplittableRandomAdapterBenchmark.java
@@ -3,14 +3,13 @@ package io.github.pr0methean.betterrandom.benchmark;
 import static io.github.pr0methean.betterrandom.seed.SecureRandomSeedGenerator.DEFAULT_INSTANCE;
 
 import io.github.pr0methean.betterrandom.prng.adapter.ReseedingSplittableRandomAdapter;
-import io.github.pr0methean.betterrandom.seed.RandomSeederThread;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import io.github.pr0methean.betterrandom.seed.SimpleRandomSeeder;
 import java.util.Random;
 
 public class ReseedingSplittableRandomAdapterBenchmark extends AbstractRandomBenchmark {
 
-  private final SimpleRandomSeeder thread = new RandomSeederThread(DEFAULT_INSTANCE);
+  private final SimpleRandomSeeder thread = new SimpleRandomSeeder(DEFAULT_INSTANCE);
 
   @Override protected Random createPrng() throws SeedException {
     return ReseedingSplittableRandomAdapter.getInstance(thread, DEFAULT_INSTANCE);
