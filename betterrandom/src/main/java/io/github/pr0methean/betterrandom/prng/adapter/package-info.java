5cbdfb4a73e00c802284a6ea77ffa1077ab2cd41
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-10-24 15:14:33 -0700

    Update package-info.java

--- betterrandom/src/main/java/io/github/pr0methean/betterrandom/prng/adapter/package-info.java
+++ betterrandom/src/main/java/io/github/pr0methean/betterrandom/prng/adapter/package-info.java
@@ -1,9 +1,10 @@
 /**
- * Implementations of {@link java.util.Random} designed to be used concurrently from multiple
- * threads, backed by instances of {@link java.util.SplittableRandom} or
- * {@link java.lang.ThreadLocal}{@code <?>}. An instance in a given state may give different
- * pseudorandom numbers depending on which thread is calling, and the mapping of threads to seeds
- * cannot in general be deserialized (since threads do not have a serializable identity).
- * {@link io.github.pr0methean.betterrandom.prng.BaseRandom#setSeed(byte[])} may be thread-local.
+ * Implementations of {@link io.github.pr0methean.betterrandom.prng.BaseRandom} that are backed by
+ * instances of {@link java.util.SplittableRandom} or {@link java.util.Random}, many of them wrapped
+ * in {@link java.lang.ThreadLocal}{@code <?>} to allow concurrent use of a shared instance by
+ * multiple threads. The latter is not in general reproducible, since threads start in a
+ * nondeterministic order and do not have a serializable identity.
+ * {@link io.github.pr0methean.betterrandom.prng.BaseRandom#setSeed(byte[])} may affect only the
+ * calling thread.
  */
 package io.github.pr0methean.betterrandom.prng.adapter;
