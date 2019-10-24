f648ce89cf854e681b2b683e3703c2ba0f7f4fc6
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-10-24 15:46:20 -0700

    Optimize imports

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/XorShiftRandomBenchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/XorShiftRandomBenchmark.java
@@ -1,10 +1,10 @@
 package io.github.pr0methean.betterrandom.benchmark;
 
+import static io.github.pr0methean.betterrandom.seed.SecureRandomSeedGenerator.DEFAULT_INSTANCE;
+
 import io.github.pr0methean.betterrandom.prng.XorShiftRandom;
 import io.github.pr0methean.betterrandom.seed.SeedException;
 import java.util.Random;
-
-import static io.github.pr0methean.betterrandom.seed.SecureRandomSeedGenerator.DEFAULT_INSTANCE;
 
 public class XorShiftRandomBenchmark extends AbstractRandomBenchmarkWithReseeding {
 
