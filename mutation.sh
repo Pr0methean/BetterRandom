e016dda43daad1c12798bda776a3533e6634877d
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2020-01-31 12:52:52 -0800

    Debug mutation failure on Azure

--- mutation.sh
+++ mutation.sh
@@ -2,4 +2,4 @@
 sudo renice -10 $$
 cd betterrandom
 set -eo pipefail # TODO: Find a pipefail method that's not Bash-specific
-mvn -B -Ppit clean test-compile org.pitest:pitest-maven:mutationCoverage 2>&1 | tee >(grep -qv "BUILD FAILURE")
+mvn -X -B -Ppit clean test-compile org.pitest:pitest-maven:mutationCoverage 2>&1 # | tee >(grep -qv "BUILD FAILURE")
