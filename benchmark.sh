3dd1448139558fc6d7d79f511d6145ad676c11b4
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2020-01-31 17:34:43 -0800

    Test solution to #445

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+JAVA_HOME=${!1} # first arg names the variable that JAVA_HOME is copied from
 JAVA_BIN=${JAVA_HOME}/bin/java
-sudo renice -10 $$
 if [ "${ANDROID}" = "true" ]; then
   MAYBE_ANDROID_FLAG="-Pandroid"
 else
@@ -14,8 +14,8 @@ mvn -B -DskipTests -Darguments=-DskipTests\
 cd ../benchmark &&\
 mvn -B -DskipTests ${MAYBE_ANDROID_FLAG} package &&\
 cd target &&\
-"${JAVA_BIN}" ${JAVA_OPTS} -jar benchmarks.jar $@ -jvm "${JAVA_BIN}"  -f 1 -t 1 -foe true -v EXTRA 2>&1 |\
+"${JAVA_BIN}" ${JAVA_OPTS} -jar benchmarks.jar $2 -jvm "${JAVA_BIN}" -f 1 -t 1 -foe true -v EXTRA 2>&1 |\
     tee benchmark_results_one_thread.txt &&\
-"${JAVA_BIN}" ${JAVA_OPTS} -jar benchmarks.jar $@ -jvm "${JAVA_BIN}" -f 1 -t 2 -foe true -v EXTRA 2>&1 |\
+"${JAVA_BIN}" ${JAVA_OPTS} -jar benchmarks.jar $2 -jvm "${JAVA_BIN}" -f 1 -t 2 -foe true -v EXTRA 2>&1 |\
     tee benchmark_results_two_threads.txt &&\
 cd ../..
