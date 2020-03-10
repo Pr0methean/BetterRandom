875e19d430427dc56e13866dbb9a4da7c039d078
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2020-03-10 15:08:52 -0700

    Check that score assertion works (should fail)

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/AbstractRandomBenchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/AbstractRandomBenchmark.java
@@ -1,6 +1,7 @@
 package io.github.pr0methean.betterrandom.benchmark;
 
 import io.github.pr0methean.betterrandom.util.EntryPoint;
+import java.util.Collection;
 import java.util.Random;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Level;
@@ -11,6 +12,7 @@ import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Timeout;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.results.RunResult;
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
@@ -21,6 +23,10 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 @State(Scope.Benchmark)
 abstract class AbstractRandomBenchmark<T extends Random> {
 
+  /**
+   * TODO: Find a way to specify this separately for each test
+   */
+  private static final double MINIMUM_OPS_PER_SEC = 1_000_000_000;
   protected T prng;
 
   AbstractRandomBenchmark() {
@@ -32,10 +38,20 @@ abstract class AbstractRandomBenchmark<T extends Random> {
         new OptionsBuilder().syncIterations(false).shouldFailOnError(true)
             .forks(1).resultFormat(ResultFormatType.CSV).detectJvmArgs();
     for (int nThreads = 1; nThreads <= 2; nThreads++) {
-      new Runner(
+      final Runner runner = new Runner(
           options.threads(nThreads).output(String.format("%d-thread_bench_results.csv", nThreads))
-              .build()).run();
+              .build());
+      Collection<RunResult> results = runner.run();
+      for (RunResult runResult : results) {
+        if (runResult.getAggregatedResult().getPrimaryResult().getScore() < MINIMUM_OPS_PER_SEC) {
+          throw new AssertionError(String.format("%s is too slow", runResult));
+        }
+      }
     }
+  }
+
+  protected double getMinimumScore() {
+    return 0;
   }
 
   @Setup(Level.Trial) public void setUp() throws Exception {
