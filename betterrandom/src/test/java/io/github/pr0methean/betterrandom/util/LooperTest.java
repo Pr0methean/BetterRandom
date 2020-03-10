4f0764d80cff029e99569da00c50629b7e1f8bc8
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2020-03-10 15:08:52 -0700

    Check that score assertion works (should fail)
    Test a fix (wrong entry point); should fail
    
    Deflake a test that gets spuriously interrupted

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/util/LooperTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/util/LooperTest.java
@@ -69,7 +69,8 @@ public class LooperTest {
     assertEquals(sleepingThread.finishedIterations.get(), 1);
   }
 
-  @Test public void testResurrect() throws InterruptedException {
+  // FIXME: Spurious interrupts
+  @Test public void testResurrect() {
     final TestLooper testLooperThread = new TestLooper();
     try {
       testLooperThread.shouldThrow.set(true);
@@ -78,7 +79,7 @@ public class LooperTest {
       while (testLooperThread.isRunning()) {
         waits--;
         assertTrue(waits >= 0, "Timed out waiting for test looper thread to die");
-        Thread.sleep(10);
+        Uninterruptibles.sleepUninterruptibly(10, TimeUnit.MILLISECONDS);
       }
       assertFalse(testLooperThread.isRunning());
       testLooperThread.shouldThrow.set(false);
@@ -88,7 +89,7 @@ public class LooperTest {
       while (testLooperThread.iterations.get() == 0) {
         waits--;
         assertTrue(waits >= 0, "Timed out waiting for test looper thread to resume running");
-        Thread.sleep(10);
+        Uninterruptibles.sleepUninterruptibly(10, TimeUnit.MILLISECONDS);
       }
     } finally {
       testLooperThread.interrupt();
