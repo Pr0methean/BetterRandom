d2b040d332338114283bb7881c2bcd0c526fd6dc
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2020-03-16 11:53:43 -0700

    Rewrite benchmark.ps1 Eliminate redundant runs

--- etc/scripts/benchmark.ps1
+++ etc/scripts/benchmark.ps1
@@ -1,17 +1,6 @@
 $JAVA_HOME = [Environment]::GetEnvironmentVariable($1) # first arg names the variable that JAVA_HOME is copied from
 $JAVA_BIN = $JAVA_HOME + '/bin/java'
-if ($env:ANDROID = 1)
-{
-    $MAYBE_ANDROID_FLAG = "-Pandroid"
-}
-else
-{
-    $MAYBE_ANDROID_FLAG = ""
-}
-if ( $env:APPVEYOR )
-{
-    $RANDOM_DOT_ORG_KEY = $env:random_dot_org_key
-}
+$RANDOM_DOT_ORG_KEY = $env:RANDOM_DOT_ORG_KEY
 $MAYBE_PROGUARD="pre-integration-test"
 cd betterrandom
 mvn -B "-DskipTests" "-Darguments=-DskipTests" "-Dmaven.test.skip=true" "$MAYBE_ANDROID_FLAG" `
@@ -19,12 +8,6 @@ mvn -B "-DskipTests" "-Darguments=-DskipTests" "-Dmaven.test.skip=true" "$MAYBE_
 cd ../benchmark
 mvn -B "-DskipTests" "$MAYBE_ANDROID_FLAG" package
 cd target
-if ( $TRAVIS ) {
-  java -jar benchmarks.jar -f 1 -t 1 -foe true
-  java -jar benchmarks.jar -f 1 -t 2 -foe true
-} else {
-  java -jar benchmarks.jar -f 1 -t 1 -foe true -v EXTRA 2>&1 | `
-      Tee-Object benchmark_results_one_thread.txt
-  java -jar benchmarks.jar -f 1 -t 2 -foe true -v EXTRA 2>&1 | `
-      Tee-Object benchmark_results_two_threads.txt
-}
+"$JAVA_BIN" -jar benchmarks.jar -f 1 -t 1 -foe true
+"$JAVA_BIN" -jar benchmarks.jar -f 1 -t 2 -foe true
+
