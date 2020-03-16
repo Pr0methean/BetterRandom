30703c6e234306b30d43e5301d775a3b2ec741ae
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2020-03-16 13:27:45 -0700

    Bug fix

--- etc/scripts/benchmark.ps1
+++ etc/scripts/benchmark.ps1
@@ -1,12 +1,12 @@
 $JAVA_HOME = [Environment]::GetEnvironmentVariable($1) # first arg names the variable that JAVA_HOME is copied from
 $JAVA_BIN = $JAVA_HOME + '/bin/java'
 $RANDOM_DOT_ORG_KEY = $env:RANDOM_DOT_ORG_KEY
-$MAYBE_PROGUARD="pre-integration-test"
 cd betterrandom
 mvn -B "-DskipTests" "-Darguments=-DskipTests" "-Dmaven.test.skip=true" "$MAYBE_ANDROID_FLAG" `
-    "clean" "$MAYBE_PROGUARD" install
+    "clean" "pre-integration-test" install
 cd ../benchmark
 mvn -B "-DskipTests" "$MAYBE_ANDROID_FLAG" package
 cd target
-"$JAVA_BIN" -jar benchmarks.jar
+$ARGS = '-jar', 'benchmarks.jar'
+& $JAVA_BIN $ARGS
 
