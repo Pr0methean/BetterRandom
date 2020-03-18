bbb461c6b91e8d9d15f4b5f56de99384c2e17e71
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2020-03-18 11:25:19 -0700

    Adjust minimums

--- benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/AbstractRandomBenchmark.java
+++ benchmark/src/main/java/io/github/pr0methean/betterrandom/benchmark/AbstractRandomBenchmark.java
@@ -54,13 +54,12 @@ abstract class AbstractRandomBenchmark<T extends Random> {
 
     builder = zeroMinimumsFor(VanillaJavaRandomBenchmark.class, ZRandomWrapperSecureRandomBenchmark.class,
         ZVanillaJavaSecureRandomBenchmark.class);
-    setMinimumNextInt(builder, AesCounterRandomBenchmark.class, 3.4e6);
     setMinimumNextLong(builder, AesCounterRandomBenchmark.class, 2.6e6);
     setMinimumNextInt(builder, Cmwc4096RandomBenchmark.class, 3.9e6);
     setMinimumNextLong(builder, Cmwc4096RandomBenchmark.class, 2.9e6);
     setMinimumNextInt(builder, MersenneTwisterRandomBenchmark.class, 4e6);
     setMinimumNextLong(builder, MersenneTwisterRandomBenchmark.class, 2.5e6);
-    setMinimumNextLong(builder, Pcg128RandomBenchmark.class, 2.8e6);
+    setMinimumNextInt(builder, Pcg128RandomBenchmark.class, 2.8e6);
     setMinimumNextInt(builder, ReseedingThreadLocalRandomWrapperAesCounterRandom128Benchmark.class, 1.9e6);
     setMinimumNextLong(builder, ReseedingThreadLocalRandomWrapperAesCounterRandom128Benchmark.class, 1.4e6);
     setMinimumNextLong(builder, SplittableRandomAdapterBenchmark.class, 2.6e6);
