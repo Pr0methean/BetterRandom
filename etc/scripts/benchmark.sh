69b161ca9287d4be735798ff1a475bb5cdccb4c4
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2020-04-05 14:07:02 -0700

    Disable Proguard on JDK14

--- etc/scripts/benchmark.sh
+++ etc/scripts/benchmark.sh
@@ -5,18 +5,13 @@ if [ "$( echo "$1" | grep -q '[A-Za-z]' )" ]; then
   export JAVA_HOME=${!1} # Bashism (https://github.com/koalaman/shellcheck/wiki/SC2039)
 fi
 JAVA_BIN=${JAVA_HOME}/bin/java
-if [ "${ANDROID}" = "true" ]; then
-  MAYBE_ANDROID_FLAG="-Pandroid"
-else
-  MAYBE_ANDROID_FLAG=""
-fi
 JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom"
 cd betterrandom
 mvn -B -DskipTests -Darguments=-DskipTests\
-    -Dmaven.test.skip=true ${MAYBE_ANDROID_FLAG}\
+    -Dmaven.test.skip=true \
     clean install &&\
 cd ../benchmark &&\
-mvn -B -DskipTests ${MAYBE_ANDROID_FLAG} package &&\
+mvn -B -DskipTests package &&\
 cd target &&\
 "${JAVA_BIN}" ${JAVA_OPTS} -jar benchmarks.jar $2 -jvm "${JAVA_BIN}" -f 1 -t 1 -foe true -v EXTRA 2>&1
 cd ../..
