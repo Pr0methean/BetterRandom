05e6e67e4827bbe320755aa03b5ae97be6db5981
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2020-04-22 18:14:10 -0700

    Fix a bug that could cause a false pass

--- etc/scripts/unit-tests.sh
+++ etc/scripts/unit-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+set -eo pipefail
 if [ "$( echo "$1" | grep -q '[A-Za-z]' )" ]; then
   # first arg names the variable that JAVA_HOME is copied from
   export JAVA_HOME=${!1} # Bashism (https://github.com/koalaman/shellcheck/wiki/SC2039)
 fi
-
 NO_GIT_PATH="${PATH}"
 if [ "${APPVEYOR}" != "" ]; then
   RANDOM_DOT_ORG_KEY=$(powershell 'Write-Host ($env:random_dot_org_key) -NoNewLine')
@@ -19,9 +19,9 @@ PATH="${NO_GIT_PATH}" mvn clean jacoco:instrument jacoco:prepare-agent \
     test jacoco:restore-instrumented-classes jacoco:report -e -B || exit 1
 if [ "${PROGUARD}" = "true" ]; then
   echo "[unit-tests.sh] Running Proguard."
-  PATH="${NO_GIT_PATH}" mvn package proguard:proguard -B && \
-      echo "[unit-tests.sh] Testing against Proguarded jar." && \
-      PATH="${NO_GIT_PATH}" mvn -Dmaven.main.skip=true integration-test -e -B
+  PATH="${NO_GIT_PATH}" mvn package proguard:proguard -B
+  echo "[unit-tests.sh] Testing against Proguarded jar."
+  PATH="${NO_GIT_PATH}" mvn -Dmaven.main.skip=true integration-test -e -B
 else
   echo "[unit-tests.sh] Proguard not enabled."
 fi
