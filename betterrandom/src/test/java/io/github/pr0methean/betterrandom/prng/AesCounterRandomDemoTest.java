b1a322817b8b4967a8bfa270cde87224afdd0cb0
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2020-04-22 19:30:44 -0700

    Bug fix

--- betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/AesCounterRandomDemoTest.java
+++ betterrandom/src/test/java/io/github/pr0methean/betterrandom/prng/AesCounterRandomDemoTest.java
@@ -1,9 +1,14 @@
 package io.github.pr0methean.betterrandom.prng;
 
 import io.github.pr0methean.betterrandom.seed.SeedException;
+import org.powermock.api.mockito.mockpolicies.Slf4jMockPolicy;
+import org.powermock.core.classloader.annotations.MockPolicy;
+import org.powermock.modules.testng.PowerMockTestCase;
 import org.testng.annotations.Test;
 
-@Test(testName = "AesCounterRandomDemo") public class AesCounterRandomDemoTest {
+@MockPolicy(Slf4jMockPolicy.class)
+@Test(testName = "AesCounterRandomDemo")
+public class AesCounterRandomDemoTest extends PowerMockTestCase {
 
   private static final String[] NO_ARGS = {};
 
