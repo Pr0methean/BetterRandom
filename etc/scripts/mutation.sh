065cf64bbe99e05d2bab40956a0c1021987d4f17
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2020-07-09 22:00:54 -0700

    Reduce log spam from Maven in mutation test

--- etc/scripts/mutation.sh
+++ etc/scripts/mutation.sh
@@ -2,5 +2,5 @@
 sudo renice -10 $$
 cd betterrandom
 set -eo pipefail # TODO: Find a pipefail method that's not Bash-specific
-mvn -Ppit clean test-compile org.pitest:pitest-maven:mutationCoverage 2>&1 | tee /tmp/mutation-log.txt
+mvn -B -Ppit clean test-compile org.pitest:pitest-maven:mutationCoverage 2>&1 | tee /tmp/mutation-log.txt
 grep -qv "BUILD FAILURE" /tmp/mutation-log.txt
